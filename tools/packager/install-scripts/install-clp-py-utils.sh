6469ec1ebbf2d3f6c0481a4b2d9bca8ab3414fff
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2021-10-01 02:01:28 -0400

    Remove obsolete installation scripts.

--- tools/packager/install-scripts/install-clp-py-utils.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing clp-py-utils"
-cd ${WORKING_DIR}/clp-py-utils
-xargs --max-args=1 --max-procs=16 pip install --target ${WORKING_DIR}/${ARTIFACT_NAME}/lib/python3/site-packages < requirements.txt
-cp -R clp_py_utils ${WORKING_DIR}/${ARTIFACT_NAME}/lib/python3/site-packages
