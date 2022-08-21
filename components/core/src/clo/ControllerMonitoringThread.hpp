e4b7635ec8420314b886f094f8848c73f96b6d73
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2022-08-21 12:22:41 -0400

    Switch from pthreads to std::thread, remove the detach method to avoid complex lifetime management, and some refactoring. (#73)

--- components/core/src/clo/ControllerMonitoringThread.hpp
+++ components/core/src/clo/ControllerMonitoringThread.hpp
@@ -2,23 +2,23 @@
 #define CONTROLLERMONITORINGTHREAD_HPP
 
 // Project headers
-#include "../PThread.hpp"
+#include "../Thread.hpp"
 
 /**
  * A thread that waits for the controller to close the connection at which time
  * it will indicate the query has been cancelled.
  */
-class ControllerMonitoringThread : public PThread {
+class ControllerMonitoringThread : public Thread {
 public:
     // Constructor
-    ControllerMonitoringThread (int controller_socket_fd) : m_controller_socket_fd(controller_socket_fd),
-                                                            m_query_cancelled(false) {}
+    ControllerMonitoringThread (int controller_socket_fd) :
+            m_controller_socket_fd(controller_socket_fd), m_query_cancelled(false) {}
 
     const std::atomic_bool& get_query_cancelled () const { return m_query_cancelled; }
 
 protected:
     // Methods
-    void* thread_method () override;
+    void thread_method () override;
 
 private:
     // Variables
