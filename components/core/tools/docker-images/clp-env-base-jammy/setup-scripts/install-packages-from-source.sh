0426eba5e05dc50a2a693de053e60874061b7fad
navh <amos@amoshebb.com>
2022-11-20 17:19:31 -0500

    no hardcoded 'x86' jammy docker tag

---
+++ components/core/tools/docker-images/clp-env-base-jammy/setup-scripts/install-packages-from-source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./tools/scripts/lib_install/fmtlib.sh 8.0.1
+./tools/scripts/lib_install/libarchive.sh 3.5.1
+./tools/scripts/lib_install/lz4.sh 1.8.2
+#./tools/scripts/lib_install/mariadb-connector-c.sh 3.3.3
+./tools/scripts/lib_install/msgpack.sh 4.1.1
+./tools/scripts/lib_install/spdlog.sh 1.9.2
+./tools/scripts/lib_install/zstandard.sh 1.4.9
---
+++ components/core/tools/docker-images/clp-env-base-jammy/setup-scripts/install-prebuilt-packages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  ca-certificates \
+  checkinstall \
+  cmake \
+  curl \
+  build-essential \
+  git \
+  libboost-filesystem-dev \
+  libboost-iostreams-dev \
+  libboost-program-options-dev \
+  libssl-dev \
+  pkg-config \
+  python3 \
+  python3-pip \
+  rsync \
+  zlib1g-dev \
+  libmariadb-dev   
