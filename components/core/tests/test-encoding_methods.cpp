d97183e9ac144c53fe71eae054ba1867eec1cecd
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-01-04 17:38:15 -0500

    Make variable encoding methods and schema match those of the existing CLP logging libraries; Add a version number for each. (#92)

--- components/core/tests/test-encoding_methods.cpp
+++ components/core/tests/test-encoding_methods.cpp
@@ -22,6 +22,12 @@ TEST_CASE("ffi::get_bounds_of_next_var", "[ffi::get_bounds_of_next_var]") {
     size_t end_pos;
     bool contains_var_placeholder = false;
 
+    // Since the outputs we use to validate depend on the schema/encoding
+    // methods, we validate the versions
+    REQUIRE(strcmp("com.yscope.clp.VariableEncodingMethodsV1",
+                   ffi::cVariableEncodingMethodsVersion) == 0);
+    REQUIRE(strcmp("com.yscope.clp.VariablesSchemaV1", ffi::cVariablesSchemaVersion) == 0);
+
     // Corner cases
     // Empty string
     str = "";
@@ -77,7 +83,7 @@ TEST_CASE("ffi::get_bounds_of_next_var", "[ffi::get_bounds_of_next_var]") {
     REQUIRE(false == contains_var_placeholder);
 
     REQUIRE(get_bounds_of_next_var(str, begin_pos, end_pos, contains_var_placeholder) == true);
-    REQUIRE("+394" == str.substr(begin_pos, end_pos - begin_pos));
+    REQUIRE("+394/-" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(false == contains_var_placeholder);
 
     REQUIRE(get_bounds_of_next_var(str, begin_pos, end_pos, contains_var_placeholder) == false);
