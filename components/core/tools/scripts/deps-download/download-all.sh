2857d135087f40323be72fe4b00b34c1a27d4af4
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-01-25 07:40:02 -0500

    Use git submodules rather than downloads when the local dir is a git repo.

--- components/core/tools/scripts/deps-download/download-all.sh
+++ components/core/tools/scripts/deps-download/download-all.sh
@@ -4,12 +4,21 @@
 set -e
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-project_root_dir=${script_dir}/../../../../../
+project_root_dir="$script_dir"/../../../../../
+component_root_dir="$script_dir"/../../../
 
-cd "${project_root_dir}"
+cd "${component_root_dir}"
 mkdir -p submodules
-python3 "${script_dir}/download-dep.py" "${script_dir}/Catch2.json"
-python3 "${script_dir}/download-dep.py" "${script_dir}/date.json"
-python3 "${script_dir}/download-dep.py" "${script_dir}/json.json"
+
+# We don't use a git submodule for sqlite3 since that would require building the
+# sqlite amalgamation
 python3 "${script_dir}/download-dep.py" "${script_dir}/sqlite3.json"
-python3 "${script_dir}/download-dep.py" "${script_dir}/yaml-cpp.json"
+
+if [ -e "$project_root_dir/.git" ] ; then
+  git submodule update --init --recursive
+else
+  python3 "${script_dir}/download-dep.py" "${script_dir}/Catch2.json"
+  python3 "${script_dir}/download-dep.py" "${script_dir}/date.json"
+  python3 "${script_dir}/download-dep.py" "${script_dir}/json.json"
+  python3 "${script_dir}/download-dep.py" "${script_dir}/yaml-cpp.json"
+fi
