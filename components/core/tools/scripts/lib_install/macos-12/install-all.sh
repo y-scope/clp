c18d9090be091d69703efb9ae8dc501ab53989d2
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-05-21 17:08:08 -0400

    Lock macOS to specific versions of fmt & spdlog to resolve build failures.

--- components/core/tools/scripts/lib_install/macos-12/install-all.sh
+++ components/core/tools/scripts/lib_install/macos-12/install-all.sh
@@ -4,12 +4,12 @@ brew update
 brew install \
   boost \
   cmake \
-  fmt \
+  fmt@8.0.1 \
   gcc \
   libarchive \
   lz4 \
   mariadb-connector-c \
   msgpack-cxx \
-  spdlog \
+  spdlog@1.9.2 \
   pkg-config \
   zstd
