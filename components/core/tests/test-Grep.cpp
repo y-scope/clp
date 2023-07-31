8f5b2919e4e70c726e4842b12fef1fd7debe1dc3
SharafMohamed <chrismohamed91@gmail.com>
2023-07-31 14:02:12 -0400

    Cleaned up test-Grep.cpp

--- components/core/tests/test-Grep.cpp
+++ components/core/tests/test-Grep.cpp
@@ -42,44 +42,53 @@ TEST_CASE("get_bounds_of_next_potential_var", "[get_bounds_of_next_potential_var
     str = "";
     begin_pos = 0;
     end_pos = 0;
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == false);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == false);
 
     // No tokens
     str = "=";
     begin_pos = 0;
     end_pos = 0;
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == false);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == false);
 
     // No wildcards
     str = " MAC address 95: ad ff 95 24 0d ff =-abc- ";
     begin_pos = 0;
     end_pos = 0;
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("95" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("ad" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("ff" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("95" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("24" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("0d" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE("ff" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
@@ -87,7 +96,8 @@ TEST_CASE("get_bounds_of_next_potential_var", "[get_bounds_of_next_potential_var
     REQUIRE("-abc-" == str.substr(begin_pos, end_pos - begin_pos));
     REQUIRE(true == is_var);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == false);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == false);
     REQUIRE(str.length() == begin_pos);
 
     // With wildcards
@@ -95,27 +105,33 @@ TEST_CASE("get_bounds_of_next_potential_var", "[get_bounds_of_next_potential_var
     begin_pos = 0;
     end_pos = 0;
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE(str.substr(begin_pos, end_pos - begin_pos) == "1\\*x");
     REQUIRE(is_var == true);
     //REQUIRE(is_var == true);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE(str.substr(begin_pos, end_pos - begin_pos) == "abc*123");
     REQUIRE(is_var == false);
     //REQUIRE(is_var == true);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE(str.substr(begin_pos, end_pos - begin_pos) == "1.2");
     REQUIRE(is_var == true);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE(str.substr(begin_pos, end_pos - begin_pos) == "+394/-");
     REQUIRE(is_var == true);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == true);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == true);
     REQUIRE(str.substr(begin_pos, end_pos - begin_pos) == "-*abc-");
     REQUIRE(is_var == false);
 
-    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer, reverse_lexer, post_string) == false);
+    REQUIRE(Grep::get_bounds_of_next_potential_var(str, begin_pos, end_pos, is_var, forward_lexer,
+                                                   reverse_lexer, post_string) == false);
 }
