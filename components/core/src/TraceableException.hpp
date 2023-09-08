c119af520282d377f5d63bf587cd477335293a26
davidlion <david.lion@yscope.com>
2023-09-08 19:28:02 -0400

    Revert formatting changes

--- components/core/src/TraceableException.hpp
+++ components/core/src/TraceableException.hpp
@@ -10,29 +10,25 @@
 class TraceableException : public std::exception {
 public:
     // Constructors
-    TraceableException(ErrorCode error_code, char const* const filename, int const line_number)
-            : m_error_code(error_code),
-              m_filename(filename),
-              m_line_number(line_number) {}
+    TraceableException (ErrorCode error_code, const char* const filename, const int line_number) : m_error_code(error_code), m_filename(filename),
+            m_line_number(line_number) {}
 
     // Copy constructor / assignment operators
-    TraceableException(TraceableException const&) = default;
-    TraceableException& operator=(TraceableException const&) = default;
+    TraceableException (const TraceableException&) = default;
+    TraceableException& operator= (const TraceableException&) = default;
 
     // Methods
-    ErrorCode get_error_code() const { return m_error_code; }
-
-    char const* get_filename() const { return m_filename; }
-
-    int get_line_number() const { return m_line_number; }
+    ErrorCode get_error_code () const { return m_error_code; }
+    const char* get_filename () const { return m_filename; }
+    int get_line_number () const { return m_line_number; }
 
     // NOTE: We make what() abstract to make the entire class abstract
-    virtual char const* what() const noexcept = 0;
+    virtual const char* what () const noexcept = 0;
 
 private:
     // Variables
     ErrorCode m_error_code;
-    char const* m_filename;
+    const char* m_filename;
     int m_line_number;
 };
 
@@ -45,4 +41,4 @@ private:
     #define __FILENAME__ __FILE__
 #endif
 
-#endif  // TRACEABLEEXCEPTION
+#endif // TRACEABLEEXCEPTION
