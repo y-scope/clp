bb51d8daae89358ae9fb94ac931b1b891a562c32
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-10-08 17:03:26 -0400

    Add mongocxx to Ubuntu 20 dependencies container image.

---
+++ components/core/tools/scripts/lib_install/mongocxx.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+cUsage="Usage: ${BASH_SOURCE[0]} <version>[ <.deb output directory>]"
+if [ "$#" -lt 1 ] ; then
+    echo $cUsage
+    exit
+fi
+version=$1
+
+package_name=libmongocxx-dev
+temp_dir=/tmp/${package_name}-installation
+deb_output_dir=${temp_dir}
+if [[ "$#" -gt 1 ]] ; then
+  deb_output_dir="$(readlink -f "$2")"
+  if [ ! -d ${deb_output_dir} ] ; then
+    echo "${deb_output_dir} does not exist or is not a directory"
+    exit
+  fi
+fi
+
+# Check if already installed
+set +e
+dpkg -l ${package_name} | grep ${version}
+installed=$?
+set -e
+if [ $installed -eq 0 ] ; then
+  # Nothing to do
+  exit
+fi
+
+echo "Checking for elevated privileges..."
+privileged_command_prefix=""
+if [ ${EUID:-$(id -u)} -ne 0 ] ; then
+  sudo echo "Script can elevate privileges."
+  privileged_command_prefix="${privileged_command_prefix} sudo"
+fi
+
+# Download
+mkdir -p $temp_dir
+cd $temp_dir
+extracted_dir=${temp_dir}/mongo-cxx-driver-r${version}
+if [ ! -e ${extracted_dir} ] ; then
+  tar_filename=mongo-cxx-driver-r${version}.tar.gz
+  if [ ! -e ${tar_filename} ] ; then
+    curl -fsSL https://github.com/mongodb/mongo-cxx-driver/releases/download/r${version}/${tar_filename} -o ${tar_filename}
+  fi
+
+  tar -xf ${tar_filename}
+fi
+
+# Set up
+cd ${extracted_dir}/build
+# NOTE: Although the mongocxx docs indicate we should use
+# '-DMONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX=OFF' to install to the default
+# location (/usr/local) but this doesn't seem to work, so we specify
+# CMAKE_INSTALL_PREFIX here
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr/local \
+  -DBUILD_SHARED_AND_STATIC_LIBS=ON \
+  -DENABLE_TESTS=OFF \
+  ..
+
+# Check if checkinstall is installed
+set +e
+command -v checkinstall
+checkinstall_installed=$?
+set -e
+
+# Install
+install_command_prefix="${privileged_command_prefix}"
+if [ $checkinstall_installed -eq 0 ] ; then
+  install_command_prefix="${install_command_prefix} checkinstall --pkgname '${package_name}' --pkgversion '${version}' --provides '${package_name}' --nodoc -y --pakdir \"${deb_output_dir}\""
+fi
+${install_command_prefix} cmake --build . --target install
+
+# Clean up
+rm -rf $temp_dir
