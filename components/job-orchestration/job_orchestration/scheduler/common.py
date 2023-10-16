cfdd7a08975dd3397be6c95142276d785f8f8496
gibber9809 <devinbook1@gmail.com>
2023-10-16 12:35:26 -0400

    Change job status enum to fix reducers not picking up new jobs

--- components/job-orchestration/job_orchestration/scheduler/common.py
+++ components/job-orchestration/job_orchestration/scheduler/common.py
@@ -2,20 +2,21 @@ from __future__ import annotations
 
 from enum import IntEnum, auto
 
-
+# When adding new states always add them to the end of this enum
+# and make necessary changes in the UI, Search Scheduler, and Reducer
 class JobStatus(IntEnum):
     PENDING = 0
     RUNNING = auto()
     DONE = auto()
     SUCCESS = auto()
     SUCCESS_WITH_ERRORS = auto()
-    NO_MATCHING_ARCHIVE = auto()
     FAILED = auto()
     CANCELLING = auto()
     CANCELLED = auto()
     PENDING_REDUCER = auto()
     REDUCER_READY = auto()
     PENDING_REDUCER_DONE = auto()
+    NO_MATCHING_ARCHIVE = auto()
 
     def __str__(self) -> str:
         return str(self.value).lower()
