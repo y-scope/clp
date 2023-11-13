ecc29aa2e63c4a616ae0bbee35319777664d6823
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-11-13 08:45:06 -0500

    Apply clang-format to networking namespace.

--- components/core/src/networking/socket_utils.cpp
+++ components/core/src/networking/socket_utils.cpp
@@ -1,58 +1,54 @@
 #include "socket_utils.hpp"
 
-// C standard libraries
 #include <sys/socket.h>
 
-// C++ standard libraries
 #include <cstdio>
 
-// Project headers
 #include "../Defs.h"
 #include "SocketOperationFailed.hpp"
 
 namespace networking {
-    ErrorCode try_send (int fd, const char* buf, size_t buf_len) {
-        if (fd < 0 || nullptr == buf) {
-            return ErrorCode_BadParam;
-        }
-
-        ssize_t num_bytes_sent = ::send(fd, buf, buf_len, 0);
-        if (-1 == num_bytes_sent) {
-            return ErrorCode_errno;
-        }
-
-        return ErrorCode_Success;
+ErrorCode try_send(int fd, char const* buf, size_t buf_len) {
+    if (fd < 0 || nullptr == buf) {
+        return ErrorCode_BadParam;
     }
 
-    void send (int fd, const char* buf, size_t buf_len) {
-        auto error_code = try_send(fd, buf, buf_len);
-        if (ErrorCode_Success != error_code) {
-            throw SocketOperationFailed(error_code, __FILENAME__, __LINE__);
-        }
+    ssize_t num_bytes_sent = ::send(fd, buf, buf_len, 0);
+    if (-1 == num_bytes_sent) {
+        return ErrorCode_errno;
     }
 
+    return ErrorCode_Success;
+}
 
-    ErrorCode try_receive (int fd, char* buf, size_t buf_len, size_t& num_bytes_received) {
-        if (fd < 0 || nullptr == buf) {
-            return ErrorCode_BadParam;
-        }
-
-        ssize_t result = recv(fd, buf, buf_len, 0);
-        if (result < 0) {
-            return ErrorCode_errno;
-        }
-        if (0 == result) {
-            return ErrorCode_EndOfFile;
-        }
-        num_bytes_received = result;
-
-        return ErrorCode_Success;
-    }
-
-    void receive (int fd, char* buf, size_t buf_len, size_t& num_bytes_received) {
-        auto error_code = try_receive(fd, buf, buf_len, num_bytes_received);
-        if (ErrorCode_Success != error_code) {
-            throw SocketOperationFailed(error_code, __FILENAME__, __LINE__);
-        }
+void send(int fd, char const* buf, size_t buf_len) {
+    auto error_code = try_send(fd, buf, buf_len);
+    if (ErrorCode_Success != error_code) {
+        throw SocketOperationFailed(error_code, __FILENAME__, __LINE__);
     }
 }
+
+ErrorCode try_receive(int fd, char* buf, size_t buf_len, size_t& num_bytes_received) {
+    if (fd < 0 || nullptr == buf) {
+        return ErrorCode_BadParam;
+    }
+
+    ssize_t result = recv(fd, buf, buf_len, 0);
+    if (result < 0) {
+        return ErrorCode_errno;
+    }
+    if (0 == result) {
+        return ErrorCode_EndOfFile;
+    }
+    num_bytes_received = result;
+
+    return ErrorCode_Success;
+}
+
+void receive(int fd, char* buf, size_t buf_len, size_t& num_bytes_received) {
+    auto error_code = try_receive(fd, buf, buf_len, num_bytes_received);
+    if (ErrorCode_Success != error_code) {
+        throw SocketOperationFailed(error_code, __FILENAME__, __LINE__);
+    }
+}
+}  // namespace networking
