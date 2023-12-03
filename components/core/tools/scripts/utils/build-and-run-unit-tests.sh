8d74bf1ab55d1068d5c656424eb72f438f537917
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-12-03 17:01:58 -0500

    Omit Stopwatch tests in clp-core-build-macos.

--- components/core/tools/scripts/utils/build-and-run-unit-tests.sh
+++ components/core/tools/scripts/utils/build-and-run-unit-tests.sh
@@ -5,15 +5,22 @@ set -e
 # Error on undefined variable
 set -u
 
-cUsage="Usage: ${BASH_SOURCE[0]} <source-dir> <build-dir>"
-if [ "$#" -lt 1 ] ; then
+cUsage="Usage: ${BASH_SOURCE[0]} <source-dir> <build-dir>[ <unit-tests-filter>]"
+if [ "$#" -lt 2 ]; then
     echo "$cUsage"
     exit 1
 fi
 src_dir="$1"
 build_dir="$2"
+if [ "$#" -gt 2 ]; then
+    unit_tests_filter="$3"
+fi
 
 cmake -S "$src_dir" -B "$build_dir"
 cmake --build "$build_dir"
 cd "$build_dir"
-./unitTest
+if [ -z "${unit_tests_filter+x}" ]; then
+    ./unitTest
+else
+    ./unitTest "$unit_tests_filter"
+fi
