6ff7a75a0a7036f40cd09e435de1db180522528c
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-12-11 05:22:50 -0500

    Apply clang-format to streaming_archive::reader namespace.

--- components/core/src/streaming_archive/reader/Message.hpp
+++ components/core/src/streaming_archive/reader/Message.hpp
@@ -1,28 +1,26 @@
 #ifndef STREAMING_ARCHIVE_READER_MESSAGE_HPP
 #define STREAMING_ARCHIVE_READER_MESSAGE_HPP
 
-// C++ libraries
 #include <cstddef>
 #include <vector>
 
-// Project headers
 #include "../../Defs.h"
 
 namespace streaming_archive { namespace reader {
     class Message {
     public:
         // Methods
-        size_t get_message_number () const;
-        logtype_dictionary_id_t get_logtype_id () const;
-        const std::vector<encoded_variable_t>& get_vars () const;
-        epochtime_t get_ts_in_milli () const;
+        size_t get_message_number() const;
+        logtype_dictionary_id_t get_logtype_id() const;
+        std::vector<encoded_variable_t> const& get_vars() const;
+        epochtime_t get_ts_in_milli() const;
 
-        void set_message_number (uint64_t message_number);
-        void set_logtype_id (logtype_dictionary_id_t logtype_id);
-        void add_var (encoded_variable_t var);
-        void set_timestamp (epochtime_t timestamp);
+        void set_message_number(uint64_t message_number);
+        void set_logtype_id(logtype_dictionary_id_t logtype_id);
+        void add_var(encoded_variable_t var);
+        void set_timestamp(epochtime_t timestamp);
 
-        void clear_vars ();
+        void clear_vars();
 
     private:
         friend class Archive;
@@ -33,6 +31,6 @@ namespace streaming_archive { namespace reader {
         std::vector<encoded_variable_t> m_vars;
         epochtime_t m_timestamp;
     };
-} }
+}}  // namespace streaming_archive::reader
 
-#endif // STREAMING_ARCHIVE_READER_MESSAGE_HPP
+#endif  // STREAMING_ARCHIVE_READER_MESSAGE_HPP
--- components/core/src/streaming_archive/reader/SegmentManager.hpp
+++ components/core/src/streaming_archive/reader/SegmentManager.hpp
@@ -1,19 +1,18 @@
 #ifndef STREAMING_ARCHIVE_READER_SEGMENTMANAGER_HPP
 #define STREAMING_ARCHIVE_READER_SEGMENTMANAGER_HPP
 
-// C++ libraries
 #include <cstddef>
 #include <list>
 #include <string>
 #include <unordered_map>
 
-// Project headers
 #include "../../Defs.h"
 #include "Segment.hpp"
 
 namespace streaming_archive { namespace reader {
     /**
-     * This class handles segments in a given directory. This primarily consists of reading from segments in a given directory.
+     * This class handles segments in a given directory. This primarily consists of reading from
+     * segments in a given directory.
      */
     class SegmentManager {
     public:
@@ -22,15 +21,16 @@ namespace streaming_archive { namespace reader {
          * Opens the segment manager
          * @param segment_dir_path
          */
-        void open (const std::string& segment_dir_path);
+        void open(std::string const& segment_dir_path);
 
         /**
          * Closes the segment manager
          */
-        void close ();
+        void close();
 
         /**
-         * Tries to read content with the given offset and length from a segment with the given ID into a buffer
+         * Tries to read content with the given offset and length from a segment with the given ID
+         * into a buffer
          * @param segment_id
          * @param decompressed_stream_pos
          * @param extraction_buf
@@ -39,7 +39,12 @@ namespace streaming_archive { namespace reader {
          * @return Same as streaming_archive::reader::Segment::try_read
          * @throw std::out_of_range if a segment ID cannot be found unexpectedly
          */
-        ErrorCode try_read (segment_id_t segment_id, const uint64_t decompressed_stream_pos, char* extraction_buf, const uint64_t extraction_len);
+        ErrorCode try_read(
+                segment_id_t segment_id,
+                uint64_t const decompressed_stream_pos,
+                char* extraction_buf,
+                uint64_t const extraction_len
+        );
 
     private:
         std::string m_segment_dir_path;
@@ -48,6 +53,6 @@ namespace streaming_archive { namespace reader {
         // List of open segment IDs in LRU order (LRU segment ID at front)
         std::list<segment_id_t> m_lru_ids_of_open_segments;
     };
-} }
+}}  // namespace streaming_archive::reader
 
-#endif // STREAMING_ARCHIVE_READER_SEGMENTMANAGER_HPP
+#endif  // STREAMING_ARCHIVE_READER_SEGMENTMANAGER_HPP
