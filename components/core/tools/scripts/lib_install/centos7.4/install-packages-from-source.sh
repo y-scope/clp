efc34ae831550958a0a403a267a613690514e29b
Devin Kenneth Gibson <gibber9809@users.noreply.github.com>
2024-01-07 22:02:24 -0500

    Add mongocxx dependencies to all dev environments. (#218)

--- components/core/tools/scripts/lib_install/centos7.4/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/centos7.4/install-packages-from-source.sh
@@ -3,6 +3,9 @@
 # Enable gcc 10
 source /opt/rh/devtoolset-10/enable
 
+# Enable git
+source /opt/rh/rh-git227/enable
+
 # NOTE: cmake and boost must be installed first since the remaining packages depend on them
 ./tools/scripts/lib_install/install-cmake.sh 3.21.2
 ./tools/scripts/lib_install/install-boost.sh 1.76.0
@@ -11,6 +14,8 @@ source /opt/rh/devtoolset-10/enable
 ./tools/scripts/lib_install/libarchive.sh 3.5.1
 ./tools/scripts/lib_install/lz4.sh 1.8.2
 ./tools/scripts/lib_install/mariadb-connector-c.sh 3.2.3
+./tools/scripts/lib_install/mongoc.sh 1.24.4
+./tools/scripts/lib_install/mongocxx.sh 3.8.0
 ./tools/scripts/lib_install/msgpack.sh 6.0.0
 ./tools/scripts/lib_install/spdlog.sh 1.9.2
 ./tools/scripts/lib_install/zstandard.sh 1.4.9
