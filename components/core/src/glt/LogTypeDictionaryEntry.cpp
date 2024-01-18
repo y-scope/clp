27a826fd22e39e3b97eeb58483f64b6f02723061
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-01-18 20:11:12 +0000

    Add get variable info for now.

--- components/core/src/glt/LogTypeDictionaryEntry.cpp
+++ components/core/src/glt/LogTypeDictionaryEntry.cpp
@@ -24,11 +24,26 @@ size_t LogTypeDictionaryEntry::get_placeholder_info(
     return m_placeholder_positions[placeholder_ix];
 }
 
+size_t LogTypeDictionaryEntry::get_variable_info(
+        size_t var_ix,
+        ir::VariablePlaceholder &placeholder
+) const {
+    if (var_ix >= m_variable_positions.size()) {
+        return SIZE_MAX;
+    }
+
+    auto var_position = m_variable_positions[var_ix];
+    placeholder = static_cast<VariablePlaceholder>(m_value[var_position]);
+
+    return var_position;
+}
+
 size_t LogTypeDictionaryEntry::get_data_size() const {
     // NOTE: sizeof(vector[0]) is executed at compile time so there's no risk of an exception at
     // runtime
     return sizeof(m_id) + m_value.length()
            + m_placeholder_positions.size() * sizeof(m_placeholder_positions[0])
+           + m_variable_positions.size() * sizeof(m_variable_positions[0])
            + m_ids_of_segments_containing_entry.size() * sizeof(segment_id_t);
 }
 
@@ -105,6 +120,7 @@ bool LogTypeDictionaryEntry::parse_next_var(
 void LogTypeDictionaryEntry::clear() {
     m_value.clear();
     m_placeholder_positions.clear();
+    m_variable_positions.clear();
     m_num_escaped_placeholders = 0;
 }
 
@@ -156,14 +172,17 @@ ErrorCode LogTypeDictionaryEntry::try_read_from_file(
             if (enum_to_underlying_type(VariablePlaceholder::Integer) == c) {
                 add_constant(constant, 0, constant.length());
                 constant.clear();
+                m_variable_positions.push_back(m_value.length());
                 add_int_var();
             } else if (enum_to_underlying_type(VariablePlaceholder::Float) == c) {
                 add_constant(constant, 0, constant.length());
                 constant.clear();
+                m_variable_positions.push_back(m_value.length());
                 add_float_var();
             } else if (enum_to_underlying_type(VariablePlaceholder::Dictionary) == c) {
                 add_constant(constant, 0, constant.length());
                 constant.clear();
+                m_variable_positions.push_back(m_value.length());
                 add_dictionary_var();
             } else {
                 constant += c;
