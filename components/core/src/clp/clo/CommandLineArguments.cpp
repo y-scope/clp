d6ceab91b6cd0f5f69fc0bde56a6d37f4498a60a
gibber9809 <devinbook1@gmail.com>
2024-01-24 17:45:40 +0000

    Format c++ files missed in previous commit

--- components/core/src/clp/clo/CommandLineArguments.cpp
+++ components/core/src/clp/clo/CommandLineArguments.cpp
@@ -272,8 +272,7 @@ CommandLineArguments::parse_arguments(int argc, char const* argv[]) {
 }
 
 void CommandLineArguments::print_basic_usage() const {
-    cerr << "Usage: " << get_program_name()
-         << " [OPTIONS] MONGODB_URI MONGODB_COLLECTION "
+    cerr << "Usage: " << get_program_name() << " [OPTIONS] MONGODB_URI MONGODB_COLLECTION "
          << R"(ARCHIVE_PATH "WILDCARD STRING" [FILE])" << endl;
 }
 }  // namespace clp::clo
