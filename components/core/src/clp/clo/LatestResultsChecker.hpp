eff038f6e48eb838d707978589182916f69f1533
wraymo <raywangv@gmail.com>
2024-01-30 19:38:42 -0500

    minor touch

--- components/core/src/clp/clo/LatestResultsChecker.hpp
+++ components/core/src/clp/clo/LatestResultsChecker.hpp
@@ -12,34 +12,34 @@ class LatestResultsChecker {
 public:
     //
     LatestResultsChecker(
-            uint64_t num_latest_results,
+            uint64_t target_num_latest_results,
             std::set<segment_id_t> const& segments_to_search
     )
-            : m_num_latest_results(num_latest_results),
+            : m_target_num_latest_results(target_num_latest_results),
               m_num_accumulated_results(0),
               last_begin_ts(cEpochTimeMin),
               m_segments_to_search(segments_to_search) {}
 
     bool need_to_scan(clp::streaming_archive::reader::File& compressed_file) {
-        if (m_num_latest_results == 0) {
+        if (m_target_num_latest_results == 0) {
             return true;
         }
 
         epochtime_t begin_ts = compressed_file.get_begin_ts();
         epochtime_t end_ts = compressed_file.get_end_ts();
-        if (m_num_accumulated_results >= m_num_latest_results && end_ts < last_begin_ts) {
-            return true;
+        if (m_num_accumulated_results >= m_target_num_latest_results && end_ts < last_begin_ts) {
+            return false;
         }
 
-        if (m_num_accumulated_results < m_num_latest_results) {
+        if (m_num_accumulated_results < m_target_num_latest_results) {
             last_begin_ts = begin_ts;
         }
 
-        return false;
+        return true;
     }
 
     bool is_segment_id_relevant(segment_id_t segment_id) const {
-        if (m_num_latest_results == 0) {
+        if (m_target_num_latest_results == 0) {
             return true;
         }
 
@@ -49,7 +49,7 @@ public:
     void increment_num_accumulated_results() { ++m_num_accumulated_results; }
 
 private:
-    uint64_t m_num_latest_results, m_num_accumulated_results;
+    uint64_t m_target_num_latest_results, m_num_accumulated_results;
     epochtime_t last_begin_ts;
     std::set<clp::segment_id_t> const& m_segments_to_search;
 };
