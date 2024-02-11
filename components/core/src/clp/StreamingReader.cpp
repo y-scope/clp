832e88dd2665fa8bcdf0446eae9ef3d2c537d17e
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-02-11 16:57:24 +0800

    Add src code of the streaming reader

---
+++ components/core/src/clp/StreamingReader.cpp
@@ -0,0 +1,352 @@
+#include "StreamingReader.hpp"
+
+#include <curl/curl.h>
+
+#include <chrono>
+#include <cstring>
+
+namespace clp {
+namespace {
+/**
+ * Casts the given ptr to a StreamingReader object. This wrapper function is used to silence
+ * clang-tidy warnings.
+ * @param ptr The address of a StreamingReader object.
+ * @return a StreamingReader type pointer.
+ */
+[[nodiscard]] auto get_reader(void* ptr) -> StreamingReader* {
+    return reinterpret_cast<StreamingReader*>(ptr);
+}
+}  // namespace
+
+bool StreamingReader::m_initialized{false};
+
+auto StreamingReader::global_init() -> ErrorCode {
+    if (m_initialized) {
+        return ErrorCode_Success;
+    }
+    if (0 != curl_global_init(CURL_GLOBAL_ALL)) {
+        return ErrorCode_Failure;
+    }
+    m_initialized = true;
+    return ErrorCode_Success;
+}
+
+auto StreamingReader::global_cleanup() -> void {
+    curl_global_cleanup();
+    m_initialized = false;
+}
+
+auto StreamingReader::transfer_thread_entry(
+        StreamingReader& reader,
+        size_t offset,
+        bool disable_caching
+) -> void {
+    CURL* curl_handle{curl_easy_init()};
+    if (nullptr == curl_handle) {
+        reader.m_status_code = StatusCode::Failed;
+        reader.m_transfer_terminated.store(true);
+        return;
+    }
+
+    // Setup download URL
+    curl_easy_setopt(curl_handle, CURLOPT_URL, reader.m_src_url.c_str());
+
+    // Setup progress callback
+    curl_easy_setopt(curl_handle, CURLOPT_XFERINFOFUNCTION, StreamingReader::progress_callback);
+    curl_easy_setopt(curl_handle, CURLOPT_XFERINFODATA, static_cast<void*>(&reader));
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 0);
+
+    // Setup write callback
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, StreamingReader::write_callback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, static_cast<void*>(&reader));
+
+    // Setup timeout
+    curl_easy_setopt(
+            curl_handle,
+            CURLOPT_CONNECTTIMEOUT,
+            static_cast<long>(reader.m_connection_timeout)
+    );
+    curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, static_cast<long>(reader.m_operation_timeout));
+
+    // Setup verbose
+    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+
+    // Setup the header (if necessary)
+    reader.m_file_pos = offset;
+    struct curl_slist* request_header{nullptr};
+    if (0 != offset) {
+        std::string range{"Range: bytes=" + std::to_string(offset) + "-"};
+        request_header = curl_slist_append(request_header, range.c_str());
+    }
+    if (disable_caching) {
+        request_header = curl_slist_append(request_header, "Cache-Control: no-cache");
+        request_header = curl_slist_append(request_header, "Pragma: no-cache");
+    }
+    if (nullptr != request_header) {
+        curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, request_header);
+    }
+
+    auto const retval{curl_easy_perform(curl_handle)};
+    reader.commit_fetching_buffer();
+    reader.m_status_code = (0 != retval) ? StatusCode::Failed : StatusCode::Finished;
+    reader.m_curl_return_code = retval;
+
+    curl_easy_cleanup(curl_handle);
+    reader.m_transfer_terminated.store(true);
+}
+
+auto StreamingReader::progress_callback(
+        void* ptr,
+        [[maybe_unused]] curl_off_t dltotal,
+        [[maybe_unused]] curl_off_t dlnow,
+        [[maybe_unused]] curl_off_t ultotal,
+        [[maybe_unused]] curl_off_t ulnow
+) -> int {
+    auto const* reader{get_reader(ptr)};
+    return reader->is_transfer_aborted() ? 1 : 0;
+}
+
+auto StreamingReader::write_callback(char* ptr, size_t size, size_t nmemb, void* reader_ptr)
+        -> size_t {
+    auto const num_total_bytes{size * nmemb};
+    try {
+        StreamingReader::BufferView input_buffer{ptr, num_total_bytes};
+        auto* reader{get_reader(ptr)};
+        while (false == input_buffer.empty()) {
+            auto const num_bytes_left{input_buffer.size()};
+            StreamingReader::BufferView fetching_buffer;
+            if (false == reader->get_buffer_to_fetch(fetching_buffer)) {
+                return 0;
+            }
+            auto const num_bytes_to_fetch{
+                    num_bytes_left > fetching_buffer.size() ? fetching_buffer.size()
+                                                            : num_bytes_left
+            };
+            memcpy(fetching_buffer.data(), input_buffer.data(), num_bytes_to_fetch);
+            input_buffer = input_buffer.last(num_bytes_left - num_bytes_to_fetch);
+            reader->commit_fetching(num_bytes_to_fetch);
+        }
+    } catch (TraceableException const& ex) {
+        // TODO: add logging to track the exceptions.
+        return 0;
+    }
+    return num_total_bytes;
+}
+
+auto StreamingReader::open(std::string_view src_url, size_t offset, bool disable_caching) -> void {
+    if (false == m_initialized) {
+        throw OperationFailed(ErrorCode_NotReady, __FILE__, __LINE__);
+    }
+    if (StatusCode::NotInit != m_status_code) {
+        throw OperationFailed(ErrorCode_NotReady, __FILE__, __LINE__);
+    }
+    m_src_url = src_url;
+    m_transfer_thread = std::make_unique<std::thread>(
+            transfer_thread_entry,
+            std::ref(*this),
+            offset,
+            disable_caching
+    );
+    m_status_code = StatusCode::InProgress;
+    m_transfer_thread->detach();
+}
+
+auto StreamingReader::terminate_current_transfer() -> void {
+    if (StatusCode::InProgress != m_status_code) {
+        while (false == is_transfer_terminated())
+            ;
+        return;
+    }
+    // If control flow reaches here, it means the we need to kill the current connected session.
+    // Since the fetcher thread is an async daemon thread, we need to set the flag for aborting
+    // and wait for it to terminate.
+    // TODO: we could use sleep here instead of actively pulling.
+    auto transfer_aborted{is_transfer_aborted()};
+    while (false == is_transfer_terminated()) {
+        if (transfer_aborted) {
+            continue;
+        }
+        abort_data_transfer();
+        transfer_aborted = true;
+    }
+}
+
+auto StreamingReader::abort_data_transfer() -> void {
+    m_transfer_aborted.store(true);
+}
+
+auto StreamingReader::set_fetching_buffer() -> bool {
+    std::unique_lock<std::mutex> buffer_resource_lock{m_buffer_resource_mutex};
+    if (m_fetching_buffer.has_value()) {
+        throw OperationFailed(ErrorCode_Corrupt, __FILE__, __LINE__);
+    }
+    while (m_num_fetched_buffer >= m_buffer_pool_size) {
+        if (m_num_fetched_buffer != m_buffer_pool_size) {
+            throw OperationFailed(ErrorCode_Corrupt, __FILE__, __LINE__);
+        }
+        if (std::cv_status::timeout
+            != m_cv_fetcher.wait_for(
+                    buffer_resource_lock,
+                    std::chrono::milliseconds(cConditionVariableTimeoutMilliSecond)
+            ))
+        {
+            continue;
+        }
+        if (is_transfer_aborted()) {
+            return false;
+        }
+        if (is_transfer_timedout()) {
+            return false;
+        }
+    }
+    m_fetching_buffer.emplace(m_buffer_pool.at(m_curr_fetching_buffer_idx).get(), m_buffer_size);
+    return true;
+}
+
+auto StreamingReader::commit_fetching_buffer() -> void {
+    if (false == m_fetching_buffer.has_value()) {
+        return;
+    }
+    std::unique_lock<std::mutex> buffer_resource_lock{m_buffer_resource_mutex};
+    auto const fetching_buffer{m_fetching_buffer.value()};
+    m_fetched_buffer_queue.emplace(fetching_buffer.data(), m_fetching_buffer_pos);
+    ++m_num_fetched_buffer;
+
+    m_fetching_buffer.reset();
+    m_fetching_buffer_pos = 0;
+    ++m_curr_fetching_buffer_idx;
+    if (m_curr_fetching_buffer_idx == m_buffer_pool_size) {
+        m_curr_fetching_buffer_idx = 0;
+    }
+
+    m_cv_reader.notify_all();
+}
+
+auto StreamingReader::set_reading_buffer() -> bool {
+    std::unique_lock<std::mutex> buffer_resource_lock{m_buffer_resource_mutex};
+    if (m_reading_buffer.has_value()) {
+        throw OperationFailed(ErrorCode_Corrupt, __FILE__, __LINE__);
+    }
+    while (m_fetched_buffer_queue.empty()) {
+        if (is_transfer_terminated()) {
+            return false;
+        }
+        m_cv_reader.wait_for(
+                buffer_resource_lock,
+                std::chrono::milliseconds(cConditionVariableTimeoutMilliSecond)
+        );
+    }
+    auto const next_reading_buffer{m_fetched_buffer_queue.front()};
+    m_fetched_buffer_queue.pop();
+    m_reading_buffer.emplace(next_reading_buffer);
+    return true;
+}
+
+auto StreamingReader::free_reading_buffer() -> void {
+    std::unique_lock<std::mutex> buffer_resource_lock{m_buffer_resource_mutex};
+    m_reading_buffer.reset();
+    --m_num_fetched_buffer;
+    m_cv_fetcher.notify_all();
+}
+
+auto StreamingReader::commit_fetching(size_t num_bytes_fetched) -> void {
+    m_fetching_buffer_pos += num_bytes_fetched;
+    if (m_fetching_buffer_pos > m_buffer_size) {
+        throw OperationFailed(ErrorCode_Corrupt, __FILE__, __LINE__);
+    }
+    if (m_fetching_buffer_pos == m_buffer_size) {
+        commit_fetching_buffer();
+    }
+}
+
+auto StreamingReader::commit_reading(size_t num_bytes_read) -> void {
+    auto const reading_buffer_size{m_reading_buffer.value().size()};
+    if (reading_buffer_size < num_bytes_read) {
+        throw OperationFailed(ErrorCode_Corrupt, __FILE__, __LINE__);
+    }
+    auto const num_bytes_left{reading_buffer_size - num_bytes_read};
+    if (0 == num_bytes_left) {
+        free_reading_buffer();
+    } else {
+        m_reading_buffer = m_reading_buffer.value().last(num_bytes_left);
+    }
+}
+
+auto StreamingReader::get_buffer_to_fetch(BufferView& fetching_buffer) -> bool {
+    if (false == m_fetching_buffer.has_value()) {
+        if (false == set_fetching_buffer()) {
+            return false;
+        }
+        fetching_buffer = m_fetching_buffer.value();
+        return true;
+    }
+    fetching_buffer = m_fetching_buffer.value().subspan(
+            m_fetching_buffer_pos,
+            m_buffer_size - m_fetching_buffer_pos
+    );
+    return true;
+}
+
+auto StreamingReader::reset() -> void {
+    if (StatusCode::NotInit == m_status_code) {
+        return;
+    }
+
+    if (false == m_transfer_terminated.load()) {
+        throw OperationFailed(ErrorCode_Failure, __FILE__, __LINE__);
+    }
+
+    m_src_url.clear();
+    m_file_pos = 0;
+
+    std::queue<BufferView> empty_buffer_queue;
+    m_fetched_buffer_queue.swap(empty_buffer_queue);
+    m_num_fetched_buffer = 0;
+    m_fetching_buffer_pos = 0;
+    m_fetching_buffer.reset();
+    m_reading_buffer.reset();
+
+    m_transfer_thread.reset();
+    m_transfer_aborted.store(false);
+    m_transfer_terminated.store(false);
+    m_status_code = StatusCode::NotInit;
+    m_curl_return_code.reset();
+}
+
+auto StreamingReader::read_from_fetched_buffers(
+        size_t num_bytes_to_read,
+        size_t& num_bytes_read,
+        char* dst
+) -> ErrorCode {
+    num_bytes_read = 0;
+    std::optional<BufferView> dst_view{std::nullopt};
+    if (nullptr != dst) {
+        dst_view = BufferView{dst, num_bytes_to_read};
+    }
+    while (0 != num_bytes_to_read) {
+        if (false == m_reading_buffer.has_value()) {
+            if (false == set_reading_buffer()) {
+                return ErrorCode_EndOfFile;
+            }
+        }
+        auto const reading_buffer{m_reading_buffer.value()};
+        auto const reading_buffer_size{reading_buffer.size()};
+        if (0 == reading_buffer_size) {
+            return ErrorCode_EndOfFile;
+        }
+        auto const num_bytes_to_consume_from_buffer{
+                num_bytes_to_read > reading_buffer_size ? reading_buffer_size : num_bytes_to_read
+        };
+        if (dst_view.has_value()) {
+            memcpy(dst_view.value().last(num_bytes_read).data(),
+                   reading_buffer.data(),
+                   num_bytes_to_consume_from_buffer);
+        }
+        num_bytes_to_read -= num_bytes_to_consume_from_buffer;
+        num_bytes_read += num_bytes_to_consume_from_buffer;
+        m_file_pos += num_bytes_to_consume_from_buffer;
+        commit_reading(num_bytes_to_consume_from_buffer);
+    }
+    return ErrorCode_Success;
+}
+}  // namespace clp
---
+++ components/core/src/clp/StreamingReader.hpp
@@ -0,0 +1,403 @@
+#ifndef CLP_S3_STREAMING_READER_HPP
+#define CLP_S3_STREAMING_READER_HPP
+
+#include <curl/curl.h>
+
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <span>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+#include "Defs.h"
+#include "ErrorCode.hpp"
+#include "ReaderInterface.hpp"
+#include "TraceableException.hpp"
+
+namespace clp {
+/**
+ * This class implements a ReaderInterface to stream data directly from a given URL (i.e., streaming
+ * from a pre-signed S3 URL). When open an URL< a daemon thread will be created to fetch data in the
+ * background using libcurl. This class guarantees the synchronization between the read operations
+ * and data streaming (fetching). However, it is assumed that both the reader and the data fetcher
+ * are single threaded.
+ */
+class StreamingReader : public ReaderInterface {
+public:
+    using BufferView = std::span<char>;
+
+    /**
+     * This enum defines the states of the reader.
+     * NotInit: The reader is not initialized with a source URL.
+     * Failed: The reader has failed to stream data from the source URL.
+     * InProgress: The reader is fetching data from the source URL.
+     * Finished: The streaming of the given source URL has been accomplished.
+     */
+    enum class StatusCode : uint8_t {
+        NotInit = 0,
+        Failed,
+        InProgress,
+        Finished
+    };
+
+    /**
+     * These static members defines the default buffer size and the number of pools.
+     */
+    static constexpr size_t cDefaultBufferPoolSize{8};
+    static constexpr size_t cDefaultBufferSize{4096};
+
+    /**
+     * These static members defines the the minimum allowed buffer size and the number of pools.
+     */
+    static constexpr size_t cMinimalBufferSize{512};
+    static constexpr size_t cMinimalBufferPoolSize{2};
+
+    /**
+     * These static members defines the default connection timeout and operation timeout.
+     * Please refer to the libcurl documentation for more details:
+     * ConnectionTimeout: https://curl.se/libcurl/c/CURLOPT_CONNECTTIMEOUT.html
+     * OperationTimeout: https://curl.se/libcurl/c/CURLOPT_TIMEOUT.html
+     */
+    static constexpr uint32_t cDefaultConnectionTimeout{10};
+    static constexpr uint32_t cDefaultOperationTimeout{45};
+
+    /**
+     * This class defines the exception thrown by the streaming reader.
+     */
+    class OperationFailed : public TraceableException {
+    public:
+        OperationFailed(ErrorCode error_code, char const* const filename, int line_number)
+                : TraceableException(error_code, filename, line_number) {}
+
+        [[nodiscard]] auto what() const noexcept -> char const* override {
+            return "StreamingReader operation failed.";
+        }
+    };
+
+    /**
+     * Initializes the underlying libcurl functionalities globally. It should be called before
+     * opening a source URL for data fetching.
+     * @return ErrorCode_Success on success.
+     * @return ErrorCode_Failure if the libcurl initialization failed.
+     */
+    [[nodiscard]] static auto global_init() -> ErrorCode;
+
+    /**
+     * Cleanups the globally initialized curl states.
+     */
+    static auto global_cleanup() -> void;
+
+    /**
+     * Constructor.
+     * @param buffer_pool_size Total number of buffers can be used for fetched
+     * data buffering.
+     * @param buffer_size The size of each data buffer.
+     */
+    StreamingReader(
+            size_t buffer_pool_size = cDefaultBufferPoolSize,
+            size_t buffer_size = cDefaultBufferSize
+    )
+            : m_buffer_pool_size{buffer_pool_size},
+              m_buffer_size{buffer_size} {
+        if (cMinimalBufferSize > m_buffer_size) {
+            m_buffer_size = cMinimalBufferSize;
+        }
+        if (cMinimalBufferPoolSize > m_buffer_pool_size) {
+            m_buffer_pool_size = cMinimalBufferPoolSize;
+        }
+        for (size_t i = 0; i < m_buffer_pool_size; ++i) {
+            m_buffer_pool.emplace_back(std::make_unique<char[]>(m_buffer_size));
+        }
+    }
+
+    /**
+     * Destructor.
+     */
+    ~StreamingReader() {
+        if (StatusCode::NotInit == m_status_code) {
+            return;
+        }
+        // We must ensure the transfer has been terminated before we cleanup the allocated
+        // resources. Otherwise, the transfer thread may trigger UB.
+        terminate_current_transfer();
+    }
+
+    /**
+     * Copy/Move Constructors.
+     * They are all disabled since the synchronization primitives are non-copyable and non-moveable.
+     */
+    StreamingReader(StreamingReader const&) = delete;
+    StreamingReader(StreamingReader&&) = delete;
+    auto operator=(StreamingReader const&) -> StreamingReader& = delete;
+    auto operator=(StreamingReader&&) -> StreamingReader& = delete;
+
+    /**
+     * Opens a new connection to streaming data from the given url with a given offset.
+     * TODO: the current implementation doesn't handle the case when the given offset is out of
+     * range. The file_pos will be set to an invalid state if this happens, which can be
+     * problematic if the other part of the program depends on this position. It can be fixed by
+     * capturing the error code 416 in the response header.
+     * @param src_url
+     * @param offset The offset of the starting byte used for data fetching.
+     * @param disable_caching Whether to disable the caching.
+     */
+    auto open(std::string_view src_url, size_t offset = 0, bool disable_caching = false) -> void;
+
+    /**
+     * Closes the current data transfer and reset the buffer state.
+     */
+    auto close() -> void {
+        if (StatusCode::NotInit == m_status_code) {
+            return;
+        }
+        terminate_current_transfer();
+        reset();
+    }
+
+    /**
+     * Sets the connection timeout in seconds. This value will be used when trying to build the
+     * connections. The default value is set to `cDefaultConnectionTimeout`.
+     * Doc: https://curl.se/libcurl/c/CURLOPT_CONNECTTIMEOUT.html
+     * @param connection_timeout_in_sec
+     */
+    auto set_connection_timeout(uint32_t connection_timeout_in_sec) -> void {
+        m_connection_timeout = connection_timeout_in_sec;
+    }
+
+    /**
+     * Sets the operation timeout in seconds. This values determines the maximum time a transfer
+     * can take. The default value is set to `cDefaultOperationTimeout`.
+     * Doc: https://curl.se/libcurl/c/CURLOPT_TIMEOUT.html
+     * @param operation_timeout_in_sec
+     */
+    auto set_operation_timeout(uint32_t operation_timeout_in_sec) -> void {
+        m_operation_timeout = operation_timeout_in_sec;
+    }
+
+    /**
+     * Tries to read up to a given number of bytes from the buffer.
+     * @param buf
+     * @param num_bytes_to_read
+     * @param num_bytes_read Returns the number of bytes read.
+     * @return ErrorCode_EndOfFile if the buffer doesn't contain any more data.
+     * @return ErrorCode_Success on success.
+     */
+    [[nodiscard]] auto try_read(char* buf, size_t num_bytes_to_read, size_t& num_bytes_read)
+            -> ErrorCode override {
+        return read_from_fetched_buffers(num_bytes_to_read, num_bytes_read, buf);
+    }
+
+    /**
+     * Tries to seek to the given position, relative to the beginning of the buffer.
+     * @param pos
+     * @return ErrorCode_Unsupported if the given position is lower than the current position.
+     * Since this is a streaming reader, it should not seek backward.
+     * @return ErrorCode_OutOfBounds if the given pos is out of bound.
+     * @return ErrorCode_Success on success.
+     */
+    [[nodiscard]] auto try_seek_from_begin(size_t pos) -> ErrorCode override {
+        if (pos < m_file_pos) {
+            return ErrorCode_Unsupported;
+        }
+        if (pos == m_file_pos) {
+            return ErrorCode_Success;
+        }
+        size_t num_bytes_read{};
+        auto const err{read_from_fetched_buffers(pos - m_file_pos, num_bytes_read, nullptr)};
+        if (ErrorCode_EndOfFile == err) {
+            return ErrorCode_OutOfBounds;
+        }
+        if (m_file_pos != pos) {
+            return ErrorCode_Failure;
+        }
+        return err;
+    }
+
+    /**
+     * @param pos Returns the position of the read head in the buffer.
+     * @return ErrorCode_Success.
+     */
+    [[nodiscard]] auto try_get_pos(size_t& pos) -> ErrorCode override {
+        pos = m_file_pos;
+        return ErrorCode_Success;
+    }
+
+    /**
+     * @return Whether the transfer has been killed.
+     */
+    [[nodiscard]] auto is_transfer_aborted() const -> bool { return m_transfer_aborted.load(); }
+
+    /**
+     * @return Whether the transfer has terminated.
+     */
+    [[nodiscard]] auto is_transfer_terminated() const -> bool {
+        return m_transfer_terminated.load();
+    }
+
+    /**
+     * @returns true if the transfer has timed out; false otherwise.
+     */
+    [[nodiscard]] auto is_transfer_timedout() const -> bool {
+        if (false == m_curl_return_code.has_value()) {
+            return false;
+        }
+        auto const val{m_curl_return_code.value()};
+        return (CURLE_FTP_ACCEPT_TIMEOUT == val || CURLE_OPERATION_TIMEDOUT == val);
+    }
+
+private:
+    static constexpr uint32_t cConditionVariableTimeoutMilliSecond{50};
+
+    static bool m_initialized;
+
+    /**
+     * The entry of the transfer thread.
+     * @param reader
+     * @param offset
+     * @param disable_caching
+     */
+    static auto transfer_thread_entry(StreamingReader& reader, size_t offset, bool disable_caching)
+            -> void;
+
+    /**
+     * Progress callback. It should be used to determine whether to abort the current transfer.
+     * Doc: https://curl.se/libcurl/c/CURLOPT_XFERINFOFUNCTION.html
+     * @param ptr A pointer to an instance of StreamingReader.
+     * @param dltotal Unused
+     * @param dlnow Unused
+     * @param ultotal Unused
+     * @param ulnow Unused
+     * @return 0 if the transfer is not aborted; 1 otherwise.
+     */
+    static auto progress_callback(
+            void* ptr,
+            [[maybe_unused]] curl_off_t dltotal,
+            [[maybe_unused]] curl_off_t dlnow,
+            [[maybe_unused]] curl_off_t ultotal,
+            [[maybe_unused]] curl_off_t ulnow
+    ) -> int;
+
+    /**
+     * Write-back callback. This is how the data is written into the fetching buffer.
+     * Doc: https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+     * @param ptr A pointer to an instance of StreamingReader.
+     * @param size Number of bytes of the input data.
+     * @param nmemb
+     * @param reader_ptr A pointer pointing to an instance of StreamingReader.
+     * @return Number of bytes transferred (fetched).
+     */
+    static auto write_callback(char* ptr, size_t size, size_t nmemb, void* reader_ptr) -> size_t;
+
+    /**
+     * Terminates the current transfer. When this function returns, it will ensure that the current
+     * data transfer session has been terminated, and all the daemon threads exit.
+     */
+     auto terminate_current_transfer() -> void;
+
+    /**
+     * Aborts the current on-going data transfer session.
+     */
+    auto abort_data_transfer() -> void;
+
+    /**
+     * Sets the next underlying buffer for fetching transferred data.
+     * @return true on success, false if the transfer has been aborted.
+     */
+    [[nodiscard]] auto set_fetching_buffer() -> bool;
+
+    /**
+     * Commits the current fetching buffer to the fetched buffer queue.
+     */
+    auto commit_fetching_buffer() -> void;
+
+    /**
+     * Sets the next underlying fetched buffer for reading.
+     * @return true if there is a buffer available for reading.
+     * @return false otherwise.
+     */
+    [[nodiscard]] auto set_reading_buffer() -> bool;
+
+    /**
+     * Frees the current reading buffer.
+     */
+    auto free_reading_buffer() -> void;
+
+    /**
+     * Commits the number of bytes fetched. If the buffer is full, commits it to the fetched buffer
+     * queue.
+     * @param num_bytes_fetched
+     */
+    auto commit_fetching(size_t num_bytes_fetched) -> void;
+
+    /**
+     * Commits the number of bytes read. If the entire buffer is consumed, free the buffer resource.
+     * @param num_bytes_read
+     */
+    auto commit_reading(size_t num_bytes_read) -> void;
+
+    /**
+     * Gets the buffer to fetch (which is the unfilled part of the current fetching buffer). If the
+     * current fetching buffer is full, it will commit the current buffer and get the next buffer
+     * from the buffer pool.
+     * @param fetching_buffer Output the buffer to fetch.
+     * @return true on success, false on failure (transfer aborted).
+     */
+    [[nodiscard]] auto get_buffer_to_fetch(BufferView& fetching_buffer) -> bool;
+
+    /**
+     * Resets the buffer states and transfer manager.
+     * Note:
+     * 1. It is assumed that the thread that calls `reset` would be the reader thread that executed
+     *  `open`.
+     * 2. The transfer should already be terminated (either naturally or aborted) before calling
+     *  this thread.
+     */
+    auto reset() -> void;
+
+    /**
+     * Reads data from the fetched buffers with a given amount of bytes.
+     * @param num_bytes_to_read
+     * @param num_bytes_read Returns the number of bytes read.
+     * @param dst The pointer to the destination buffer. If the buffer is not nullptr, copy the data
+     * to the destination.
+     * @return ErrorCode_EndOfFile if the buffer doesn't contain any more data.
+     * @return ErrorCode_Success on success.
+     */
+    [[nodiscard]] auto
+    read_from_fetched_buffers(size_t num_bytes_to_read, size_t& num_bytes_read, char* dst)
+            -> ErrorCode;
+
+    std::string m_src_url;
+    size_t m_file_pos{0};
+
+    size_t m_buffer_pool_size{cDefaultBufferPoolSize};
+    size_t m_buffer_size{cDefaultBufferSize};
+    size_t m_num_fetched_buffer{0};
+    size_t m_curr_fetching_buffer_idx{0};
+    size_t m_fetching_buffer_pos{0};
+
+    uint32_t m_connection_timeout{cDefaultConnectionTimeout};
+    uint32_t m_operation_timeout{cDefaultOperationTimeout};
+
+    std::vector<std::unique_ptr<char[]>> m_buffer_pool;
+    std::queue<BufferView> m_fetched_buffer_queue;
+    std::optional<BufferView> m_fetching_buffer{std::nullopt};
+    std::optional<BufferView> m_reading_buffer{std::nullopt};
+
+    std::mutex m_buffer_resource_mutex;
+    std::condition_variable m_cv_fetcher;
+    std::condition_variable m_cv_reader;
+
+    std::unique_ptr<std::thread> m_transfer_thread{nullptr};
+    std::atomic<bool> m_transfer_aborted{false};
+    std::atomic<bool> m_transfer_terminated{false};
+    StatusCode m_status_code{StatusCode::NotInit};
+    std::optional<CURLcode> m_curl_return_code{std::nullopt};
+};
+}  // namespace clp
+
+#endif
