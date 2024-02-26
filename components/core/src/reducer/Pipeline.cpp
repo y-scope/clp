609ddfb0bbdc5a6f52009c56e1b0a6b4042d2308
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-02-25 20:45:09 -0500

    Refactor Pipeline

--- components/core/src/reducer/Pipeline.cpp
+++ components/core/src/reducer/Pipeline.cpp
@@ -3,48 +3,50 @@
 #include "RecordGroup.hpp"
 
 namespace reducer {
-void Pipeline::push_record_group(GroupTags const& tags, ConstRecordIterator& record_it) {
-    if (m_pipeline.size() > 0) {
-        if (m_input_mode == PipelineInputMode::InterStage) {
-            m_pipeline[0]->push_inter_stage_record_group(tags, record_it);
-        } else /*input_mode == PipelineInputMode::IntraStage*/ {
-            m_pipeline[0]->push_intra_stage_record_group(tags, record_it);
-        }
-    }
-    // else silently drop
-}
-
 void Pipeline::push_record(Record const& record) {
     auto record_it = SingleRecordIterator(record);
     push_record_group(m_empty_group_tags, record_it);
 }
 
-void Pipeline::add_pipeline_stage(std::shared_ptr<Operator> op) {
-    m_pipeline.push_back(op);
-    if (m_pipeline.size() > 1) {
-        m_pipeline[m_pipeline.size() - 2]->set_next_stage(op);
+void Pipeline::push_record_group(GroupTags const& tags, ConstRecordIterator& record_it) {
+    if (m_stages.empty()) {
+        // Silently drop the record group
+        return;
+    }
+
+    if (PipelineInputMode::InterStage == m_input_mode) {
+        m_stages.front()->push_inter_stage_record_group(tags, record_it);
+    } else {  // PipelineInputMode::IntraStage == m_input_mode
+        m_stages.front()->push_intra_stage_record_group(tags, record_it);
+    }
+}
+
+void Pipeline::add_pipeline_stage(std::shared_ptr<Operator> const& op) {
+    m_stages.emplace_back(op);
+    if (m_stages.size() > 1) {
+        m_stages[m_stages.size() - 2]->set_next_stage(op);
     }
 }
 
 std::unique_ptr<RecordGroupIterator> Pipeline::finish() {
-    for (auto op = m_pipeline.begin(); op != m_pipeline.end(); ++op) {
-        (*op)->finish();
+    if (m_stages.empty()) {
+        return std::make_unique<EmptyRecordGroupIterator>();
     }
 
-    if (!m_pipeline.empty()) {
-        return m_pipeline.back()->get_stored_result_iterator();
+    for (auto& m_stage : m_stages) {
+        m_stage->finish();
     }
 
-    return std::make_unique<EmptyRecordGroupIterator>();
+    return m_stages.back()->get_stored_result_iterator();
 }
 
 std::unique_ptr<RecordGroupIterator> Pipeline::finish(std::set<GroupTags> const& filtered_tags) {
-    // FIXME: assumes no need to push results between stages, but we will change the
-    // programming model to eliminate the possibility of flushing between stages at the end later.
-    if (!m_pipeline.empty()) {
-        return m_pipeline.back()->get_stored_result_iterator(filtered_tags);
+    if (m_stages.empty()) {
+        return std::make_unique<EmptyRecordGroupIterator>();
     }
 
-    return std::make_unique<EmptyRecordGroupIterator>();
+    // TODO: This assumes there's no need to push results between stages; we'll change the
+    // programming model to eliminate the possibility of flushing between stages later.
+    return m_stages.back()->get_stored_result_iterator(filtered_tags);
 }
 }  // namespace reducer
--- components/core/src/reducer/Pipeline.hpp
+++ components/core/src/reducer/Pipeline.hpp
@@ -17,8 +17,7 @@ enum class PipelineInputMode : uint8_t {
 };
 
 /**
- * Class describing an in memory aggregation pipeline consisting of a chained set of Operator
- * objects.
+ * An in-memory aggregation pipeline consisting of set of Operator objects chained together.
  */
 class Pipeline {
 public:
@@ -27,13 +26,13 @@ public:
     void push_record(Record const& record);
     void push_record_group(GroupTags const& tags, ConstRecordIterator& record_it);
 
-    void add_pipeline_stage(std::shared_ptr<Operator> op);
+    void add_pipeline_stage(std::shared_ptr<Operator> const& op);
 
     std::unique_ptr<RecordGroupIterator> finish();
     std::unique_ptr<RecordGroupIterator> finish(std::set<GroupTags> const& filtered_tags);
 
 private:
-    std::vector<std::shared_ptr<Operator>> m_pipeline;
+    std::vector<std::shared_ptr<Operator>> m_stages;
     PipelineInputMode m_input_mode;
     GroupTags m_empty_group_tags;
 };
