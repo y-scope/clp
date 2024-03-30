5bfefc4f863396d0d499fd04a264710574d5ce90
Junhao Liao <junhao@junhao.ca>
2024-03-29 23:27:57 -0400

    Correctly pass `pkg-config` found libraries to find library dependencies marco; Update internal variable naming in Findspdlog.cmake.

--- components/core/cmake/Modules/Findspdlog.cmake
+++ components/core/cmake/Modules/Findspdlog.cmake
@@ -8,29 +8,30 @@
 #  spdlog_VERSION - The version of spdlog installed on the system
 #
 # Conventions:
-# - Variables only for use within the script are prefixed with "spdlog_"
+# - Variables only for use within the script are prefixed with "SPDLOG_"
 # - Variables that should be externally visible are prefixed with "spdlog_"
 
-set(spdlog_LIBNAME "spdlog")
+set(SPDLOG_LIBNAME "spdlog")
+set(SPDLOG_TARGET_NAME "spdlog::spdlog")
 
 include(cmake/Modules/FindLibraryDependencies.cmake)
 
 # Run pkg-config
 find_package(PkgConfig)
-pkg_check_modules(spdlog_PKGCONF QUIET ${spdlog_LIBNAME})
+pkg_check_modules(SPDLOG_PKGCONF QUIET ${SPDLOG_LIBNAME})
 
 # Set include directory
 find_path(
-        spdlog_INCLUDE_DIR
-        spdlog.h
-        HINTS ${spdlog_PKGCONF_INCLUDEDIR}
-        PATH_SUFFIXES spdlog
+    spdlog_INCLUDE_DIR
+    spdlog.h
+    HINTS ${SPDLOG_PKGCONF_INCLUDEDIR}
+    PATH_SUFFIXES spdlog
 )
 
 # Handle static libraries
-if(spdlog_USE_STATIC_LIBS)
+if(SPDLOG_USE_STATIC_LIBS)
     # Save current value of CMAKE_FIND_LIBRARY_SUFFIXES
-    set(spdlog_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+    set(SPDLOG_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
 
     # Temporarily change CMAKE_FIND_LIBRARY_SUFFIXES to static library suffix
     set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
@@ -38,48 +39,55 @@ endif()
 
 # Find library
 find_library(
-        spdlog_LIBRARY
-        NAMES ${spdlog_LIBNAME}
-        HINTS ${spdlog_PKGCONF_LIBDIR}
-        PATH_SUFFIXES lib
+    SPDLOG_LIBRARY
+    NAMES ${SPDLOG_LIBNAME}
+    HINTS ${SPDLOG_PKGCONF_LIBDIR}
+    PATH_SUFFIXES lib
 )
-if (spdlog_LIBRARY)
+if (SPDLOG_LIBRARY)
     # NOTE: This must be set for find_package_handle_standard_args to work
     set(spdlog_FOUND ON)
 endif()
 
-if(spdlog_USE_STATIC_LIBS)
-    FindStaticLibraryDependencies(${spdlog_LIBNAME} spdlog "${spdlog_PKGCONF_STATIC_LIBRARIES}")
+if(SPDLOG_USE_STATIC_LIBS)
+    FindStaticLibraryDependencies(${SPDLOG_LIBNAME} SPDLOG "${SPDLOG_PKGCONF_STATIC_LIBRARIES}")
 
     # Restore original value of CMAKE_FIND_LIBRARY_SUFFIXES
-    set(CMAKE_FIND_LIBRARY_SUFFIXES ${spdlog_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
-    unset(spdlog_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES)
-else()
-    # Add dependencies manually since spdlog's pkgconfig doesn't include it
-    list(APPEND spdlog_DYNAMIC_LIBS "fmt")
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${SPDLOG_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
+    unset(SPDLOG_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES)
 endif()
 
-list(APPEND spdlog_DYNAMIC_LIBS "pthread")
-FindDynamicLibraryDependencies(spdlog "${spdlog_DYNAMIC_LIBS}")
+FindDynamicLibraryDependencies(SPDLOG "${SPDLOG_PKGCONF_LIBRARIES}")
 
 # Set version
-set(spdlog_VERSION ${spdlog_PKGCONF_VERSION})
+set(spdlog_VERSION ${SPDLOG_PKGCONF_VERSION})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-        spdlog
-        REQUIRED_VARS spdlog_INCLUDE_DIR
-        VERSION_VAR spdlog_VERSION
+    ${SPDLOG_LIBNAME}
+    REQUIRED_VARS spdlog_INCLUDE_DIR
+    VERSION_VAR spdlog_VERSION
 )
 
-if(NOT TARGET spdlog::spdlog)
+if(NOT TARGET ${SPDLOG_TARGET_NAME})
     # Add library to build
     if (spdlog_FOUND)
-        if (spdlog_USE_STATIC_LIBS)
-            add_library(spdlog::spdlog STATIC IMPORTED)
+        if (SPDLOG_USE_STATIC_LIBS)
+            add_library(${SPDLOG_TARGET_NAME} STATIC IMPORTED)
+            set_target_properties(
+                ${SPDLOG_TARGET_NAME}
+                PROPERTIES
+                COMPILE_FLAGS "${SPDLOG_PKGCONF_CFLAGS}"
+            )
         else()
-            # NOTE: spdlog is only available as a static library or a dynamic one.
-            add_library(spdlog::spdlog SHARED IMPORTED)
+            # NOTE: We use UNKNOWN so that if the user doesn't have the SHARED
+            # libraries installed, we can still use the STATIC libraries.
+            add_library(${SPDLOG_TARGET_NAME} UNKNOWN IMPORTED)
+            set_target_properties(
+                ${SPDLOG_TARGET_NAME}
+                PROPERTIES
+                COMPILE_FLAGS "${SPDLOG_PKGCONF_STATIC_CFLAGS}"
+            )
         endif()
     endif()
 
@@ -88,29 +96,29 @@ if(NOT TARGET spdlog::spdlog)
         set(spdlog_FOUND OFF)
     else()
         set_target_properties(
-                spdlog::spdlog
-                PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES "${spdlog_INCLUDE_DIR}"
+            ${SPDLOG_TARGET_NAME}
+            PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${spdlog_INCLUDE_DIR}"
         )
     endif()
 
     # Set location of library
-    if(NOT EXISTS "${spdlog_LIBRARY}")
+    if(NOT EXISTS "${SPDLOG_LIBRARY}")
         set(spdlog_FOUND OFF)
     else()
         set_target_properties(
-                spdlog::spdlog
+                ${SPDLOG_TARGET_NAME}
                 PROPERTIES
                 IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                IMPORTED_LOCATION "${spdlog_LIBRARY}"
+                IMPORTED_LOCATION "${SPDLOG_LIBRARY}"
         )
 
         # Add component's dependencies for linking
-        if(spdlog_LIBRARY_DEPENDENCIES)
+        if(SPDLOG_LIBRARY_DEPENDENCIES)
             set_target_properties(
-                    spdlog::spdlog
+                    ${SPDLOG_TARGET_NAME}
                     PROPERTIES
-                    INTERFACE_LINK_LIBRARIES "${spdlog_LIBRARY_DEPENDENCIES}"
+                    INTERFACE_LINK_LIBRARIES "${SPDLOG_LIBRARY_DEPENDENCIES}"
             )
         endif()
     endif()
