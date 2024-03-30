0256c9129e58f5bad4ee8ffb05b2080ba01bc824
Junhao Liao <junhao@junhao.ca>
2024-03-29 23:39:59 -0400

    Remove command `dpkg -l libspdlog-dev` in `spdlog.sh` as we do not need dpkg info.

--- components/core/tools/scripts/lib_install/spdlog.sh
+++ components/core/tools/scripts/lib_install/spdlog.sh
@@ -16,6 +16,7 @@ if [ "$#" -lt 1 ] ; then
 fi
 version=$1
 
+lib_name=spdlog
 package_name=libspdlog-dev
 temp_dir=/tmp/${package_name}-installation
 deb_output_dir=${temp_dir}
@@ -29,18 +30,16 @@ fi
 
 # Check if already installed
 set +e
-pkg-config --exact-version="${version}" "spdlog"
+pkg-config --exact-version="${version}" "${lib_name}"
 pkg_found=$?
 if [ $pkg_found -eq 0 ] ; then
   find /usr/lib/ /usr/local/lib/ -name "libspdlog.a" | grep -q "."
   static_lib_found=$?
 fi
-if [ $static_lib_found -eq 0 ] ; then
-  dpkg -l ${package_name} | grep ${version}
-fi
-installed=$(($? | pkg_found | static_lib_found))
+installed=$((pkg_found | static_lib_found))
 set -e
 if [ $installed -eq 0 ] ; then
+  echo "Found library ${lib_name}=${version} ."
   # Nothing to do
   exit
 fi
