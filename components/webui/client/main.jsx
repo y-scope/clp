e6122d441cf5e8fdf1b07993798a4e6b1bddaf5e
Junhao Liao <junhao@junhao.ca>
2024-04-05 02:58:10 -0400

    Revert changes in `main.jsx`.

--- components/webui/client/main.jsx
+++ components/webui/client/main.jsx
@@ -1,9 +1,9 @@
-import {Meteor} from "meteor/meteor";
 import React from "react";
-import {render} from "react-dom";
-import {Router} from "react-router";
 
 import {createBrowserHistory} from "history";
+import {Meteor} from "meteor/meteor";
+import {render} from "react-dom";
+import {Router, Switch} from "react-router";
 
 import {App} from "/imports/ui/App.jsx";
 
