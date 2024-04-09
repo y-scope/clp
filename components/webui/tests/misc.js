d7b7ac22e54addcebf414e3d70932e04acbddced
Junhao Liao <junhao@junhao.ca>
2024-04-09 19:12:52 -0400

    Reformat/Refactor tests.

--- components/webui/tests/misc.js
+++ components/webui/tests/misc.js
@@ -2,35 +2,41 @@ import assert from "assert";
 
 import {unquoteString} from "/imports/utils/misc";
 
-describe("misc", function () {
-    it("unquoteString", function () {
+
+describe("misc", () => {
+    it("unquoteString", () => {
         // Empty string
-        assert.strictEqual(unquoteString("", '"', '\\'), "");
+        assert.strictEqual(unquoteString("", '"', "\\"), "");
+
         // Unquoted string
-        assert.strictEqual(unquoteString("abc", '"', '\\'), "abc");
+        assert.strictEqual(unquoteString("abc", '"', "\\"), "abc");
+
         // Double-quoted string
-        assert.strictEqual(unquoteString("\"abc\"", '"', '\\'), "abc");
+        assert.strictEqual(unquoteString("\"abc\"", '"', "\\"), "abc");
+
         // Single-quoted string
-        assert.strictEqual(unquoteString("'abc'", "'", '\\'), "abc");
+        assert.strictEqual(unquoteString("'abc'", "'", "\\"), "abc");
+
         // With escaped characters
-        assert.strictEqual(unquoteString("a\\\\b\\\"c\\*\\?", '"', '\\'), "a\\\\b\"c\\*\\?");
+        assert.strictEqual(unquoteString("a\\\\b\\\"c\\*\\?", '"', "\\"), "a\\\\b\"c\\*\\?");
+
         // Double-quoted with escaped characters
-        assert.strictEqual(unquoteString("\"a\\\\b\\\"c\\*\\?\"", '"', '\\'), "a\\\\b\"c\\*\\?");
+        assert.strictEqual(unquoteString("\"a\\\\b\\\"c\\*\\?\"", '"', "\\"), "a\\\\b\"c\\*\\?");
 
         // With one of the quotes missing
         assert.throws(() => {
-            unquoteString("\"abc", '"', '\\');
+            unquoteString("\"abc", '"', "\\");
         }, Error);
         assert.throws(() => {
-            unquoteString("abc\"", '"', '\\');
+            unquoteString("abc\"", '"', "\\");
         }, Error);
 
         // With an unescaped quote in the middle
         assert.throws(() => {
-            unquoteString("ab\"c", '"', '\\');
+            unquoteString("ab\"c", '"', "\\");
         }, Error);
         assert.throws(() => {
-            unquoteString("\"ab\"c\"", '"', '\\');
+            unquoteString("\"ab\"c\"", '"', "\\");
         }, Error);
     });
 });
