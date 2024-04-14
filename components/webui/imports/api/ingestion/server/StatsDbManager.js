c9921252fff3324640e87da3ba02072d27a832b7
Junhao Liao <junhao@junhao.ca>
2024-04-14 04:56:00 -0400

    Move CompressionStats typedef into a both client and server accessible location.

--- components/webui/imports/api/ingestion/server/StatsDbManager.js
+++ components/webui/imports/api/ingestion/server/StatsDbManager.js
@@ -47,16 +47,6 @@ class StatsDbManager {
     }
 
     /**
-     * @typedef {object} CompressionStats
-     * @property {number|null} begin_timestamp
-     * @property {number|null} end_timestamp
-     * @property {number|null} total_uncompressed_size
-     * @property {number|null} total_compressed_size
-     * @property {number|null} num_files
-     * @property {number|null} num_messages
-     */
-
-    /**
      * Queries compression stats.
      *
      * @return {Promise<CompressionStats>}
