dc56550b4c0497f57bd7919f6dbadc4c9a10c45a
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-05-24 20:38:46 +0000

    small fix

--- components/core/src/clp/streaming_archive/writer/utils.cpp
+++ components/core/src/clp/streaming_archive/writer/utils.cpp
@@ -25,14 +25,14 @@ auto split_file(
         Archive& archive_writer
 ) -> void {
     auto const& encoded_file = archive_writer.get_file();
-    auto has_ts_pattern = encoded_file.has_ts_pattern();
-    auto orig_file_id = encoded_file.get_orig_file_id();
-    auto split_ix = encoded_file.get_split_ix();
+    auto const has_ts_pattern = encoded_file.has_ts_pattern();
+    auto const orig_file_id = encoded_file.get_orig_file_id();
     auto const end_msg_idx = encoded_file.get_end_msg_idx();
+    auto const split_ix = encoded_file.get_split_ix();
     archive_writer.set_file_is_split(true);
     close_file_and_append_to_segment(archive_writer);
 
-    archive_writer.create_and_open_file(path_for_compression, group_id, orig_file_id, ++split_ix);
+    archive_writer.create_and_open_file(path_for_compression, group_id, orig_file_id, split_ix + 1);
     if (has_ts_pattern) {
         // Initialize the file's timestamp pattern to the previous split's pattern
         archive_writer.change_ts_pattern(last_timestamp_pattern);
@@ -57,7 +57,7 @@ auto split_file_and_archive(
 
     split_archive(archive_user_config, archive_writer);
 
-    archive_writer.create_and_open_file(path_for_compression, group_id, orig_file_id, ++split_ix);
+    archive_writer.create_and_open_file(path_for_compression, group_id, orig_file_id, split_ix + 1);
     if (has_ts_pattern) {
         // Initialize the file's timestamp pattern to the previous split's pattern
         archive_writer.change_ts_pattern(last_timestamp_pattern);
