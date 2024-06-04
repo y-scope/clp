47f8dfb262d46d1695f7fda8fdcfe2b39dd672f1
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-06-04 16:57:18 +0000

    Remove unnecessary import

--- components/core/src/clp/ir/Constant.hpp
+++ components/core/src/clp/ir/Constant.hpp
@@ -1,10 +1,6 @@
 #ifndef CLP_IR_CONSTANTS_HPP
 #define CLP_IR_CONSTANTS_HPP
 
-#include <string_view>
-
-#include "../Defs.h"
-
 namespace clp::ir {
 constexpr char cIrExtension[] = "clp.zst";
 }  // namespace clp::ir
