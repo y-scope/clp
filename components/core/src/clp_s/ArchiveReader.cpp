e8d82188268a5866cf488e025f5b45ab33b44dc7
gibber9809 <devinbook1@gmail.com>
2024-06-06 21:12:38 +0000

    Fix lint

--- components/core/src/clp_s/ArchiveReader.cpp
+++ components/core/src/clp_s/ArchiveReader.cpp
@@ -225,7 +225,7 @@ SchemaReader& ArchiveReader::create_schema_reader(
             );
             i += length;
             continue;
-        } 
+        }
         if (i >= schema.get_num_ordered()) {
             // Length one unordered object that doesn't have a tag. This is only allowed when the
             // column id is the root of the unordered object, so we can pass it directly to
