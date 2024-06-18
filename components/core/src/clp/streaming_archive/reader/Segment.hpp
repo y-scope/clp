4901cbdf1c0d5f94878af8db31f9cf10b1ddf9aa
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-06-18 01:05:32 -0400

    Formatting

--- components/core/src/clp/streaming_archive/reader/Segment.hpp
+++ components/core/src/clp/streaming_archive/reader/Segment.hpp
@@ -20,7 +20,7 @@ namespace clp::streaming_archive::reader {
 class Segment {
 public:
     // Constructor
-    Segment() : m_segment_path({}){};
+    Segment() : m_segment_path({}) {};
 
     // Destructor
     ~Segment();
