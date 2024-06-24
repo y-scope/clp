191c4ff2bb2967361d37e9f0b40e75bd78cae4c5
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-06-24 19:01:47 -0400

    Refactoring according to the code review comments

--- components/core/src/clp/ffi/utils.cpp
+++ components/core/src/clp/ffi/utils.cpp
@@ -8,122 +8,131 @@
 #include <string>
 #include <string_view>
 #include <tuple>
-#include <utility>
 
 using std::string;
 using std::string_view;
 
 namespace clp::ffi {
-namespace {
-/*
- * @param byte
- * @return Whether the input byte is a valid utf8 continuation byte. A valid utf8 continuation byte
- * should match 0b10xx_xxxx.
- */
-[[nodiscard]] auto is_valid_utf8_continuation_byte(uint8_t byte) -> bool;
+auto validate_and_escape_utf8_string(string_view raw) -> std::optional<string> {
+    string_view::const_iterator bookmark{raw.cbegin()};
+    string escaped;
+    escaped.reserve(raw.size() + (raw.size() / 2));
 
-/**
- * Appends a single-byte utf8 character into the given string, and escapes it if necessary.
- * @param character Single-byte utf8 character.
- * @parma escaped_string Input string where the character(s) are appended to.
- */
-auto escape_and_append_single_byte_utf8_char(uint8_t character, string& escaped_string) -> void;
+    auto escape_handler = [&](string_view::const_iterator it) -> void {
+        // Allocate 6 + 1 size buffer to format control characters as "\u00bb", with the last byte
+        // used by `snprintf` to append '\0'
+        constexpr size_t cControlCharacterBufSize{7};
+        std::array<char, cControlCharacterBufSize> buf{};
+        std::string_view escaped_content;
+        bool escape_required{true};
+        switch (*it) {
+            case '\b':
+                escaped_content = "\\b";
+                break;
+            case '\t':
+                escaped_content = "\\t";
+                break;
+            case '\n':
+                escaped_content = "\\n";
+                break;
+            case '\f':
+                escaped_content = "\\f";
+                break;
+            case '\r':
+                escaped_content = "\\r";
+                break;
+            case '\\':
+                escaped_content = "\\\\";
+                break;
+            case '"':
+                escaped_content = "\\\"";
+                break;
+            default: {
+                constexpr uint8_t cLargestControlCharacter{0x1F};
+                auto const byte{static_cast<uint8_t>(*it)};
+                if (cLargestControlCharacter >= byte) {
+                    std::ignore = snprintf(buf.data(), buf.size(), "\\u00%02x", byte);
+                    escaped_content = {buf.data(), buf.size() - 1};
+                } else {
+                    escape_required = false;
+                }
+                break;
+            }
+        }
+        if (escape_required) {
+            escaped.append(bookmark, it);
+            escaped.append(escaped_content.cbegin(), escaped_content.cend());
+            bookmark = it + 1;
+        }
+    };
 
-/**
- * Validates whether the given code point is a valid UTF8 encoding with the given length.
- * The valid range is defined as following:
- * .---------------------------------------------.
- * | Length | First Code Point | Last Code Point |
- * |--------|------------------|-----------------|
- * | 1 Byte | 0x00             | 0x7F            |
- * | 2 Byte | 0x80             | 0x7FF           |
- * | 3 Byte | 0x8FF            | 0xFFFF          |
- * | 4 Byte | 0x10000          | 0x10FFFF        |
- * |--------|------------------|-----------------|
- * @param code_point
- * @param encoding_length
- * @return Whether the code point is a valid encoding.
- */
-[[nodiscard]] auto is_valid_code_point(uint32_t code_point, size_t encoding_length) -> bool;
+    if (false == generic_validate_utf8_string(raw, escape_handler)) {
+        return std::nullopt;
+    }
 
-/**
- * Updates the code point by applying the payload of the given continuation byte.
- * @param code_point
- * @param continuation_byte
- * @return Updated code point.
- */
-[[nodiscard]] auto update_code_point(uint32_t code_point, uint8_t continuation_byte) -> uint32_t;
+    if (raw.cend() != bookmark) {
+        escaped.append(bookmark, raw.cend());
+    }
+
+    return escaped;
+}
+
+auto is_utf8_encoded(string_view str) -> bool {
+    auto escape_handler = []([[maybe_unused]] string_view::const_iterator it) -> void {};
+    return generic_validate_utf8_string(str, escape_handler);
+}
+
+namespace utils_hpp {
+auto validate_header_byte_and_set_code_point(
+        uint8_t header,
+        size_t& num_continuation_bytes,
+        uint32_t& code_point,
+        uint32_t& code_point_lower_bound,
+        uint32_t& code_point_upper_bound
+) -> bool {
+    constexpr uint8_t cThreeByteContinuationMask{0xF8};  // 0b1111_1xxx
+    constexpr uint8_t cValidThreeByteContinuation{0xF0};  // 0b1111_0xxx
+    constexpr uint8_t cTwoByteContinuationMask{0xF0};  // 0b1111_xxxx
+    constexpr uint8_t cValidTwoByteContinuation{0xE0};  // 0b1110_xxxx
+    constexpr uint8_t cOneByteContinuationMask{0xE0};  // 0b111x_xxxx
+    constexpr uint8_t cValidOneByteContinuation{0xC0};  // 0b110x_xxxx
+
+    if ((header & cThreeByteContinuationMask) == cValidThreeByteContinuation) {
+        num_continuation_bytes = 3;
+        code_point = (~cThreeByteContinuationMask & header);
+        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        code_point_lower_bound = 0x1'0000;
+        code_point_upper_bound = 0x10'FFFF;
+        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    } else if ((header & cTwoByteContinuationMask) == cValidTwoByteContinuation) {
+        num_continuation_bytes = 2;
+        code_point = (~cTwoByteContinuationMask & header);
+        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        code_point_lower_bound = 0x800;
+        code_point_upper_bound = 0xFFFF;
+        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    } else if ((header & cOneByteContinuationMask) == cValidOneByteContinuation) {
+        num_continuation_bytes = 1;
+        code_point = (~cOneByteContinuationMask & header);
+        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        code_point_lower_bound = 0x80;
+        code_point_upper_bound = 0x7FF;
+        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    } else {
+        return false;
+    }
+    return true;
+}
+
+auto is_ascii_char(uint8_t byte) -> bool {
+    constexpr uint8_t cLargestValidASCIIChar{0x7F};
+    return cLargestValidASCIIChar >= byte;
+}
 
 auto is_valid_utf8_continuation_byte(uint8_t byte) -> bool {
     constexpr uint8_t cContinuationByteMask{0xC0};
     constexpr uint8_t cValidMaskedContinuationByte{0x80};
     return (byte & cContinuationByteMask) == cValidMaskedContinuationByte;
-}
-
-auto escape_and_append_single_byte_utf8_char(uint8_t character, string& escaped_string) -> void {
-    switch (character) {
-        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        case 0x08:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('b');
-            break;
-        case 0x09:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('t');
-            break;
-        case 0x0A:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('n');
-            break;
-        case 0x0C:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('f');
-            break;
-        case 0x0D:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('r');
-            break;
-        case 0x22:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('\"');
-            break;
-        case 0x5C:
-            escaped_string.push_back('\\');
-            escaped_string.push_back('\\');
-            break;
-        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        default: {
-            constexpr uint8_t cControlCharacter{0x1F};
-            if (cControlCharacter >= character) {
-                // Allocate 6 + 1 size buffer to format control characters as "\u00bb", with the
-                // last byte used by `snprintf` to append '\0'
-                constexpr size_t cControlCharacterBufSize{7};
-                std::array<char, cControlCharacterBufSize> buf{};
-                std::ignore = snprintf(buf.data(), buf.size(), "\\u00%02x", character);
-                escaped_string.append(buf.cbegin(), buf.cend() - 1);
-            } else {
-                escaped_string.push_back(static_cast<char>(character));
-            }
-            break;
-        }
-    }
-}
-
-auto is_valid_code_point(uint32_t code_point, size_t encoding_length) -> bool {
-    switch (encoding_length) {
-        // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        case 1:
-            return code_point <= 0x7F;
-        case 2:
-            return (0x80 <= code_point && code_point <= 0x7FF);
-        case 3:
-            return (0x800 <= code_point && code_point <= 0xFFFF);
-        case 4:
-            return (0x1'0000 <= code_point && code_point <= 0x10'FFFF);
-        // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-        default:
-            return false;
-    }
 }
 
 auto update_code_point(uint32_t code_point, uint8_t continuation_byte) -> uint32_t {
@@ -132,99 +141,6 @@ auto update_code_point(uint32_t code_point, uint8_t continuation_byte) -> uint32
     return (code_point << cNumContinuationBytePayloadBits)
            + (continuation_byte & cContinuationBytePayloadMask);
 }
-}  // namespace
+}  // namespace utils_hpp
 
-auto validate_and_escape_utf8_string(string_view raw) -> std::optional<string> {
-    string_view::const_iterator bookmark_it{};
-    size_t encoding_length{};
-    enum class State : uint8_t {
-        HeadByteToValidate = 0,
-        OneContinuationByteToValidate,
-        TwoContinuationBytesToValidate,
-        ThreeContinuationBytesToValidate
-    };
-    State state{State::HeadByteToValidate};
-    string escaped;
-    escaped.reserve(raw.size() + (raw.size() >> 2));
-
-    uint32_t code_point{};
-    auto validate_encoding_length_and_set_state = [&](uint8_t byte) -> bool {
-        constexpr uint8_t cThreeByteContinuationMask{0xF8};  // 0b1111_1xxx
-        constexpr uint8_t cValidThreeByteContinuation{0xF0};  // 0b1111_0xxx
-        constexpr uint8_t cTwoByteContinuationMask{0xF0};  // 0b1111_xxxx
-        constexpr uint8_t cValidTwoByteContinuation{0xE0};  // 0b1110_xxxx
-        constexpr uint8_t cOneByteContinuationMask{0xE0};  // 0b111x_xxxx
-        constexpr uint8_t cValidOneByteContinuation{0xC0};  // 0b110x_xxxx
-        if ((byte & cThreeByteContinuationMask) == cValidThreeByteContinuation) {
-            encoding_length = 4;
-            code_point = (~cThreeByteContinuationMask & byte);
-            state = State::ThreeContinuationBytesToValidate;
-        } else if ((byte & cTwoByteContinuationMask) == cValidTwoByteContinuation) {
-            encoding_length = 3;
-            code_point = (~cTwoByteContinuationMask & byte);
-            state = State::TwoContinuationBytesToValidate;
-        } else if ((byte & cOneByteContinuationMask) == cValidOneByteContinuation) {
-            encoding_length = 2;
-            code_point = (~cOneByteContinuationMask & byte);
-            state = State::OneContinuationByteToValidate;
-        } else {
-            return false;
-        }
-        return true;
-    };
-
-    // For multibyte encoded values, we will incrementally build the code point, and validate its
-    // range in the end.
-    for (string_view::const_iterator it{raw.cbegin()}; it != raw.cend(); ++it) {
-        auto const byte{static_cast<uint8_t>(*it)};
-        switch (state) {
-            case State::HeadByteToValidate: {
-                if (is_valid_code_point(static_cast<uint32_t>(byte), 1)) {
-                    escape_and_append_single_byte_utf8_char(byte, escaped);
-                } else {
-                    if (false == validate_encoding_length_and_set_state(byte)) {
-                        return std::nullopt;
-                    }
-                    bookmark_it = it;
-                }
-                break;
-            }
-            case State::OneContinuationByteToValidate:
-                if (false == is_valid_utf8_continuation_byte(byte)) {
-                    return std::nullopt;
-                }
-                code_point = update_code_point(code_point, byte);
-
-                if (false == is_valid_code_point(code_point, encoding_length)) {
-                    return std::nullopt;
-                }
-                escaped.append(bookmark_it, bookmark_it + encoding_length);
-                state = State::HeadByteToValidate;
-                break;
-            case State::TwoContinuationBytesToValidate:
-                if (false == is_valid_utf8_continuation_byte(byte)) {
-                    return std::nullopt;
-                }
-                code_point = update_code_point(code_point, byte);
-                state = State::OneContinuationByteToValidate;
-                break;
-            case State::ThreeContinuationBytesToValidate:
-                if (false == is_valid_utf8_continuation_byte(byte)) {
-                    return std::nullopt;
-                }
-                code_point = update_code_point(code_point, byte);
-                state = State::TwoContinuationBytesToValidate;
-                break;
-            default:
-                return std::nullopt;
-        }
-    }
-
-    if (State::HeadByteToValidate != state) {
-        // Incomplete multibyte UTF8 sequence
-        return std::nullopt;
-    }
-
-    return std::move(escaped);
-}
 }  // namespace clp::ffi
--- components/core/src/clp/ffi/utils.hpp
+++ components/core/src/clp/ffi/utils.hpp
@@ -1,13 +1,15 @@
 #ifndef CLP_FFI_UTILS_HPP
 #define CLP_FFI_UTILS_HPP
 
+#include <cstddef>
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <string_view>
 
 namespace clp::ffi {
 /**
- * Validates whether the given string is UTF8 encoded, and escapes any characters to generate to
+ * Validates whether the given string is UTF-8 encoded, and escapes any characters to generate to
  * make the string human readable.
  * @param raw The raw string to escape.
  * @return The escaped string on success.
@@ -15,6 +17,125 @@ namespace clp::ffi {
  */
 [[nodiscard]] auto validate_and_escape_utf8_string(std::string_view raw
 ) -> std::optional<std::string>;
+
+/**
+ * @param str
+ * @return Whether the input is a valid UTF-8 encoded string.
+ */
+[[nodiscard]] auto is_utf8_encoded(std::string_view str) -> bool;
+
+/**
+ * Validates whether the given string is UTF-8 encoded, optionally escaping ASCII characters using
+ * the given handler.
+ * @tparam EscapeHandler Method to optionally escape any ASCII character in the string. Signature:
+ * (std::string_view::const_iterator it_ascii_char) -> void
+ * @param src
+ * @param escape_handler
+ * @return Whether the input is a valid UTF-8 encoded string.
+ */
+template <typename EscapeHandler>
+[[nodiscard]] auto
+generic_validate_utf8_string(std::string_view src, EscapeHandler escape_handler) -> bool;
+
+namespace utils_hpp {
+/**
+ * Validates whether the given byte is a valid UTF-8 header with continuation bytes, and set code
+ * point and code point range accordingly.
+ * The valid code point range is defined as following:
+ * .----------------------------------------------------------.
+ * | Continuation Length | First Code Point | Last Code Point |
+ * |---------------------|------------------|-----------------|
+ * | 1 Byte              | 0x80             | 0x7FF           |
+ * | 2 Byte              | 0x800            | 0xFFFF          |
+ * | 3 Byte              | 0x10000          | 0x10FFFF        |
+ * |---------------------|------------------|-----------------|
+ * @param header Input byte to validate
+ * @param num_continuation_bytes Outputs the number of continuation bytes corresponded to the header
+ * byte, if the header is valid.
+ * @param code_point Outputs the code extracted from the header byte, if the header is valid.
+ * @param code_point_lower_bound Outputs the lower bound of the valid code point range corresponded
+ * with the header byte, if the header if valid.
+ * @param code_point_upper_bound Outputs the upper bound of the valid code point range corresponded
+ * with the header byte, if the header if valid.
+ * @return Whether the input byte is a valid header byte.
+ */
+[[nodiscard]] auto validate_header_byte_and_set_code_point(
+        uint8_t header,
+        size_t& num_continuation_bytes,
+        uint32_t& code_point,
+        uint32_t& code_point_lower_bound,
+        uint32_t& code_point_upper_bound
+) -> bool;
+
+/**
+ * @param byte
+ * @return Whether the given byte is a valid ASCII character.
+ */
+[[nodiscard]] auto is_ascii_char(uint8_t byte) -> bool;
+
+/*
+ * @param byte
+ * @return Whether the input byte is a valid UTF-8 continuation byte. A valid UTF-8 continuation
+ * byte should match 0b10xx_xxxx.
+ */
+[[nodiscard]] auto is_valid_utf8_continuation_byte(uint8_t byte) -> bool;
+
+/**
+ * Updates the code point by applying the payload of the given continuation byte.
+ * @param code_point
+ * @param continuation_byte
+ * @return Updated code point.
+ */
+[[nodiscard]] auto update_code_point(uint32_t code_point, uint8_t continuation_byte) -> uint32_t;
+}  // namespace utils_hpp
+
+template <typename EscapeHandler>
+auto generic_validate_utf8_string(std::string_view src, EscapeHandler escape_handler) -> bool {
+    size_t num_continuation_bytes_to_validate{0};
+    uint32_t code_point{};
+    uint32_t code_point_lower_bound{};
+    uint32_t code_point_upper_bound{};
+
+    for (std::string_view::const_iterator it{src.cbegin()}; it != src.cend(); ++it) {
+        auto const byte{static_cast<uint8_t>(*it)};
+        if (0 == num_continuation_bytes_to_validate) {
+            if (utils_hpp::is_ascii_char(byte)) {
+                escape_handler(it);
+            } else {
+                if (false
+                    == utils_hpp::validate_header_byte_and_set_code_point(
+                            byte,
+                            num_continuation_bytes_to_validate,
+                            code_point,
+                            code_point_lower_bound,
+                            code_point_upper_bound
+                    ))
+                {
+                    return false;
+                }
+            }
+        } else {
+            if (false == utils_hpp::is_valid_utf8_continuation_byte(byte)) {
+                return false;
+            }
+            code_point = utils_hpp::update_code_point(code_point, byte);
+            --num_continuation_bytes_to_validate;
+            if (0 != num_continuation_bytes_to_validate) {
+                continue;
+            }
+            if (code_point < code_point_lower_bound || code_point_upper_bound < code_point) {
+                return false;
+            }
+        }
+    }
+
+    if (0 != num_continuation_bytes_to_validate) {
+        // Incomplete continuation byte sequence
+        return false;
+    }
+
+    return true;
+}
 }  // namespace clp::ffi
 
 #endif  // CLP_UTILS_HPP
