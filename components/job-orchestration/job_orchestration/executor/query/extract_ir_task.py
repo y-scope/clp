76e16b33105e3edb83defa8e495fed0f3a6176f6
Haiqi Xu <14502009+haiqi96@users.noreply.github.com>
2024-06-26 15:50:59 -0400

    Refactor common functions

--- components/job-orchestration/job_orchestration/executor/query/extract_ir_task.py
+++ components/job-orchestration/job_orchestration/executor/query/extract_ir_task.py
@@ -1,8 +1,7 @@
 import datetime
 import os
-import subprocess
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 
 from celery.app.task import Task
 from celery.utils.log import get_task_logger
@@ -10,14 +9,10 @@ from clp_py_utils.clp_config import Database, StorageEngine
 from clp_py_utils.clp_logging import set_logging_level
 from clp_py_utils.sql_adapter import SQL_Adapter
 from job_orchestration.executor.query.celery import app
+from job_orchestration.executor.query.utils import generic_run_query_task, report_command_creation_failure
 from job_orchestration.scheduler.job_config import ExtractIrJobConfig
-from job_orchestration.scheduler.scheduler_data import QueryTaskResult, QueryTaskStatus
+from job_orchestration.scheduler.scheduler_data import QueryTaskStatus
 
-from job_orchestration.executor.query.utils import (
-    generate_final_task_result,
-    get_task_log_file_path,
-    update_query_task_metadata,
-)
 # Setup logging
 logger = get_task_logger(__name__)
 
@@ -30,11 +25,12 @@ def make_command(
     ir_output_dir: Path,
     extract_ir_config: ExtractIrJobConfig,
     results_cache_uri: str,
-    results_collection: str,
-):
+    ir_collection: str,
+) -> Optional[List[str]]:
     if StorageEngine.CLP == storage_engine:
         if not extract_ir_config.file_split_id:
-            raise ValueError(f"file_split_id not supplied")
+            logger.error("file_split_id not supplied")
+            return None
         command = [
             str(clp_home / "bin" / "clo"),
             "i",
@@ -42,13 +38,14 @@ def make_command(
             extract_ir_config.file_split_id,
             str(ir_output_dir),
             results_cache_uri,
-            results_collection,
+            ir_collection,
         ]
         if extract_ir_config.target_uncompressed_size is not None:
             command.append("--target-size")
             command.append(extract_ir_config.target_uncompressed_size)
     else:
-        raise ValueError(f"Unsupported storage engine {storage_engine}")
+        logger.error(f"Unsupported storage engine {storage_engine}")
+        return None
 
     return command
 
@@ -63,91 +60,55 @@ def extract_ir(
     clp_metadata_db_conn_params: dict,
     results_cache_uri: str,
 ) -> Dict[str, Any]:
-    clp_home = Path(os.getenv("CLP_HOME"))
-    archive_directory = Path(os.getenv("CLP_ARCHIVE_OUTPUT_DIR"))
-    ir_directory = Path(os.getenv("CLP_IR_OUTPUT_DIR"))
-    clp_logs_dir = Path(os.getenv("CLP_LOGS_DIR"))
-    clp_logging_level = str(os.getenv("CLP_LOGGING_LEVEL"))
-    clp_storage_engine = str(os.getenv("CLP_STORAGE_ENGINE"))
-
-    ir_collection = str(os.getenv("CLP_IR_COLLECTION"))
+    # Task name
+    TASK_NAME = "IR extraction"
 
     # Setup logging to file
+    clp_logs_dir = Path(os.getenv("CLP_LOGS_DIR"))
+    clp_logging_level = str(os.getenv("CLP_LOGGING_LEVEL"))
     set_logging_level(logger, clp_logging_level)
-    clo_log_path = get_task_log_file_path(clp_logs_dir, job_id, task_id)
-    clo_log_file = open(clo_log_path, "w")
 
-    logger.info(f"Started IR extraction task for job {job_id}")
-
-    extract_ir_config = ExtractIrJobConfig.parse_obj(job_config_obj)
-    sql_adapter = SQL_Adapter(Database.parse_obj(clp_metadata_db_conn_params))
+    logger.info(f"Started {TASK_NAME} task for job {job_id}")
 
     start_time = datetime.datetime.now()
     task_status: QueryTaskStatus
-    try:
-        task_command = make_command(
-            storage_engine=clp_storage_engine,
-            clp_home=clp_home,
-            archives_dir=archive_directory,
-            archive_id=archive_id,
-            ir_output_dir=ir_directory,
-            extract_ir_config=extract_ir_config,
-            results_cache_uri=results_cache_uri,
-            results_collection=ir_collection,
-        )
-    except ValueError as e:
-        error_message = f"Error creating IR extraction command: {e}"
-        logger.error(error_message)
-        clo_log_file.write(error_message)
+    sql_adapter = SQL_Adapter(Database.parse_obj(clp_metadata_db_conn_params))
 
-        task_status = QueryTaskStatus.FAILED
-        update_query_task_metadata(
-            sql_adapter,
-            task_id,
-            dict(status=task_status, duration=0, start_time=start_time),
-        )
+    # Make task_command
+    clp_home = Path(os.getenv("CLP_HOME"))
+    archive_directory = Path(os.getenv("CLP_ARCHIVE_OUTPUT_DIR"))
+    clp_storage_engine = str(os.getenv("CLP_STORAGE_ENGINE"))
+    ir_output_dir = Path(os.getenv("CLP_IR_OUTPUT_DIR"))
+    ir_collection = str(os.getenv("CLP_IR_COLLECTION"))
+    extract_ir_config = ExtractIrJobConfig.parse_obj(job_config_obj)
 
-        clo_log_file.close()
-        return QueryTaskResult(
+    task_command = make_command(
+        storage_engine=clp_storage_engine,
+        clp_home=clp_home,
+        archives_dir=archive_directory,
+        archive_id=archive_id,
+        ir_output_dir=ir_output_dir,
+        extract_ir_config=extract_ir_config,
+        results_cache_uri=results_cache_uri,
+        ir_collection=ir_collection,
+    )
+
+    if not task_command:
+        return report_command_creation_failure(
+            sql_adapter=sql_adapter,
+            logger=logger,
+            task_name=TASK_NAME,
             task_id=task_id,
-            status=task_status,
-            duration=0,
-            error_log_path=str(clo_log_path),
-        ).dict()
-
-    task_status = QueryTaskStatus.RUNNING
-    update_query_task_metadata(
-        sql_adapter, task_id, dict(status=task_status, start_time=start_time)
-    )
-
-    logger.info(f'Running: {" ".join(task_command)}')
-    extract_proc = subprocess.Popen(
-        task_command,
-        preexec_fn=os.setpgrp,
-        close_fds=True,
-        stdout=clo_log_file,
-        stderr=clo_log_file,
-    )
-
-    logger.info("Waiting for IR extraction to finish")
-    # communicate is equivalent to wait in this case, but avoids deadlocks if we switch to piping
-    # stdout/stderr in the future.
-    extract_proc.communicate()
-    return_code = extract_proc.returncode
-    if 0 != return_code:
-        task_status = QueryTaskStatus.FAILED
-        logger.error(
-            f"IR extraction task {task_id} failed for job {job_id} - return_code={return_code}"
+            start_time=start_time,
         )
-    else:
-        task_status = QueryTaskStatus.SUCCEEDED
-        logger.info(f"IR extraction task {task_id} completed for job {job_id}")
 
-    clo_log_file.close()
-    duration = (datetime.datetime.now() - start_time).total_seconds()
-
-    update_query_task_metadata(
-        sql_adapter, task_id, dict(status=task_status, start_time=start_time, duration=duration)
+    return generic_run_query_task(
+        sql_adapter=sql_adapter,
+        logger=logger,
+        clp_logs_dir=clp_logs_dir,
+        task_command=task_command,
+        task_name=TASK_NAME,
+        job_id=job_id,
+        task_id=task_id,
+        start_time=start_time,
     )
-
-    return generate_final_task_result(task_id, task_status, duration, clo_log_path)
--- components/job-orchestration/job_orchestration/executor/query/fs_search_task.py
+++ components/job-orchestration/job_orchestration/executor/query/fs_search_task.py
@@ -1,8 +1,5 @@
 import datetime
 import os
-import signal
-import subprocess
-import sys
 from pathlib import Path
 from typing import Any, Dict
 
@@ -12,14 +9,9 @@ from clp_py_utils.clp_config import Database, StorageEngine
 from clp_py_utils.clp_logging import set_logging_level
 from clp_py_utils.sql_adapter import SQL_Adapter
 from job_orchestration.executor.query.celery import app
+from job_orchestration.executor.query.utils import generic_run_query_task, report_command_creation_failure
 from job_orchestration.scheduler.job_config import SearchJobConfig
-from job_orchestration.scheduler.scheduler_data import QueryTaskResult, QueryTaskStatus
-
-from job_orchestration.executor.query.utils import (
-    generate_final_task_result,
-    get_task_log_file_path,
-    update_query_task_metadata,
-)
+from job_orchestration.scheduler.scheduler_data import QueryTaskStatus
 
 # Setup logging
 logger = get_task_logger(__name__)
@@ -107,100 +99,53 @@ def search(
     clp_metadata_db_conn_params: dict,
     results_cache_uri: str,
 ) -> Dict[str, Any]:
-    clp_home = Path(os.getenv("CLP_HOME"))
-    archive_directory = Path(os.getenv("CLP_ARCHIVE_OUTPUT_DIR"))
-    clp_logs_dir = Path(os.getenv("CLP_LOGS_DIR"))
-    clp_logging_level = str(os.getenv("CLP_LOGGING_LEVEL"))
-    clp_storage_engine = str(os.getenv("CLP_STORAGE_ENGINE"))
+    # Task name
+    TASK_NAME = "search"
 
     # Setup logging to file
+    clp_logs_dir = Path(os.getenv("CLP_LOGS_DIR"))
+    clp_logging_level = str(os.getenv("CLP_LOGGING_LEVEL"))
     set_logging_level(logger, clp_logging_level)
-    clo_log_path = get_task_log_file_path(clp_logs_dir, job_id, task_id)
-    clo_log_file = open(clo_log_path, "w")
 
-    logger.info(f"Started search task for job {job_id}")
+    logger.info(f"Started {TASK_NAME} task for job {job_id}")
 
-    search_config = SearchJobConfig.parse_obj(job_config_obj)
-    sql_adapter = SQL_Adapter(Database.parse_obj(clp_metadata_db_conn_params))
 
     start_time = datetime.datetime.now()
     task_status: QueryTaskStatus
-    try:
-        task_command = make_command(
-            storage_engine=clp_storage_engine,
-            clp_home=clp_home,
-            archives_dir=archive_directory,
-            archive_id=archive_id,
-            search_config=search_config,
-            results_cache_uri=results_cache_uri,
-            results_collection=job_id,
-        )
-    except ValueError as e:
-        error_message = f"Error creating search command: {e}"
-        logger.error(error_message)
-        clo_log_file.write(error_message)
+    sql_adapter = SQL_Adapter(Database.parse_obj(clp_metadata_db_conn_params))
 
-        task_status = QueryTaskStatus.FAILED
-        update_query_task_metadata(
-            sql_adapter,
-            task_id,
-            dict(status=task_status, duration=0, start_time=start_time),
-        )
+    # Make task_command
+    clp_home = Path(os.getenv("CLP_HOME"))
+    archive_directory = Path(os.getenv("CLP_ARCHIVE_OUTPUT_DIR"))
+    clp_storage_engine = str(os.getenv("CLP_STORAGE_ENGINE"))
+    search_config = SearchJobConfig.parse_obj(job_config_obj)
 
-        clo_log_file.close()
-        return QueryTaskResult(
+    task_command = make_command(
+        storage_engine=clp_storage_engine,
+        clp_home=clp_home,
+        archives_dir=archive_directory,
+        archive_id=archive_id,
+        search_config=search_config,
+        results_cache_uri=results_cache_uri,
+        results_collection=str(task_id),
+    )
+
+    if not task_command:
+        return report_command_creation_failure(
+            sql_adapter=sql_adapter,
+            logger=logger,
+            task_name=TASK_NAME,
             task_id=task_id,
-            status=task_status,
-            duration=0,
-            error_log_path=str(clo_log_path),
-        ).dict()
+            start_time=start_time,
+        )
 
-    task_status = QueryTaskStatus.RUNNING
-    update_query_task_metadata(
-        sql_adapter, task_id, dict(status=task_status, start_time=start_time)
+    return generic_run_query_task(
+        sql_adapter=sql_adapter,
+        logger=logger,
+        clp_logs_dir=clp_logs_dir,
+        task_command=task_command,
+        task_name=TASK_NAME,
+        job_id=job_id,
+        task_id=task_id,
+        start_time=start_time,
     )
-
-    logger.info(f'Running: {" ".join(task_command)}')
-    search_proc = subprocess.Popen(
-        task_command,
-        preexec_fn=os.setpgrp,
-        close_fds=True,
-        stdout=clo_log_file,
-        stderr=clo_log_file,
-    )
-
-    def sigterm_handler(_signo, _stack_frame):
-        logger.debug("Entered sigterm handler")
-        if search_proc.poll() is None:
-            logger.debug("Trying to kill search process")
-            # Kill the process group in case the search process also forked
-            os.killpg(os.getpgid(search_proc.pid), signal.SIGTERM)
-            os.waitpid(search_proc.pid, 0)
-            logger.info(f"Cancelling search task.")
-        # Add 128 to follow convention for exit codes from signals
-        # https://tldp.org/LDP/abs/html/exitcodes.html#AEN23549
-        sys.exit(_signo + 128)
-
-    # Register the function to kill the child process at exit
-    signal.signal(signal.SIGTERM, sigterm_handler)
-
-    logger.info("Waiting for search to finish")
-    # communicate is equivalent to wait in this case, but avoids deadlocks if we switch to piping
-    # stdout/stderr in the future.
-    search_proc.communicate()
-    return_code = search_proc.returncode
-    if 0 != return_code:
-        task_status = QueryTaskStatus.FAILED
-        logger.error(f"Search task {task_id} failed for job {job_id} - return_code={return_code}")
-    else:
-        task_status = QueryTaskStatus.SUCCEEDED
-        logger.info(f"Search task {task_id} completed for job {job_id}")
-
-    clo_log_file.close()
-    duration = (datetime.datetime.now() - start_time).total_seconds()
-
-    update_query_task_metadata(
-        sql_adapter, task_id, dict(status=task_status, start_time=start_time, duration=duration)
-    )
-
-    return generate_final_task_result(task_id, task_status, duration, clo_log_path)
