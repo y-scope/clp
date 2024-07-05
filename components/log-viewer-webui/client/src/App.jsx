1f0b637f9a91d8955f1cee32a5de5301470f77c4
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-05 01:02:31 -0400

    log-viewer-webui: Add boilerplate React client. (#468)
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

---
+++ components/log-viewer-webui/client/src/App.jsx
@@ -0,0 +1,12 @@
+/**
+ * Renders the main application.
+ *
+ * @return {JSX.Element}
+ */
+const App = () => {
+    return (
+        <h1>Hello world!</h1>
+    );
+};
+
+export default App;
---
+++ components/log-viewer-webui/client/src/index.jsx
@@ -0,0 +1,14 @@
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+);
