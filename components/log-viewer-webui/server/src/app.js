bd846f6238000ced42fe72e76e96551c1f0144d9
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-07-07 06:17:47 -0400

    log-viewer-webui: Serve client using server in production; Add npm scripts to setup, lint, and start client and server together during development. (#474)

--- components/log-viewer-webui/server/src/app.js
+++ components/log-viewer-webui/server/src/app.js
@@ -1,4 +1,8 @@
 import fastify from "fastify";
+import * as path from "node:path";
+import process from "node:process";
+
+import {fastifyStatic} from "@fastify/static";
 
 import exampleRoutes from "./routes/examples.js";
 
@@ -6,11 +10,26 @@ import exampleRoutes from "./routes/examples.js";
 /**
  * Creates the Fastify app with the given options.
  *
+ * @param {string} clientDir Absolute path to the client directory to serve when in running in a
+ * production environment.
  * @param {import("fastify").FastifyServerOptions} fastifyOptions
  * @return {Promise<import("fastify").FastifyInstance>}
  */
-const app = async (fastifyOptions = {}) => {
+const app = async (clientDir, fastifyOptions = {}) => {
     const server = fastify(fastifyOptions);
+
+    if ("production" === process.env.NODE_ENV) {
+        // In the development environment, we expect the client to use a separate webserver that
+        // supports live reloading.
+        if (false === path.isAbsolute(clientDir)) {
+            throw new Error("`clientDir` must be an absolute path.");
+        }
+
+        await server.register(fastifyStatic, {
+            prefix: "/",
+            root: clientDir,
+        });
+    }
     await server.register(exampleRoutes);
 
     return server;
