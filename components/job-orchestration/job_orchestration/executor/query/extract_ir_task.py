y-scope/clp

commit 9ba04514e00cd32e36e724515c7ae8d1e2fb7da4

clp-package: Add handling for IR extraction jobs to the query scheduler and workers. (#460)

components/job-orchestration/job_orchestration/executor/query/extract_ir_task.py
@@ -13,7 +13,7 @@ from job_orchestration.executor.query.utils import (
     report_command_creation_failure,
     run_query_task,
 )
-from job_orchestration.scheduler.job_config import SearchJobConfig
+from job_orchestration.scheduler.job_config import ExtractIrJobConfig
 from job_orchestration.scheduler.scheduler_data import QueryTaskStatus
 
 # Setup logging
@@ -25,76 +25,36 @@ def make_command(
     clp_home: Path,
     archives_dir: Path,
     archive_id: str,
-    search_config: SearchJobConfig,
+    ir_output_dir: Path,
+    extract_ir_config: ExtractIrJobConfig,
     results_cache_uri: str,
-    results_collection: str,
+    ir_collection: str,
 ) -> Optional[List[str]]:
     if StorageEngine.CLP == storage_engine:
-        command = [str(clp_home / "bin" / "clo"), "s", str(archives_dir / archive_id)]
-        if search_config.path_filter is not None:
-            command.append("--file-path")
-            command.append(search_config.path_filter)
-    elif StorageEngine.CLP_S == storage_engine:
+        if not extract_ir_config.file_split_id:
+            logger.error("file_split_id not supplied")
+            return None
         command = [
-            str(clp_home / "bin" / "clp-s"),
-            "s",
-            str(archives_dir),
-            "--archive-id",
-            archive_id,
+            str(clp_home / "bin" / "clo"),
+            "i",
+            str(archives_dir / archive_id),
+            extract_ir_config.file_split_id,
+            str(ir_output_dir),
+            results_cache_uri,
+            ir_collection,
         ]
+        if extract_ir_config.target_uncompressed_size is not None:
+            command.append("--target-size")
+            command.append(extract_ir_config.target_uncompressed_size)
     else:
         logger.error(f"Unsupported storage engine {storage_engine}")
         return None
-
-    command.append(search_config.query_string)
-    if search_config.begin_timestamp is not None:
-        command.append("--tge")
-        command.append(str(search_config.begin_timestamp))
-    if search_config.end_timestamp is not None:
-        command.append("--tle")
-        command.append(str(search_config.end_timestamp))
-    if search_config.ignore_case:
-        command.append("--ignore-case")
-
-    if search_config.aggregation_config is not None:
-        aggregation_config = search_config.aggregation_config
-        if aggregation_config.do_count_aggregation is not None:
-            command.append("--count")
-        if aggregation_config.count_by_time_bucket_size is not None:
-            command.append("--count-by-time")
-            command.append(str(aggregation_config.count_by_time_bucket_size))
-
-        # fmt: off
-        command.extend((
-            "reducer",
-            "--host", aggregation_config.reducer_host,
-            "--port", str(aggregation_config.reducer_port),
-            "--job-id", str(aggregation_config.job_id)
-        ))
-        # fmt: on
-    elif search_config.network_address is not None:
-        # fmt: off
-        command.extend((
-            "network",
-            "--host", search_config.network_address[0],
-            "--port", str(search_config.network_address[1])
-        ))
-        # fmt: on
-    else:
-        # fmt: off
-        command.extend((
-            "results-cache",
-            "--uri", results_cache_uri,
-            "--collection", results_collection,
-            "--max-num-results", str(search_config.max_num_results)
-        ))
-        # fmt: on
 
     return command
 
 
 @app.task(bind=True)
-def search(
+def extract_ir(
     self: Task,
     job_id: str,
     task_id: int,
@@ -103,7 +63,7 @@ def search(
     clp_metadata_db_conn_params: dict,
     results_cache_uri: str,
 ) -> Dict[str, Any]:
-    task_name = "search"
+    task_name = "IR extraction"
 
     # Setup logging to file
     clp_logs_dir = Path(os.getenv("CLP_LOGS_DIR"))
@@ -120,16 +80,19 @@ def search(
     clp_home = Path(os.getenv("CLP_HOME"))
     archive_directory = Path(os.getenv("CLP_ARCHIVE_OUTPUT_DIR"))
     clp_storage_engine = os.getenv("CLP_STORAGE_ENGINE")
-    search_config = SearchJobConfig.parse_obj(job_config_obj)
+    ir_output_dir = Path(os.getenv("CLP_IR_OUTPUT_DIR"))
+    ir_collection = os.getenv("CLP_IR_COLLECTION")
+    extract_ir_config = ExtractIrJobConfig.parse_obj(job_config_obj)
 
     task_command = make_command(
         storage_engine=clp_storage_engine,
         clp_home=clp_home,
         archives_dir=archive_directory,
         archive_id=archive_id,
-        search_config=search_config,
+        ir_output_dir=ir_output_dir,
+        extract_ir_config=extract_ir_config,
         results_cache_uri=results_cache_uri,
-        results_collection=str(task_id),
+        ir_collection=ir_collection,
     )
     if not task_command:
         return report_command_creation_failure(

components/clp-package-utils/clp_package_utils/general.py
@@ -69,6 +69,7 @@ class CLPDockerMounts:
         self.data_dir: typing.Optional[DockerMount] = None
         self.logs_dir: typing.Optional[DockerMount] = None
         self.archives_output_dir: typing.Optional[DockerMount] = None
+        self.ir_output_dir: typing.Optional[DockerMount] = None
 
 
 def get_clp_home():
@@ -222,6 +223,19 @@ def generate_container_config(clp_config: CLPConfig, clp_home: pathlib.Path):
             DockerMountType.BIND,
             clp_config.archive_output.directory,
             container_clp_config.archive_output.directory,
+        )
+
+    container_clp_config.ir_output.directory = pathlib.Path("/") / "mnt" / "ir-output"
+    if not is_path_already_mounted(
+        clp_home,
+        CONTAINER_CLP_HOME,
+        clp_config.ir_output.directory,
+        container_clp_config.ir_output.directory,
+    ):
+        docker_mounts.ir_output_dir = DockerMount(
+            DockerMountType.BIND,
+            clp_config.ir_output.directory,
+            container_clp_config.ir_output.directory,
         )
 
     return container_clp_config, docker_mounts
@@ -391,6 +405,7 @@ def validate_results_cache_config(
 def validate_worker_config(clp_config: CLPConfig):
     clp_config.validate_input_logs_dir()
     clp_config.validate_archive_output_dir()
+    clp_config.validate_ir_output_dir()
 
 
 def validate_webui_config(

components/clp-package-utils/clp_package_utils/scripts/start_clp.py
@@ -8,8 +8,8 @@ import socket
 import subprocess
 import sys
 import time
-import typing
 import uuid
+from typing import Any, Dict, List, Optional
 
 import yaml
 from clp_py_utils.clp_config import (
@@ -526,6 +526,8 @@ def start_compression_worker(
         clp_config.redis.compression_backend_database,
         num_cpus,
         mounts,
+        None,
+        None,
     )
 
 
@@ -538,6 +540,13 @@ def start_query_worker(
 ):
     celery_method = "job_orchestration.executor.query"
     celery_route = f"{QueueName.QUERY}"
+
+    query_worker_mount = [mounts.ir_output_dir]
+    query_worker_env = {
+        "CLP_IR_OUTPUT_DIR": container_clp_config.ir_output.directory,
+        "CLP_IR_COLLECTION": clp_config.results_cache.ir_collection_name,
+    }
+
     generic_start_worker(
         QUERY_WORKER_COMPONENT_NAME,
         instance_id,
@@ -549,6 +558,8 @@ def start_query_worker(
         clp_config.redis.query_backend_database,
         num_cpus,
         mounts,
+        query_worker_env,
+        query_worker_mount,
     )
 
 
@@ -563,6 +574,8 @@ def generic_start_worker(
     redis_database: int,
     num_cpus: int,
     mounts: CLPDockerMounts,
+    worker_specific_env: Dict[str, Any],
+    worker_specific_mount: List[Optional[DockerMount]],
 ):
     logger.info(f"Starting {component_name}...")
 
@@ -578,6 +591,7 @@ def generic_start_worker(
 
     # Create necessary directories
     clp_config.archive_output.directory.mkdir(parents=True, exist_ok=True)
+    clp_config.ir_output.directory.mkdir(parents=True, exist_ok=True)
 
     clp_site_packages_dir = CONTAINER_CLP_HOME / "lib" / "python3" / "site-packages"
     # fmt: off
@@ -605,19 +619,28 @@ def generic_start_worker(
         "-e", f"CLP_LOGGING_LEVEL={worker_config.logging_level}",
         "-e", f"CLP_STORAGE_ENGINE={clp_config.package.storage_engine}",
         "-u", f"{os.getuid()}:{os.getgid()}",
-        "--mount", str(mounts.clp_home),
     ]
+    if worker_specific_env:
+        for env_name, env_value in worker_specific_env.items():
+            container_start_cmd.append("-e")
+            container_start_cmd.append(f"{env_name}={env_value}")
+
     # fmt: on
     necessary_mounts = [
+        mounts.clp_home,
         mounts.data_dir,
         mounts.logs_dir,
         mounts.archives_output_dir,
         mounts.input_logs_dir,
     ]
+    if worker_specific_mount:
+        necessary_mounts.extend(worker_specific_mount)
+
     for mount in necessary_mounts:
-        if mount:
-            container_start_cmd.append("--mount")
-            container_start_cmd.append(str(mount))
+        if not mount:
+            raise ValueError(f"Required mount configuration is empty: {necessary_mounts}")
+        container_start_cmd.append("--mount")
+        container_start_cmd.append(str(mount))
     container_start_cmd.append(clp_config.execution_container)
 
     worker_cmd = [
@@ -645,8 +668,8 @@ def generic_start_worker(
 
 def update_meteor_settings(
     parent_key_prefix: str,
-    settings: typing.Dict[str, typing.Any],
-    updates: typing.Dict[str, typing.Any],
+    settings: Dict[str, Any],
+    updates: Dict[str, Any],
 ):
     """
     Recursively updates the given Meteor settings object with the values from `updates`.

components/clp-py-utils/clp_py_utils/clp_config.py
@@ -254,7 +254,8 @@ class Reducer(BaseModel):
 class ResultsCache(BaseModel):
     host: str = "localhost"
     port: int = 27017
-    db_name: str = "clp-search"
+    db_name: str = "clp-query-results"
+    ir_collection_name: str = "ir-files"
 
     @validator("host")
     def validate_host(cls, field):
@@ -266,6 +267,12 @@ class ResultsCache(BaseModel):
     def validate_db_name(cls, field):
         if "" == field:
             raise ValueError(f"{RESULTS_CACHE_COMPONENT_NAME}.db_name cannot be empty.")
+        return field
+
+    @validator("ir_collection_name")
+    def validate_ir_collection_name(cls, field):
+        if "" == field:
+            raise ValueError(f"{RESULTS_CACHE_COMPONENT_NAME}.ir_collection_name cannot be empty.")
         return field
 
     def get_uri(self):
@@ -309,6 +316,32 @@ class ArchiveOutput(BaseModel):
     def validate_target_segment_size(cls, field):
         if field <= 0:
             raise ValueError("target_segment_size must be greater than 0")
+        return field
+
+    def make_config_paths_absolute(self, clp_home: pathlib.Path):
+        self.directory = make_config_path_absolute(clp_home, self.directory)
+
+    def dump_to_primitive_dict(self):
+        d = self.dict()
+        # Turn directory (pathlib.Path) into a primitive string
+        d["directory"] = str(d["directory"])
+        return d
+
+
+class IrOutput(BaseModel):
+    directory: pathlib.Path = pathlib.Path("var") / "data" / "ir"
+    target_uncompressed_size: int = 128 * 1024 * 1024
+
+    @validator("directory")
+    def validate_directory(cls, field):
+        if "" == field:
+            raise ValueError("directory can not be empty")
+        return field
+
+    @validator("target_uncompressed_size")
+    def validate_target_uncompressed_size(cls, field):
+        if field <= 0:
+            raise ValueError("target_uncompressed_size must be greater than 0")
         return field
 
     def make_config_paths_absolute(self, clp_home: pathlib.Path):
@@ -368,6 +401,7 @@ class CLPConfig(BaseModel):
     credentials_file_path: pathlib.Path = CLP_DEFAULT_CREDENTIALS_FILE_PATH
 
     archive_output: ArchiveOutput = ArchiveOutput()
+    ir_output: IrOutput = IrOutput()
     data_directory: pathlib.Path = pathlib.Path("var") / "data"
     logs_directory: pathlib.Path = pathlib.Path("var") / "log"
 
@@ -377,6 +411,7 @@ class CLPConfig(BaseModel):
         self.input_logs_directory = make_config_path_absolute(clp_home, self.input_logs_directory)
         self.credentials_file_path = make_config_path_absolute(clp_home, self.credentials_file_path)
         self.archive_output.make_config_paths_absolute(clp_home)
+        self.ir_output.make_config_paths_absolute(clp_home)
         self.data_directory = make_config_path_absolute(clp_home, self.data_directory)
         self.logs_directory = make_config_path_absolute(clp_home, self.logs_directory)
         self._os_release_file_path = make_config_path_absolute(clp_home, self._os_release_file_path)
@@ -395,6 +430,12 @@ class CLPConfig(BaseModel):
             validate_path_could_be_dir(self.archive_output.directory)
         except ValueError as ex:
             raise ValueError(f"archive_output.directory is invalid: {ex}")
+
+    def validate_ir_output_dir(self):
+        try:
+            validate_path_could_be_dir(self.ir_output.directory)
+        except ValueError as ex:
+            raise ValueError(f"ir_output.directory is invalid: {ex}")
 
     def validate_data_dir(self):
         try:
@@ -463,6 +504,7 @@ class CLPConfig(BaseModel):
     def dump_to_primitive_dict(self):
         d = self.dict()
         d["archive_output"] = self.archive_output.dump_to_primitive_dict()
+        d["ir_output"] = self.ir_output.dump_to_primitive_dict()
         # Turn paths into primitive strings
         d["input_logs_directory"] = str(self.input_logs_directory)
         d["credentials_file_path"] = str(self.credentials_file_path)

components/job-orchestration/job_orchestration/executor/query/celeryconfig.py
@@ -2,10 +2,14 @@ import os
 
 from job_orchestration.scheduler.constants import QueueName
 
-imports = "job_orchestration.executor.query.fs_search_task"
+imports = (
+    "job_orchestration.executor.query.fs_search_task",
+    "job_orchestration.executor.query.extract_ir_task",
+)
 
 task_routes = {
     "job_orchestration.executor.query.fs_search_task.search": QueueName.QUERY,
+    "job_orchestration.executor.query.extract_ir_task.extract_ir": QueueName.QUERY,
 }
 task_create_missing_queues = True
 

components/job-orchestration/job_orchestration/executor/query/utils.py
@@ -1,0 +1,135 @@
+import datetime
+import os
+import signal
+import subprocess
+import sys
+from contextlib import closing
+from logging import Logger
+from pathlib import Path
+from typing import Any, Dict, List
+
+from clp_py_utils.clp_config import QUERY_TASKS_TABLE_NAME
+from clp_py_utils.sql_adapter import SQL_Adapter
+from job_orchestration.scheduler.scheduler_data import QueryTaskResult, QueryTaskStatus
+
+
+def get_task_log_file_path(clp_logs_dir: Path, job_id: str, task_id: int) -> Path:
+    worker_logs_dir = clp_logs_dir / job_id
+    worker_logs_dir.mkdir(exist_ok=True, parents=True)
+    return worker_logs_dir / f"{task_id}-clo.log"
+
+
+def report_command_creation_failure(
+    sql_adapter: SQL_Adapter,
+    logger: Logger,
+    task_name: str,
+    task_id: int,
+    start_time: datetime.datetime,
+):
+    logger.error(f"Error creating {task_name} command")
+    task_status = QueryTaskStatus.FAILED
+    update_query_task_metadata(
+        sql_adapter,
+        task_id,
+        dict(status=task_status, duration=0, start_time=start_time),
+    )
+
+    return QueryTaskResult(
+        task_id=task_id,
+        status=task_status,
+        duration=0,
+    ).dict()
+
+
+def run_query_task(
+    sql_adapter: SQL_Adapter,
+    logger: Logger,
+    clp_logs_dir: Path,
+    task_command: List[str],
+    task_name: str,
+    job_id: str,
+    task_id: int,
+    start_time: datetime.datetime,
+):
+    clo_log_path = get_task_log_file_path(clp_logs_dir, job_id, task_id)
+    clo_log_file = open(clo_log_path, "w")
+
+    task_status = QueryTaskStatus.RUNNING
+    update_query_task_metadata(
+        sql_adapter, task_id, dict(status=task_status, start_time=start_time)
+    )
+
+    logger.info(f'Running: {" ".join(task_command)}')
+    task_proc = subprocess.Popen(
+        task_command,
+        preexec_fn=os.setpgrp,
+        close_fds=True,
+        stdout=clo_log_file,
+        stderr=clo_log_file,
+    )
+
+    def sigterm_handler(_signo, _stack_frame):
+        logger.debug("Entered sigterm handler")
+        if task_proc.poll() is None:
+            logger.debug(f"Trying to kill {task_name} process")
+            # Kill the process group in case the task process also forked
+            os.killpg(os.getpgid(task_proc.pid), signal.SIGTERM)
+            os.waitpid(task_proc.pid, 0)
+            logger.info(f"Cancelling {task_name} task.")
+        # Add 128 to follow convention for exit codes from signals
+        # https://tldp.org/LDP/abs/html/exitcodes.html#AEN23549
+        sys.exit(_signo + 128)
+
+    # Register the function to kill the child process at exit
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    logger.info(f"Waiting for {task_name} to finish")
+    # `communicate` is equivalent to `wait` in this case, but avoids deadlocks if we switch to
+    # piping stdout/stderr in the future.
+    task_proc.communicate()
+    return_code = task_proc.returncode
+    if 0 != return_code:
+        task_status = QueryTaskStatus.FAILED
+        logger.error(
+            f"{task_name} task {task_id} failed for job {job_id} - return_code={return_code}"
+        )
+    else:
+        task_status = QueryTaskStatus.SUCCEEDED
+        logger.info(f"{task_name} task {task_id} completed for job {job_id}")
+
+    clo_log_file.close()
+    duration = (datetime.datetime.now() - start_time).total_seconds()
+
+    update_query_task_metadata(
+        sql_adapter, task_id, dict(status=task_status, start_time=start_time, duration=duration)
+    )
+
+    task_result = QueryTaskResult(
+        status=task_status,
+        task_id=task_id,
+        duration=duration,
+    )
+
+    if QueryTaskStatus.FAILED == task_status:
+        task_result.error_log_path = str(clo_log_path)
+
+    return task_result.dict()
+
+
+def update_query_task_metadata(
+    sql_adapter: SQL_Adapter,
+    task_id: int,
+    kv_pairs: Dict[str, Any],
+):
+    with closing(sql_adapter.create_connection(True)) as db_conn, closing(
+        db_conn.cursor(dictionary=True)
+    ) as db_cursor:
+        if not kv_pairs or len(kv_pairs) == 0:
+            raise ValueError("No key-value pairs provided to update query task metadata")
+
+        query = f"""
+            UPDATE {QUERY_TASKS_TABLE_NAME}
+            SET {', '.join([f'{k}="{v}"' for k, v in kv_pairs.items()])}
+            WHERE id = {task_id}
+        """
+        db_cursor.execute(query)

components/job-orchestration/job_orchestration/scheduler/constants.py
@@ -71,6 +71,7 @@ class QueryTaskStatus(IntEnum):
 
 class QueryJobType(IntEnum):
     SEARCH_OR_AGGREGATION = 0
+    EXTRACT_IR = auto()
 
     def __str__(self) -> str:
         return str(self.value)

components/job-orchestration/job_orchestration/scheduler/job_config.py
@@ -42,6 +42,13 @@ class AggregationConfig(BaseModel):
 class QueryJobConfig(BaseModel): ...
 
 
+class ExtractIrJobConfig(QueryJobConfig):
+    orig_file_id: str
+    msg_ix: int
+    file_split_id: typing.Optional[str] = None
+    target_uncompressed_size: typing.Optional[int] = None
+
+
 class SearchJobConfig(QueryJobConfig):
     query_string: str
     max_num_results: int

commit 862fccf63424e3a5a677abce1ee1245ac2099cee

clp-package: Fix bugs introduced in #460: (#481)
- Write search results to collection named job_id rather than task_id.
- Convert int to str in IR extraction command generation.

components/job-orchestration/job_orchestration/executor/query/extract_ir_task.py
@@ -45,7 +45,7 @@ def make_command(
         ]
         if extract_ir_config.target_uncompressed_size is not None:
             command.append("--target-size")
-            command.append(extract_ir_config.target_uncompressed_size)
+            command.append(str(extract_ir_config.target_uncompressed_size))
     else:
         logger.error(f"Unsupported storage engine {storage_engine}")
         return None

components/job-orchestration/job_orchestration/executor/query/fs_search_task.py
@@ -129,7 +129,7 @@ def search(
         archive_id=archive_id,
         search_config=search_config,
         results_cache_uri=results_cache_uri,
-        results_collection=str(task_id),
+        results_collection=job_id,
     )
     if not task_command:
         return report_command_creation_failure(