c051ec20efc81b925570a5ec4d375bfb51ee6896
Junhao Liao <junhao@junhao.ca>
2024-07-23 16:05:37 -0400

    Reformat Python code: add trailing comma to list item.

--- components/clp-package-utils/clp_package_utils/scripts/start_clp.py
+++ components/clp-package-utils/clp_package_utils/scripts/start_clp.py
@@ -816,7 +816,7 @@ def start_log_viewer_webui(
         "MongoDbIrFilesCollectionName": clp_config.results_cache.ir_collection_name,
         "ClientDir": str(container_log_viewer_webui_dir / "client"),
         "IrFilesDir": str(container_clp_config.ir_output.directory),
-        "LogViewerDir": str(container_log_viewer_webui_dir / "yscope-log-viewer")
+        "LogViewerDir": str(container_log_viewer_webui_dir / "yscope-log-viewer"),
     }
     settings_json = read_and_update_settings_json(settings_json_path, settings_json_updates)
     with open(settings_json_path, "w") as settings_json_file:
