aad78ced11ecd5d4aa1781d0f2785d2bdc75b68e
Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>
2024-08-15 19:55:07 -0400

    ffi: Add class for key-value pair log events. (#507)
    Co-authored-by: kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>

--- components/core/src/clp/ir/EncodedTextAst.hpp
+++ components/core/src/clp/ir/EncodedTextAst.hpp
@@ -26,17 +26,6 @@ public:
               m_dict_vars{std::move(dict_vars)},
               m_encoded_vars{std::move(encoded_vars)} {}
 
-    // Disable copy constructor and assignment operator
-    EncodedTextAst(EncodedTextAst const&) = delete;
-    auto operator=(EncodedTextAst const&) -> EncodedTextAst& = delete;
-
-    // Default move constructor and assignment operator
-    EncodedTextAst(EncodedTextAst&&) = default;
-    auto operator=(EncodedTextAst&&) -> EncodedTextAst& = default;
-
-    // Destructor
-    ~EncodedTextAst() = default;
-
     // Methods
     auto operator==(EncodedTextAst const&) const -> bool = default;
 
