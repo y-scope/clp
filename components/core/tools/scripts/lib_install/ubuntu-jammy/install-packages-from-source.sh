14216d80fe676d326c447baa882c23374db99319
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-08-19 15:05:11 -0400

    Try ubuntu...

--- components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
@@ -9,7 +9,6 @@ set -u
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 lib_install_scripts_dir=$script_dir/..
 
-"$lib_install_scripts_dir"/install-curl.sh 8.9.1
 "$lib_install_scripts_dir"/fmtlib.sh 8.0.1
 "$lib_install_scripts_dir"/libarchive.sh 3.5.1
 "$lib_install_scripts_dir"/lz4.sh 1.8.2
@@ -18,3 +17,5 @@ lib_install_scripts_dir=$script_dir/..
 "$lib_install_scripts_dir"/msgpack.sh 6.0.0
 "$lib_install_scripts_dir"/spdlog.sh 1.9.2
 "$lib_install_scripts_dir"/zstandard.sh 1.4.9
+
+"$lib_install_scripts_dir"/install-curl.sh 8.9.1
