994bcb0d4fe6bc96dc32bc31866e78611b57c4a1
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-08-30 13:50:52 -0400

    Add shared library build to workflow checks.

--- components/core/tools/scripts/utils/build-and-run-unit-tests.sh
+++ components/core/tools/scripts/utils/build-and-run-unit-tests.sh
@@ -5,6 +5,30 @@ set -e
 # Error on undefined variable
 set -u
 
+# Builds core.
+#
+# @param src_dir
+# @param build_dir
+# @param use_static_libs Whether to use static libraries when building.
+# @param num_jobs Max number of jobs to run when building.
+build () {
+    src_dir="$1"
+    build_dir="$2"
+    use_static_libs="$3"
+    num_jobs="$4"
+
+    cmake_flags=()
+    if [ "$use_static_libs" = true ]; then
+        cmake_flags+=(-DCLP_USE_STATIC_LIBS=ON)
+    else
+        cmake_flags+=(-DCLP_USE_STATIC_LIBS=OFF)
+    fi
+
+    rm -rf "$build_dir"
+    cmake "${cmake_flags[@]}" -S "$src_dir" -B "$build_dir"
+    cmake --build "$build_dir" --parallel "$num_jobs"
+}
+
 cUsage="Usage: ${BASH_SOURCE[0]} <source-dir> <build-dir>[ <unit-tests-filter>]"
 if [ "$#" -lt 2 ]; then
     echo "$cUsage"
@@ -16,8 +40,13 @@ if [ "$#" -gt 2 ]; then
     unit_tests_filter="$3"
 fi
 
-cmake -S "$src_dir" -B "$build_dir"
-cmake --build "$build_dir" --parallel "$(getconf _NPROCESSORS_ONLN)"
+num_cores="$(getconf _NPROCESSORS_ONLN)"
+
+# NOTE: The static-linking build must be last since those binaries are added to the core binaries
+# container images.
+build "$src_dir" "$build_dir" false "$num_cores"
+build "$src_dir" "$build_dir" true "$num_cores"
+
 cd "$build_dir"
 if [ -z "${unit_tests_filter+x}" ]; then
     ./unitTest
