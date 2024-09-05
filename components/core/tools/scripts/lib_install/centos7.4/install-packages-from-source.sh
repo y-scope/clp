412e0e2be7b255a7475187f9316734656a2365bb
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-09-05 04:52:20 -0400

    core: Link reducer-server against missing dynamic libraries; Upgrade to mongocxx 3.10.2; Add test for shared library build (fixes #526). (#527)
    Co-authored-by: Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>

--- components/core/tools/scripts/lib_install/centos7.4/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/centos7.4/install-packages-from-source.sh
@@ -28,8 +28,7 @@ lib_install_scripts_dir=$script_dir/..
 "$lib_install_scripts_dir"/libarchive.sh 3.5.1
 "$lib_install_scripts_dir"/lz4.sh 1.8.2
 "$lib_install_scripts_dir"/mariadb-connector-c.sh 3.2.3
-"$lib_install_scripts_dir"/mongoc.sh 1.24.4
-"$lib_install_scripts_dir"/mongocxx.sh 3.8.0
+"$lib_install_scripts_dir"/mongocxx.sh 3.10.2
 "$lib_install_scripts_dir"/msgpack.sh 6.0.0
 "$lib_install_scripts_dir"/spdlog.sh 1.9.2
 "$lib_install_scripts_dir"/zstandard.sh 1.4.9
