ebd64b183de3211e1119d596858a06dec3a6de74
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-09-05 21:18:41 -0400

    Update components/core/tools/scripts/lib_install/centos-stream-9/install-packages-from-source.sh
    Co-authored-by: Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>

--- components/core/tools/scripts/lib_install/centos-stream-9/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/centos-stream-9/install-packages-from-source.sh
@@ -9,7 +9,7 @@ set -u
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 lib_install_scripts_dir="${script_dir}/.."
 
-# NOTE: The remaining installation scripts depend on and boost, so we install it beforehand.
+# NOTE: The remaining installation scripts depend on boost, so we install it beforehand.
 "${lib_install_scripts_dir}/install-boost.sh" 1.76.0
 
 "${lib_install_scripts_dir}/fmtlib.sh" 8.0.1
