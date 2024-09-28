7b10f41b943902f9a7b7212b9b9895b611bb5397
Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>
2024-09-27 22:41:56 -0400

    ffi: Add `IrUnitHandlerInterface` to perform user-defined handling for deserialized IR units. (#540)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: davidlion <davidlion2@protonmail.com>

---
+++ components/core/src/clp/ffi/ir_stream/IrUnitHandlerInterface.hpp
@@ -0,0 +1,62 @@
+#ifndef CLP_FFI_IR_STREAM_IRUNITHANDLERINTERFACE_HPP
+#define CLP_FFI_IR_STREAM_IRUNITHANDLERINTERFACE_HPP
+
+#include <concepts>
+#include <utility>
+
+#include "../../time_types.hpp"
+#include "../KeyValuePairLogEvent.hpp"
+#include "../SchemaTree.hpp"
+#include "decoding_methods.hpp"
+
+namespace clp::ffi::ir_stream {
+/**
+ * Concept that defines the IR unit handler interface.
+ */
+template <typename Handler>
+concept IrUnitHandlerInterface = requires(
+        Handler handler,
+        KeyValuePairLogEvent&& log_event,
+        UtcOffset utc_offset_old,
+        UtcOffset utc_offset_new,
+        SchemaTree::NodeLocator schema_tree_node_locator
+) {
+    /**
+     * Handles a log event IR unit.
+     * @param log_event The deserialized result from IR deserializer.
+     * @return IRErrorCode::Success on success, user-defined error code on failures.
+     */
+    {
+        handler.handle_log_event(std::forward<KeyValuePairLogEvent&&>(log_event))
+    } -> std::same_as<IRErrorCode>;
+
+    /**
+     * Handles a UTC offset change IR unit.
+     * @param utc_offset_old The offset before the change.
+     * @param utc_offset_new The deserialized new offset.
+     * @return IRErrorCode::Success on success, user-defined error code on failures.
+     */
+    {
+        handler.handle_utc_offset_change(utc_offset_old, utc_offset_new)
+    } -> std::same_as<IRErrorCode>;
+
+    /**
+     * Handles a schema tree node insertion IR unit.
+     * @param schema_tree_node_locator The locator of the node to insert.
+     * @return IRErrorCode::Success on success, user-defined error code on failures.
+     */
+    {
+        handler.handle_schema_tree_node_insertion(schema_tree_node_locator)
+    } -> std::same_as<IRErrorCode>;
+
+    /**
+     * Handles an end-of-stream indicator IR unit.
+     * @return IRErrorCode::Success on success, user-defined error code on failures.
+     */
+    {
+        handler.handle_end_of_stream()
+    } -> std::same_as<IRErrorCode>;
+};
+}  // namespace clp::ffi::ir_stream
+
+#endif  // CLP_FFI_IR_STREAM_IRUNITHANDLERINTERFACE_HPP
