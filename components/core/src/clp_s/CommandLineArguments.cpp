b291d77f48ac566a861ce5e1f6f87a59a5416c8e
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-10-04 18:38:41 -0400

    Edit CLI help for projection option.

--- components/core/src/clp_s/CommandLineArguments.cpp
+++ components/core/src/clp_s/CommandLineArguments.cpp
@@ -430,8 +430,9 @@ CommandLineArguments::parse_arguments(int argc, char const** argv) {
                 po::value<std::vector<std::string>>(&m_projection_columns)
                     ->multitoken()
                     ->value_name("COLUMN_A COLUMN_B ..."),
-                "The set of projected columns that will be marshalled for matching results -- "
-                "must appear after all positional options"
+                "Project only the given set of columns for matching results. This option must be"
+                " specified after all positional options. Values that are objects or structured"
+                " arrays are currently unsupported."
             );
             // clang-format on
             search_options.add(match_options);
