1548ae3a2ff1069dd9f4d88fada2e39f171accde
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-11-26 19:57:53 -0500

    Update components/clp-package-utils/clp_package_utils/__init__.py
    Co-authored-by: kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>

--- components/clp-package-utils/clp_package_utils/__init__.py
+++ components/clp-package-utils/clp_package_utils/__init__.py
@@ -6,6 +6,7 @@ logging_formatter = logging.Formatter(
     "%(asctime)s.%(msecs)03d %(levelname)s [%(module)s] %(message)s", datefmt="%Y-%m-%dT%H:%M:%S"
 )
 logging_console_handler.setFormatter(logging_formatter)
+
 # Set up root logger
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
