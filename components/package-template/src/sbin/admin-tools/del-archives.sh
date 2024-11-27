4d1e9e6cd6da82c9e9a7b2767fa3f28aec15f599
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-11-27 11:54:47 -0500

    feat(clp-package): Add support for deleting archives that are exclusively within a time range. (#594)
    Co-authored-by: kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>

---
+++ components/package-template/src/sbin/admin-tools/del-archives.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+package_root="$script_dir/../.."
+
+PYTHONPATH=$(readlink -f "$package_root/lib/python3/site-packages") \
+    python3 \
+    -m clp_package_utils.scripts.del_archives \
+    "$@"
