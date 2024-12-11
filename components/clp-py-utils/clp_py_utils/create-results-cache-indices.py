fb4516ecf7c226759b58c198d1f4647fb4057075
Junhao Liao <junhao@junhao.ca>
2024-12-11 09:37:37 -0500

    Enable MongoDB replica set for the results cache and initialize it when starting the package.

--- components/clp-py-utils/clp_py_utils/create-results-cache-indices.py
+++ components/clp-py-utils/clp_py_utils/create-results-cache-indices.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 from pymongo import IndexModel, MongoClient
+from pymongo.errors import OperationFailure
 
 # Setup logging
 # Create logger
@@ -13,6 +14,22 @@ logging_console_handler = logging.StreamHandler()
 logging_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 logging_console_handler.setFormatter(logging_formatter)
 logger.addHandler(logging_console_handler)
+
+
+def initialize_replica_set(client, uri):
+    try:
+        result = client.admin.command("replSetGetStatus")
+        logger.info("Replica set already initialized: %s", result)
+    except OperationFailure as e:
+        logger.info("Initializing replica set")
+
+        # Explicit host specification is required, or the docker's ID would be used as the hostname.
+        config = {
+            "_id": "rs0",
+            "members": [{"_id": 0, "host": "localhost:27017"}],
+        }
+        client.admin.command("replSetInitiate", config)
+        logger.info("Replica set initialized successfully.")
 
 
 def main(argv):
@@ -27,9 +44,11 @@ def main(argv):
     stream_collection_name = parsed_args.stream_collection
 
     try:
+        with MongoClient(results_cache_uri, directConnection=True) as results_cache_client:
+            initialize_replica_set(results_cache_client, results_cache_uri)
+
         with MongoClient(results_cache_uri) as results_cache_client:
             stream_collection = results_cache_client.get_default_database()[stream_collection_name]
-
             file_split_id_index = IndexModel(["file_split_id"])
             orig_file_id_index = IndexModel(["orig_file_id", "begin_msg_ix", "end_msg_ix"])
             stream_collection.create_indexes([file_split_id_index, orig_file_id_index])
