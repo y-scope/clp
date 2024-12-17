3b870a44438a191b02dbe5bb832a51d67a430718
Haiqi Xu <14502009+haiqi96@users.noreply.github.com>
2024-12-16 20:24:32 -0500

    Add docstrings

--- components/clp-py-utils/clp_py_utils/s3_utils.py
+++ components/clp-py-utils/clp_py_utils/s3_utils.py
@@ -11,11 +11,21 @@ from clp_py_utils.clp_config import S3Config
 def s3_put(
     s3_config: S3Config, src_file: Path, dest_file_name: str, total_max_attempts: int = 3
 ) -> Result[bool, str]:
-
+    """
+    Upload a local file to S3 bucket using AWS's PutObject operation.
+    :param s3_config: S3 configuration specifies the upload destination and credentials.
+    :param src_file: local file to upload.
+    :param dest_file_name: The name for uploaded file in the S3 bucket.
+    :param total_max_attempts: Maximum number of retry attempts for the upload. Defaults to 3.
+    :return: Result.OK(bool) on success.
+             Result.Err(str) with the error message if put operation fails.
+    """
     if not src_file.exists():
         return Err(f"{src_file} doesn't exist")
     if not src_file.is_file():
         return Err(f"{src_file} is not a file")
+    if src_file.stat().st_size > 5 * 1024 * 1024 * 1024:
+        return Err("File size exceeds 5GB limit for single put_object operation")
 
     config = Config(retries=dict(total_max_attempts=total_max_attempts, mode="adaptive"))
 
