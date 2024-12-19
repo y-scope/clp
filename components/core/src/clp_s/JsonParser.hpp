81d627b6bdf6723b8390bb531be5db6766b31d05
Abigail Matthews <abigail.v.matthews@gmail.com>
2024-12-19 15:40:51 -0500

    feat(clp-s): Add support for kv-pair-IR ingestion. (#630)

--- components/core/src/clp_s/JsonParser.hpp
+++ components/core/src/clp_s/JsonParser.hpp
@@ -1,15 +1,22 @@
 #ifndef CLP_S_JSONPARSER_HPP
 #define CLP_S_JSONPARSER_HPP
 
+#include <cstdint>
 #include <map>
+#include <optional>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <variant>
 #include <vector>
 
+#include <absl/container/flat_hash_map.h>
 #include <boost/uuid/random_generator.hpp>
 #include <simdjson.h>
 
+#include "../clp/ffi/KeyValuePairLogEvent.hpp"
+#include "../clp/ffi/SchemaTree.hpp"
+#include "../clp/ffi/Value.hpp"
 #include "../clp/GlobalMySQLMetadataDB.hpp"
 #include "ArchiveWriter.hpp"
 #include "CommandLineArguments.hpp"
@@ -26,6 +33,7 @@
 #include "ZstdCompressor.hpp"
 
 using namespace simdjson;
+using clp::ffi::KeyValuePairLogEvent;
 
 namespace clp_s {
 struct JsonParserOption {
@@ -66,6 +74,12 @@ public:
     [[nodiscard]] bool parse();
 
     /**
+     * Parses the Key Value IR Stream and stores the data in the archive.
+     * @return whether the IR Stream was parsed successfully
+     */
+    [[nodiscard]] auto parse_from_ir() -> bool;
+
+    /**
      * Writes the metadata and archive data to disk.
      */
     void store();
@@ -79,6 +93,51 @@ private:
      * @throw simdjson::simdjson_error when encountering invalid fields while parsing line
      */
     void parse_line(ondemand::value line, int32_t parent_node_id, std::string const& key);
+
+    /**
+     * Determines the archive node type based on the IR node type and value.
+     * @param ir_node_type schema node type from the IR stream
+     * @param node_has_value Boolean that says whether or not the node has value.
+     * @param node_value The IR schema node value if the node has value
+     * @return The NodeType that should be used for the archive node
+     */
+    static auto get_archive_node_type(
+            clp::ffi::SchemaTree const& tree,
+            std::pair<clp::ffi::SchemaTree::Node::id_t, std::optional<clp::ffi::Value>> const&
+                    kv_pair
+    ) -> NodeType;
+
+    /**
+     * Adds new schema node to archive and adds translation for IR node ID and NodeType to mapping
+     * @param ir_node_id ID of the IR node
+     * @param ir_node_to_add IR Schema Node that is being translated to archive
+     * @param archive_node_type Type of the archive node
+     * @param parent_node_id ID of the parent of the IR node
+     */
+    auto add_node_to_archive_and_translations(
+            uint32_t ir_node_id,
+            clp::ffi::SchemaTree::Node const& ir_node_to_add,
+            NodeType archive_node_type,
+            int32_t parent_node_id
+    ) -> int;
+
+    /**
+     * Gets the archive node ID for an IR node.
+     * @param ir_node_id ID of the IR node
+     * @param archive_node_type Type of the archive node
+     * @param ir_tree The IR schema tree
+     */
+    auto get_archive_node_id(
+            uint32_t ir_node_id,
+            NodeType archive_node_type,
+            clp::ffi::SchemaTree const& ir_tree
+    ) -> int;
+
+    /**
+     * Parses a Key Value Log Event.
+     * @param kv the Key Value Log Event
+     */
+    void parse_kv_log_event(KeyValuePairLogEvent const& kv);
 
     /**
      * Parses an array within a JSON line
@@ -123,6 +182,9 @@ private:
     size_t m_max_document_size;
     bool m_structurize_arrays{false};
     bool m_record_log_order{true};
+
+    absl::flat_hash_map<std::pair<uint32_t, NodeType>, int32_t>
+            m_ir_node_to_archive_node_id_mapping;
 };
 }  // namespace clp_s
 
