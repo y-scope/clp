818be9e2eef61a5b33d81798ebef45a4b5b882e9
Eden Zhang <49173122+Eden-D-Zhang@users.noreply.github.com>
2025-01-08 10:03:42 -0500

    refactor(core-clp): Remove unused functions from `src/clp/Utils.*`. (#654)

--- components/core/src/clp/Utils.hpp
+++ components/core/src/clp/Utils.hpp
@@ -36,28 +36,6 @@ ErrorCode create_directory(std::string const& path, mode_t mode, bool exist_ok);
 ErrorCode create_directory_structure(std::string const& path, mode_t mode);
 
 /**
- * Gets the parent directory path for a given path
- * Corner cases:
- * - get_dirname("abc") = "."
- * - get_dirname(".") = "."
- * - get_dirname("..") = "."
- * - get_dirname("/") = "/"
- * - get_dirname("/.") = "/"
- * - get_dirname("/..") = "/"
- * - get_dirname("/abc") = "/"
- * @param path
- * @return Parent directory path
- */
-std::string get_parent_directory_path(std::string const& path);
-
-/**
- * Removes ".", "..", and consecutive "/" from a given path and returns the result
- * @param path The given path
- * @return The unambiguous path
- */
-std::string get_unambiguous_path(std::string const& path);
-
-/**
  * Read a list of paths from a file
  * @param list_path
  * @param paths
