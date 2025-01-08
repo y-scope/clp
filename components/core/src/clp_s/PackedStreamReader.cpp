ae419b060a2c8bcb22e8d08179f8bf0ebb12f854
gibber9809 <devinbook1@gmail.com>
2025-01-08 21:28:12 +0000

    Update several components to use ArchiveReaderAdaptor

--- components/core/src/clp_s/PackedStreamReader.cpp
+++ components/core/src/clp_s/PackedStreamReader.cpp
@@ -1,14 +1,15 @@
 #include "PackedStreamReader.hpp"
 
+#include "../clp/BoundedReader.hpp"
+#include "archive_constants.hpp"
+#include "ArchiveReaderAdaptor.hpp"
+
 namespace clp_s {
 
 void PackedStreamReader::read_metadata(ZstdDecompressor& decompressor) {
     switch (m_state) {
         case PackedStreamReaderState::Uninitialized:
             m_state = PackedStreamReaderState::MetadataRead;
-            break;
-        case PackedStreamReaderState::PackedStreamsOpened:
-            m_state = PackedStreamReaderState::PackedStreamsOpenedAndMetadataRead;
             break;
         default:
             throw OperationFailed(ErrorCodeNotReady, __FILE__, __LINE__);
@@ -40,31 +41,41 @@ void PackedStreamReader::read_metadata(ZstdDecompressor& decompressor) {
     }
 }
 
-void PackedStreamReader::open_packed_streams(std::string const& tables_file_path) {
+void PackedStreamReader::open_packed_streams(std::shared_ptr<ArchiveReaderAdaptor> adaptor) {
     switch (m_state) {
-        case PackedStreamReaderState::Uninitialized:
+        case PackedStreamReaderState::MetadataRead:
             m_state = PackedStreamReaderState::PackedStreamsOpened;
             break;
-        case PackedStreamReaderState::MetadataRead:
-            m_state = PackedStreamReaderState::PackedStreamsOpenedAndMetadataRead;
-            break;
+        case PackedStreamReaderState::Uninitialized:
         default:
             throw OperationFailed(ErrorCodeNotReady, __FILE__, __LINE__);
     }
-    m_packed_stream_reader.open(tables_file_path);
+    m_adaptor = adaptor;
+    m_packed_stream_reader = m_adaptor->checkout_reader_for_section(constants::cArchiveTablesFile);
+    if (auto rc = m_packed_stream_reader->try_get_pos(m_begin_offset);
+        clp::ErrorCode::ErrorCode_Success != rc)
+    {
+        throw OperationFailed(static_cast<ErrorCode>(rc), __FILE__, __LINE__);
+    }
 }
 
 void PackedStreamReader::close() {
+    bool needs_checkin{false};
     switch (m_state) {
         case PackedStreamReaderState::PackedStreamsOpened:
-        case PackedStreamReaderState::PackedStreamsOpenedAndMetadataRead:
         case PackedStreamReaderState::ReadingPackedStreams:
+            needs_checkin = true;
             break;
         default:
-            throw OperationFailed(ErrorCodeNotReady, __FILE__, __LINE__);
+            needs_checkin = false;
+            break;
     }
-    m_packed_stream_reader.close();
-    m_prev_stream_id = 0;
+    if (needs_checkin) {
+        m_adaptor->checkin_reader_for_section(constants::cArchiveTablesFile);
+    }
+    m_adaptor.reset();
+    m_prev_stream_id = 0ULL;
+    m_begin_offset = 0ULL;
     m_stream_metadata.clear();
     m_state = PackedStreamReaderState::Uninitialized;
 }
@@ -80,7 +91,7 @@ void PackedStreamReader::read_stream(
     }
 
     switch (m_state) {
-        case PackedStreamReaderState::PackedStreamsOpenedAndMetadataRead:
+        case PackedStreamReaderState::PackedStreamsOpened:
             m_state = PackedStreamReaderState::ReadingPackedStreams;
             break;
         case PackedStreamReaderState::ReadingPackedStreams:
@@ -94,12 +105,20 @@ void PackedStreamReader::read_stream(
     m_prev_stream_id = stream_id;
 
     auto& [file_offset, uncompressed_size] = m_stream_metadata[stream_id];
-    if (auto error = m_packed_stream_reader.try_seek_from_begin(file_offset);
-        ErrorCodeSuccess != error)
+    size_t adjusted_file_offset = m_begin_offset + file_offset;
+    if (auto error = m_packed_stream_reader->try_seek_from_begin(adjusted_file_offset);
+        clp::ErrorCode::ErrorCode_Success != error)
     {
-        throw OperationFailed(error, __FILE__, __LINE__);
+        throw OperationFailed(static_cast<ErrorCode>(error), __FILE__, __LINE__);
     }
-    m_packed_stream_decompressor.open(m_packed_stream_reader, cDecompressorFileReadBufferCapacity);
+
+    size_t end_pos = m_adaptor->get_header().compressed_size;
+    if ((stream_id + 1) < m_stream_metadata.size()) {
+        end_pos = m_begin_offset + m_stream_metadata[stream_id + 1].file_offset;
+    }
+    clp::BoundedReader bounded_reader{m_packed_stream_reader.get(), end_pos};
+
+    m_packed_stream_decompressor.open(bounded_reader, cDecompressorFileReadBufferCapacity);
     if (buf_size < uncompressed_size) {
         // make_shared is supposed to work here for c++20, but it seems like the compiler version
         // we use doesn't support it, so we convert a unique_ptr to a shared_ptr instead.
