c3b26e2270f5a49eec1456405031a4be827933d6
Junhao Liao <junhao@junhao.ca>
2025-01-11 18:53:17 -0500

    Add comment to clarify the proxy target address.

--- components/log-viewer-webui/client/vite.config.ts
+++ components/log-viewer-webui/client/vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
         port: 8080,
         proxy: {
             "/query": {
+                // Below target should match the server's configuration in
+                // `components/log-viewer-webui/server/.env` (or `.env.local` if overridden)
                 target: "http://localhost:3000/query",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/query/, ""),
