b6c8e69d32f8266d947a587da7e03bbc1ad46dda
Haiqi Xu <14502009+haiqi96@users.noreply.github.com>
2025-01-17 17:34:04 -0500

    Resolve simpler comments

--- components/clp-py-utils/clp_py_utils/clp_config.py
+++ components/clp-py-utils/clp_py_utils/clp_config.py
@@ -408,11 +408,11 @@ class StreamFsStorage(FsStorage):
 
 
 class ArchiveS3Storage(S3Storage):
-    staging_directory: pathlib.Path = CLP_DEFAULT_DATA_DIRECTORY_PATH / "staged_archives"
+    staging_directory: pathlib.Path = CLP_DEFAULT_DATA_DIRECTORY_PATH / "staged-archives"
 
 
 class StreamS3Storage(S3Storage):
-    staging_directory: pathlib.Path = CLP_DEFAULT_DATA_DIRECTORY_PATH / "staged_streams"
+    staging_directory: pathlib.Path = CLP_DEFAULT_DATA_DIRECTORY_PATH / "staged-streams"
 
 
 def _get_directory_from_storage_config(storage_config: Union[FsStorage, S3Storage]) -> pathlib.Path:
