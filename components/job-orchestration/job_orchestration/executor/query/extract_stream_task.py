04b94ba2efdd1944caa5f562b995ec2933d637fa
Haiqi Xu <14502009+haiqi96@users.noreply.github.com>
2025-01-17 17:44:17 -0500

    Add CLP prefix to env

--- components/job-orchestration/job_orchestration/executor/query/extract_stream_task.py
+++ components/job-orchestration/job_orchestration/executor/query/extract_stream_task.py
@@ -201,7 +201,7 @@ def extract_stream(
 
         if upload_error:
             task_results.status = QueryTaskStatus.FAILED
-            task_results.error_log_path = str(os.getenv("WORKER_LOG_PATH"))
+            task_results.error_log_path = str(os.getenv("CLP_WORKER_LOG_PATH"))
         else:
             logger.info(f"Finished uploading streams.")
 
