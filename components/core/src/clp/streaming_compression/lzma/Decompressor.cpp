884ef039f854c7c5ad18050a6e409e0bdc5a1d8b
Bingran Hu <bingran.hu@yscope.com>
2025-01-31 06:11:18 -0500

    Add LZMA decompressor interface

---
+++ components/core/src/clp/streaming_compression/lzma/Decompressor.cpp
@@ -0,0 +1,51 @@
+#include "Decompressor.hpp"
+
+#include <cstddef>
+
+#include "../../ErrorCode.hpp"
+#include "../../ReaderInterface.hpp"
+#include "../../TraceableException.hpp"
+
+namespace clp::streaming_compression::lzma {
+[[nodiscard]] auto Decompressor::try_read(
+        [[maybe_unused]] char* buf,
+        [[maybe_unused]] size_t num_bytes_to_read,
+        [[maybe_unused]] size_t& num_bytes_read
+) -> ErrorCode {
+    return ErrorCode_Unsupported;
+}
+
+[[nodiscard]] auto Decompressor::try_seek_from_begin([[maybe_unused]] size_t pos) -> ErrorCode {
+    return ErrorCode_Unsupported;
+}
+
+[[nodiscard]] auto Decompressor::try_get_pos([[maybe_unused]] size_t& pos) -> ErrorCode {
+    return ErrorCode_Unsupported;
+}
+
+auto Decompressor::open(
+        [[maybe_unused]] char const* compressed_data_buf,
+        [[maybe_unused]] size_t compressed_data_buf_size
+) -> void {
+    throw OperationFailed(ErrorCode_Unsupported, __FILENAME__, __LINE__);
+}
+
+auto Decompressor::open(
+        [[maybe_unused]] ReaderInterface& file_reader,
+        [[maybe_unused]] size_t file_read_buffer_capacity
+) -> void {
+    throw OperationFailed(ErrorCode_Unsupported, __FILENAME__, __LINE__);
+}
+
+auto Decompressor::close() -> void {
+    throw OperationFailed(ErrorCode_Unsupported, __FILENAME__, __LINE__);
+}
+
+[[nodiscard]] auto Decompressor::get_decompressed_stream_region(
+        [[maybe_unused]] size_t decompressed_stream_pos,
+        [[maybe_unused]] char* extraction_buf,
+        [[maybe_unused]] size_t extraction_len
+) -> ErrorCode {
+    return ErrorCode_Unsupported;
+}
+}  // namespace clp::streaming_compression::lzma
