9c7e93d3f052cab68dcb38dab2e2c092d81e3714
Bingran Hu <bingran.hu@yscope.com>
2025-03-12 00:38:58 -0400

    Address code review concerns

--- components/core/cmake/Toolchains/utils.cmake
+++ components/core/cmake/Toolchains/utils.cmake
@@ -3,12 +3,12 @@ function(setup_toolchains)
         execute_process(
             COMMAND
                 "sw_vers" "--productVersion"
-            OUTPUT_VARIABLE CMAKE_OSX_VERSION
+            OUTPUT_VARIABLE MACOS_VERSION
             OUTPUT_STRIP_TRAILING_WHITESPACE
         )
-        if("${CMAKE_OSX_VERSION}" VERSION_LESS "15")
+        if("${MACOS_VERSION}" VERSION_LESS "15")
             set(CMAKE_TOOLCHAIN_FILE
-                "${CMAKE_TOOLCHAINS_DIR}/llvm-clang-toolchains.cmake"
+                "${CMAKE_SOURCE_DIR}/cmake/Toolchains/llvm-clang-toolchain.cmake"
                 CACHE STRING
                 "Toolchain file"
             )
@@ -19,22 +19,22 @@ endfunction()
 # Require compiler versions that support the C++20 features necessary for compiling CLP
 function(validate_compiler_versions)
 	if("AppleClang" STREQUAL "${CMAKE_CXX_COMPILER_ID}")
-		set(CLP_CMAKE_CXX_COMPILER_MIN_VERSION "16")
+		set(CXX_COMPILER_MIN_VERSION "16")
 	elseif("Clang" STREQUAL "${CMAKE_CXX_COMPILER_ID}")
-		set(CLP_CMAKE_CXX_COMPILER_MIN_VERSION "16")
+		set(CXX_COMPILER_MIN_VERSION "16")
 	elseif("GNU" STREQUAL "${CMAKE_CXX_COMPILER_ID}")
-		set(CLP_CMAKE_CXX_COMPILER_MIN_VERSION "11")
+		set(CXX_COMPILER_MIN_VERSION "11")
 	else()
 		message(
 			FATAL_ERROR
 			"Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}. Please use AppleClang, Clang, or GNU."
 		)
 	endif()
-	if("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS "${CLP_CMAKE_CXX_COMPILER_MIN_VERSION}")
+	if("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS "${CXX_COMPILER_MIN_VERSION}")
 		message(
 			FATAL_ERROR
 			"${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION} is too low. Must be at \
-			least ${CLP_CMAKE_CXX_COMPILER_MIN_VERSION}."
+			least ${CXX_COMPILER_MIN_VERSION}."
 		)
 	endif()
 endfunction()
