48f6376aa28e6736173b5049f1b2be48c14a3f9c
Bingran Hu <bingranhu98@gmail.com>
2025-03-20 03:50:15 -0400

    Apply suggestions from code review
    Co-authored-by: davidlion <davidlion2@protonmail.com>

--- components/core/src/clp/ffi/ir_stream/IrErrorCode.hpp
+++ components/core/src/clp/ffi/ir_stream/IrErrorCode.hpp
@@ -17,7 +17,6 @@ enum class IrErrorCodeEnum : uint8_t {
 };
 
 using IrErrorCode = ystdlib::error_handling::ErrorCode<IrErrorCodeEnum>;
-using IrErrorCategory = ystdlib::error_handling::ErrorCategory<IrErrorCodeEnum>;
 }  // namespace clp::ffi::ir_stream
 
 YSTDLIB_ERROR_HANDLING_MARK_AS_ERROR_CODE_ENUM(clp::ffi::ir_stream::IrErrorCodeEnum);
