e2029173788db3ae9c36ac3fbece2ea2b0234815
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-03-20 19:14:30 -0400

    Update docs/conf/conf.py
    Co-authored-by: Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>

--- docs/conf/conf.py
+++ docs/conf/conf.py
@@ -48,8 +48,8 @@ html_theme = "pydata_sphinx_theme"
 # -- sphinxcontrib-mermaid options ---------------------------------------------
 # https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest/
 
-mermaid_version = "11.5.0"
 mermaid_include_elk = "0.1.7"
+mermaid_version = "11.5.0"
 
 # -- Theme options -------------------------------------------------------------
 # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/layout.html
