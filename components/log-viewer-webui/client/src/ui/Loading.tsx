fc1fe7cdf0a3fefcec75b8f3262aa54cf7361fb1
Junhao Liao <junhao.liao@yscope.com>
2025-03-23 21:56:46 -0400

    build(log-viewer-webui-client): Switch from Webpack to Vite for bundling; Convert the codebase to TypeScript. (#645)
    Co-authored-by: davemarco <83603688+davemarco@users.noreply.github.com>

--- components/log-viewer-webui/client/src/ui/Loading.tsx
+++ components/log-viewer-webui/client/src/ui/Loading.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import {
     Box,
     LinearProgress,
@@ -7,25 +9,36 @@ import {
     Stepper,
     Typography,
 } from "@mui/joy";
+import {DefaultColorPalette} from "@mui/joy/styles/types";
 
+import {Nullable} from "../typings/common";
 import {
+    QUERY_LOADING_STATE,
     QUERY_LOADING_STATE_DESCRIPTIONS,
-    QUERY_LOADING_STATES,
-} from "../typings/query.js";
+    QUERY_LOADING_STATE_VALUES,
+} from "../typings/query";
 
 import "./Loading.css";
 
 
+interface LoadingStepProps {
+    description: string;
+    isActive: boolean;
+    isError: boolean;
+    label: string;
+    stepIndicatorText: number | string;
+}
+
 /**
  * Renders a step with a label and description.
  *
- * @param {object} props
- * @param {string} props.description
- * @param {boolean} props.isActive
- * @param {boolean} props.isError
- * @param {string} props.label
- * @param {number | string} props.stepIndicatorText
- * @return {React.ReactElement}
+ * @param props
+ * @param props.description
+ * @param props.isActive
+ * @param props.isError
+ * @param props.label
+ * @param props.stepIndicatorText
+ * @return
  */
 const LoadingStep = ({
     description,
@@ -33,8 +46,8 @@ const LoadingStep = ({
     isError,
     label,
     stepIndicatorText,
-}) => {
-    let color = isActive ?
+}: LoadingStepProps) => {
+    let color: DefaultColorPalette = isActive ?
         "primary" :
         "neutral";
 
@@ -68,17 +81,25 @@ const LoadingStep = ({
     );
 };
 
+interface LoadingProps {
+    currentState: QUERY_LOADING_STATE;
+    errorMsg: Nullable<string>;
+}
+
 /**
  * Displays status of a pending query job.
  *
- * @param {object} props
- * @param {QueryLoadState} props.currentState
- * @param {string} props.errorMsg
- * @return {React.ReactElement}
+ * @param props
+ * @param props.currentState
+ * @param props.errorMsg
+ * @return
  */
-const Loading = ({currentState, errorMsg}) => {
-    const steps = [];
-    Object.values(QUERY_LOADING_STATES).forEach((state) => {
+const Loading = ({
+    currentState,
+    errorMsg,
+}:LoadingProps) => {
+    const steps: React.ReactNode[] = [];
+    QUERY_LOADING_STATE_VALUES.forEach((state) => {
         const isActive = (currentState === state);
         const stateDescription = QUERY_LOADING_STATE_DESCRIPTIONS[state];
         steps.push(
@@ -104,26 +125,24 @@ const Loading = ({currentState, errorMsg}) => {
     });
 
     return (
-        <>
-            <Sheet className={"loading-sheet"}>
-                <Box className={"loading-progress-container"}>
-                    <LinearProgress
-                        determinate={null !== errorMsg}
-                        color={null === errorMsg ?
-                            "primary" :
-                            "danger"}/>
-                </Box>
-                <Box className={"loading-stepper-container"}>
-                    <Stepper
-                        className={"loading-stepper"}
-                        orientation={"vertical"}
-                        size={"lg"}
-                    >
-                        {steps}
-                    </Stepper>
-                </Box>
-            </Sheet>
-        </>
+        <Sheet className={"loading-sheet"}>
+            <Box className={"loading-progress-container"}>
+                <LinearProgress
+                    determinate={null !== errorMsg}
+                    color={null === errorMsg ?
+                        "primary" :
+                        "danger"}/>
+            </Box>
+            <Box className={"loading-stepper-container"}>
+                <Stepper
+                    className={"loading-stepper"}
+                    orientation={"vertical"}
+                    size={"lg"}
+                >
+                    {steps}
+                </Stepper>
+            </Box>
+        </Sheet>
     );
 };
 
