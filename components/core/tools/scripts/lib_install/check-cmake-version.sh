9ecc8681e07dbbdd2aa70304ec956091e7a711ef
Bingran Hu <bingran.hu@yscope.com>
2025-04-02 00:49:47 -0400

    Use cmake -E capabilities which does not rely on custom parsing

--- components/core/tools/scripts/lib_install/check-cmake-version.sh
+++ components/core/tools/scripts/lib_install/check-cmake-version.sh
@@ -7,12 +7,12 @@ set -e
 set -u
 
 # Get the installed cmake version string
-cmake_version=$(cmake --version | head --lines 1 | awk '{print $3}')
-cmake_major_version=$(echo "$cmake_version" | cut -d. -f1)
+cmake_version=$(cmake -E capabilities | jq ".version.string")
+cmake_major_version=$(cmake -E capabilities | jq ".version.major")
 
 # Check if version is 4.0 or higher
 # shellcheck disable=SC2071
-if ! [[ "$cmake_version" < "4" ]]; then
+if ! [[ "$cmake_major_version" < "4" ]]; then
   echo "CMake version $cmake_version is currently unsupported (>= 4.0)."
   exit 1
 fi
