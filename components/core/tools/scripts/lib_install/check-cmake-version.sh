cb88d876f03a46b25e2f929aa408509b8a9ee157
Bingran Hu <bingran.hu@yscope.com>
2025-04-02 11:53:11 -0400

    Use number comparison

--- components/core/tools/scripts/lib_install/check-cmake-version.sh
+++ components/core/tools/scripts/lib_install/check-cmake-version.sh
@@ -11,8 +11,7 @@ cmake_version=$(cmake -E capabilities | jq --raw-output ".version.string")
 cmake_major_version=$(cmake -E capabilities | jq --raw-output ".version.major")
 
 # Check if version is 4.0 or higher
-# shellcheck disable=SC2071
-if ! [[ "$cmake_major_version" < "4" ]]; then
+if [[ "$cmake_major_version" -ge "4" ]]; then
   echo "CMake version $cmake_version is currently unsupported (>= 4.0)."
   exit 1
 fi
