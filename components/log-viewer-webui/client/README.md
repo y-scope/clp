3ecb02ca425518b6538255c1ef081da04ed497fc
davemarco <83603688+davemarco@users.noreply.github.com>
2025-04-04 10:44:28 -0400

    feat(webui): Adds a switch to enable new Ant Design webui and a basic menu. (#791)

---
+++ components/log-viewer-webui/client/README.md
@@ -0,0 +1,13 @@
+# Log Viewer WebUI Client
+
+### Start a Dev Server
+Run:
+```bash
+npm run start
+```
+
+### Start a Dev Server for the New WebUI (Under Development)
+Run:
+```bash
+npm run antd
+```
---
+++ components/log-viewer-webui/client/src/AntdApp.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import MainLayout from "./ui/MainLayout";
+
+
+/**
+ * Renders Web UI app.
+ *
+ * @return
+ */
+const AntApp = () => {
+    return (
+        <MainLayout/>
+    );
+};
+
+export default AntApp;
