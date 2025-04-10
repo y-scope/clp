1b29ad2e5e178aa22df1b4c0fd2b80df32a89a9e
AVMatthews <abigail.v.matthews@gmail.com>
2025-04-10 16:00:33 -0400

    fix file changes

--- components/log-viewer-webui/client/vite.config.ts
+++ components/log-viewer-webui/client/vite.config.ts
@@ -6,19 +6,13 @@ import {defineConfig} from "vite";
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: "0.0.0.0",
-	port: 8081,
+        port: 8080,
         proxy: {
             "/query/": {
                 // Below target should match the server's configuration in
                 // `components/log-viewer-webui/server/.env` (or `.env.local` if overridden)
                 target: "http://localhost:3000/",
                 changeOrigin: true,
-            },
-            "/socket.io/": {
-                target: "ws://localhost:3000/",
-                changeOrigin: true,
-                ws: true,
             },
         },
     },
