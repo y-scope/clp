eebbcd20b177d1ffd2290f40a2a94cc47bae8ac5
gibber9809 <devinbook1@gmail.com>
2025-04-17 18:27:14 +0000

    Add RangeIndexWriter class to manage details of writing archive range index

---
+++ components/core/src/clp_s/RangeIndexWriter.cpp
@@ -0,0 +1,108 @@
+#include "RangeIndexWriter.hpp"
+
+#include <algorithm>
+#include <cstdint>
+#include <exception>
+#include <string>
+#include <system_error>
+#include <vector>
+
+#include <json/single_include/nlohmann/json.hpp>
+#include <outcome/single-header/outcome.hpp>
+
+#include "../clp/WriterInterface.hpp"
+#include "ErrorCode.hpp"
+#include "SingleFileArchiveDefs.hpp"
+
+namespace clp_s {
+auto RangeIndexWriter::open_range(size_t start_index)
+        -> OUTCOME_V2_NAMESPACE::std_result<handle_t> {
+    for (auto it = m_ranges.begin(); m_ranges.end() != it; ++it) {
+        if (start_index <= it->start_index || false == it->end_index.has_value()
+            || start_index < it->end_index.value())
+        {
+            return std::errc::invalid_argument;
+        }
+    }
+    handle_t handle{m_ranges.size()};
+    m_ranges.emplace_back(start_index);
+    return handle;
+}
+
+template <typename T>
+auto RangeIndexWriter::add_value_to_range(handle_t handle, std::string const& key, T const& value)
+        -> ErrorCode {
+    if (handle >= m_ranges.size()) {
+        return ErrorCodeOutOfBounds;
+    }
+
+    auto& range = m_ranges.at(handle);
+    if (range.end_index.has_value()) {
+        return ErrorCodeNotReady;
+    }
+
+    auto& fields = range.fields;
+    if (auto it = fields.find(key); fields.end() != it) {
+        return ErrorCodeBadParam;
+    }
+
+    fields.emplace(key, value);
+    return ErrorCodeSuccess;
+}
+
+auto RangeIndexWriter::close_range(handle_t handle, size_t end_index) -> ErrorCode {
+    if (handle >= m_ranges.size()) {
+        return ErrorCodeOutOfBounds;
+    }
+
+    auto& range = m_ranges.at(handle);
+    if (range.end_index.has_value()) {
+        return ErrorCodeNotReady;
+    }
+
+    if (end_index < range.start_index) {
+        return ErrorCodeBadParam;
+    }
+
+    range.end_index = end_index;
+    return ErrorCodeSuccess;
+}
+
+auto RangeIndexWriter::write(clp::WriterInterface& writer) -> ErrorCode {
+    if (m_ranges.empty()) {
+        return ErrorCodeSuccess;
+    }
+
+    nlohmann::json ranges_array{nlohmann::json::array()};
+    for (auto it = m_ranges.begin(); m_ranges.end() != it; ++it) {
+        if (false == it->end_index.has_value()) {
+            return ErrorCodeNotReady;
+        }
+
+        nlohmann::json obj;
+        obj["s"] = it->start_index;
+        obj["e"] = it->end_index.value();
+
+        nlohmann::json fields_obj;
+        for (auto field_it = it->fields.begin(); it->fields.end() != field_it; ++field_it) {
+            fields_obj[field_it->first] = std::move(field_it->second);
+        }
+        obj["f"] = std::move(fields_obj);
+
+        ranges_array.emplace_back(std::move(obj));
+    }
+
+    std::vector<char> serialized_range_index;
+    nlohmann::json::to_msgpack(ranges_array, serialized_range_index);
+    try {
+        writer.write_numeric_value(ArchiveMetadataPacketType::RangeIndex);
+        writer.write_numeric_value<uint32_t>(serialized_range_index.size());
+        writer.write(serialized_range_index.data(), serialized_range_index.size());
+    } catch (std::exception const& e) {
+        return ErrorCodeFailure;
+    }
+    m_ranges.clear();
+
+    return ErrorCodeSuccess;
+}
+}  // namespace clp_s
---
+++ components/core/src/clp_s/RangeIndexWriter.hpp
@@ -0,0 +1,83 @@
+#ifndef CLP_S_RANGEINDEXWRITER_HPP
+#define CLP_S_RANGEINDEXWRITER_HPP
+
+#include <cstdint>
+#include <map>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <json/single_include/nlohmann/json.hpp>
+#include <outcome/single-header/outcome.hpp>
+
+#include "../clp/WriterInterface.hpp"
+#include "ErrorCode.hpp"
+
+namespace clp_s {
+/**
+ * This class is responsible for constructing and writing a range index associated with an archive.
+ *
+ * The range index is written as a msgpack object structured as follows:
+ *
+ * [
+ *  {"s": <idx>, "e": <idx>, "f": {<key value pairs>}},
+ *  ...
+ * ]
+ *
+ * Where each object in the array holds key value pairs corresponding to a range [s, e). Note that
+ * in the current implementation ranges are not allowed to overlap, and that ranges containing no
+ * records (i.e. s == e) are legal.
+ *
+ */
+class RangeIndexWriter {
+public:
+    // Types
+    using handle_t = size_t;
+
+    /**
+     * Opens a new range starting at start_index.
+     * @param start_index
+     * @return A handle to the newly opened range on success or the relevant error code on failure.
+     */
+    auto open_range(size_t start_index) -> OUTCOME_V2_NAMESPACE::std_result<handle_t>;
+
+    /**
+     * Adds a key value pair to the open range indicated by `handle`.
+     * @param handle
+     * @param key
+     * @param value
+     * @return ErrorCodeSuccess on success or the relevant error code on failure.
+     */
+    template <typename T>
+    auto add_value_to_range(handle_t handle, std::string const& key, T const& value) -> ErrorCode;
+
+    /**
+     * Closes the open range indicated by `handle`.
+     * @param handle
+     * @param end_index
+     * @return ErrorCodeSuccess on success or the relvant error code on failure.
+     */
+    auto close_range(handle_t handle, size_t end_index) -> ErrorCode;
+
+    /**
+     * Writes ranges to a `clp::WriterInterface` then clears internal state.
+     * @param writer
+     * @return ErrorCodeSuccess on success or the relvant error code on failure.
+     */
+    auto write(clp::WriterInterface& writer) -> ErrorCode;
+
+private:
+    // Types
+    struct Range {
+        explicit Range(size_t start) : start_index{start} {}
+
+        size_t start_index{0ULL};
+        std::optional<size_t> end_index{std::nullopt};
+        std::map<std::string, nlohmann::json> fields;
+    };
+
+    std::vector<Range> m_ranges;
+};
+}  // namespace clp_s
+
+#endif  // CLP_S_RANGEINDEXWRITER_HPP
