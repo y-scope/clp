27a188adce3066513ef4262a2b05aa4526fcf558
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-04-27 19:21:43 -0400

    Add Ast evaluation result.

---
+++ components/core/src/clp/ffi/ir_stream/search/AstEvaluationResult.hpp
@@ -0,0 +1,23 @@
+#ifndef CLP_FFI_IR_STREAM_SEARCH_ASTEVALUATIONRESULT_HPP
+#define CLP_FFI_IR_STREAM_SEARCH_ASTEVALUATIONRESULT_HPP
+
+#include <cstdint>
+
+namespace clp::ffi::ir_stream::search {
+/**
+ * Enum representing the result of evaluating a search AST.
+ *
+ * Possible values:
+ * - True:   The AST evaluates to `true`.
+ * - False:  The AST evaluates to `false`.
+ * - Pruned: The AST evaluation is intentionally skipped because it belongs to a pruned branch of
+ *           the parent tree.
+ */
+enum class AstEvaluationResult : uint8_t {
+    True,
+    False,
+    Pruned,
+};
+}
+
+#endif // CLP_FFI_IR_STREAM_SEARCH_ASTEVALUATIONRESULT_HPP
