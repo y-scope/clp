88327aa89a8c4aa3fae818a3212e79598e0b6cca
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-04-27 13:08:02 -0400

    Update naming

--- components/core/src/clp/ffi/ir_stream/search/NewProjectedSchemaTreeNodeCallbackReq.hpp
+++ components/core/src/clp/ffi/ir_stream/search/NewProjectedSchemaTreeNodeCallbackReq.hpp
@@ -1,5 +1,5 @@
-#ifndef CLP_FFI_IR_STREAM_SEARCH_PROJECTIONRESOLUTIONCALLBACKREQ_HPP
-#define CLP_FFI_IR_STREAM_SEARCH_PROJECTIONRESOLUTIONCALLBACKREQ_HPP
+#ifndef CLP_FFI_IR_STREAM_SEARCH_NEWPROJECTEDSCHEMATREENODECALLBACKREQ_HPP
+#define CLP_FFI_IR_STREAM_SEARCH_NEWPROJECTEDSCHEMATREENODECALLBACKREQ_HPP
 
 #include <string_view>
 #include <type_traits>
@@ -12,22 +12,22 @@ namespace clp::ffi::ir_stream::search {
 /**
  * Requirements for a callback that's called whenever:
  *
- * 1. a new schema-tree node is read from the IR stream; and
+ * 1. a new schema-tree node is deserialized from the IR stream; and
  * 2. that node corresponds to one of the projected key paths in the query.
  *
- * @tparam ProjectionResolutionCallbackType
+ * @tparam NewProjectedSchemaTreeNodeCallbackType
  * @param arg_0 Whether the schema-tree node is for an auto-generated kv-pair.
  * @param arg_1 The ID of the schema-tree node.
  * @param arg_2 The projected key path.
  * @return A void result on success or a user-defined error code indicating the failure.
  */
-template <typename ProjectionResolutionCallbackType>
-concept ProjectionResolutionCallbackReq = std::is_invocable_r_v<
+template <typename NewProjectedSchemaTreeNodeCallbackType>
+concept NewProjectedSchemaTreeNodeCallbackReq = std::is_invocable_r_v<
         outcome_v2::std_result<void>,
-        ProjectionResolutionCallbackType,
+        NewProjectedSchemaTreeNodeCallbackType,
         bool,
         SchemaTree::Node::id_t,
         std::string_view>;
 }  // namespace clp::ffi::ir_stream::search
 
-#endif  // CLP_FFI_IR_STREAM_SEARCH_PROJECTIONRESOLUTIONCALLBACKREQ_HPP
+#endif  // CLP_FFI_IR_STREAM_SEARCH_NEWPROJECTEDSCHEMATREENODECALLBACKREQ_HPP
