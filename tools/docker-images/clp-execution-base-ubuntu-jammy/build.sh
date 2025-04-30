1f70d17463db65785ceb36eca8b8794e8953e81c
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-04-30 13:24:36 -0400

    feat: Add revision & source labels to locally-built docker container images. (#869)

--- tools/docker-images/clp-execution-base-ubuntu-jammy/build.sh
+++ tools/docker-images/clp-execution-base-ubuntu-jammy/build.sh
@@ -20,5 +20,19 @@ else
     exit 1
 fi
 
-docker build -t clp-execution-${arch_name}-ubuntu-jammy:dev ${repo_root} \
-    --file ${script_dir}/Dockerfile
+build_cmd=(
+    docker build
+    --tag clp-execution-${arch_name}-ubuntu-jammy:dev
+    "$repo_root"
+    --file "${script_dir}/Dockerfile"
+)
+
+if command -v git >/dev/null && git -C "$script_dir" rev-parse --is-inside-work-tree >/dev/null ;
+then
+    build_cmd+=(
+        --label "org.opencontainers.image.revision=$(git -C "$script_dir" rev-parse HEAD)"
+        --label "org.opencontainers.image.source=$(git -C "$script_dir" remote get-url origin)"
+    )
+fi
+
+"${build_cmd[@]}"
