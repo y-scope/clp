dd76117e61a6d26dc86033142d81756183ac3b0e
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-04-30 10:25:47 -0400

    Use -C instead of pushd/popd based on the rabbit's recommendation.

--- tools/docker-images/clp-execution-base-ubuntu-jammy/build.sh
+++ tools/docker-images/clp-execution-base-ubuntu-jammy/build.sh
@@ -27,13 +27,12 @@ build_cmd=(
     --file "${script_dir}/Dockerfile"
 )
 
-pushd "$script_dir"
-if command -v git && git rev-parse --is-inside-work-tree >/dev/null ; then
+if command -v git && git -C "$script_dir" rev-parse --is-inside-work-tree >/dev/null ; then
     build_cmd+=(
-        --label "org.opencontainers.image.revision=$(git rev-parse HEAD)"
-        --label "org.opencontainers.image.source=$(git config --get remote.origin.url)"
+        --label "org.opencontainers.image.revision=$(git -C "$script_dir" rev-parse HEAD)"
+        --label
+        "org.opencontainers.image.source=$(git -C "$script_dir" config --get remote.origin.url)"
     )
 fi
-popd
 
 "${build_cmd[@]}"
