24d2bdc18017e4d70d63b430e6d4e8a228c0df4f
AVMatthews <abigail.v.matthews@gmail.com>
2025-04-30 12:51:08 -0400

    refactor and fix missing updates

---
+++ components/log-viewer-webui/server/src/plugins/MongoSocketIoServer/typings.ts
@@ -0,0 +1,151 @@
+import {
+    ChangeStream,
+    Document,
+    Filter,
+    FindOptions,
+} from "mongodb";
+import {Socket} from "socket.io";
+
+
+/**
+ * Error response to event.
+ */
+interface Err {
+    error: string;
+    queryId?: number;
+}
+
+/**
+ * Success response to event.
+ */
+interface Success<T> {
+    data: T;
+}
+
+/**
+ * Event response.
+ */
+type Response<T> = Err | Success<T>;
+
+/**
+ * Events that the client can emit to the server.
+ */
+type ClientToServerEvents = {
+    "disconnect": () => void;
+    "collection::init": (
+        requestArgs: {
+            collectionName: string;
+        },
+        callback: (res: Response<void>) => void
+    ) => void;
+    "collection::find::toArray": (
+        requestArgs: {
+            query: Filter<Document>;
+            options: FindOptions;
+        },
+        callback: (res: Response<{data: Document[]}>) => void) => void;
+    "collection::find::toReactiveArray": (
+        requestArgs: {
+            query: Filter<Document>;
+            options: FindOptions;
+        },
+        callback: (res: Response<{queryId: number}>) => void) => void;
+    "collection::find::unsubscribe": (
+        requestArgs: {
+            queryId: number;
+        }
+    ) => Promise<void>;
+};
+
+/**
+ * Events that the server can emit to the client.
+ */
+interface ServerToClientEvents {
+    "collection::find::update": (respArgs: {
+        queryId: number;
+        data: Document[];
+    }) => void;
+}
+
+/**
+ * Collection associated with each socket connection.
+ */
+interface SocketData {
+    collectionName: string;
+}
+
+/**
+ * Empty but required by Socket IO.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface InterServerEvents {
+}
+
+/**
+ * Custom socket type for Mongo Socket IO server.
+ */
+type MongoCustomSocket = Socket<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>;
+
+/**
+ * Unique ID for each active unique query. Multiple clients can subscribe to the same ID if the
+ * queries are identical. The ID is also used to represent the socket room, and Mongo change stream.
+ */
+type QueryId = number;
+
+/**
+ * Unique ID to repsent each socket connection.
+ */
+type ConnectionId = string;
+
+
+/**
+ * Parameters for MongoDB queries.
+ */
+interface QueryParameters {
+    collectionName: string;
+    query: Filter<Document>;
+    options: FindOptions;
+}
+
+/**
+ * Options to connect to MongoDb database.
+ */
+interface DbOptions {
+    // Name of database.
+    database: string;
+    host: string;
+    port: number;
+}
+
+/**
+ * Timeout for emitting updates to the client.
+ */
+const CLIENT_UPDATE_TIMEOUT_MS = 500;
+
+/**
+ * MongoDb change stream for a query, and a list of subscribed connections.
+ */
+interface Watcher {
+    changeStream: ChangeStream;
+    subscribers: ConnectionId[];
+}
+
+export {
+    CLIENT_UPDATE_TIMEOUT_MS,
+    ClientToServerEvents,
+    ConnectionId,
+    DbOptions,
+    InterServerEvents,
+    MongoCustomSocket,
+    QueryId,
+    QueryParameters,
+    Response,
+    ServerToClientEvents,
+    SocketData,
+    Watcher,
+};
