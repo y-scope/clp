8f51308f674ca430ef2ab77fb956fedf19356ff3
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-05-07 13:39:03 -0400

    WIP

--- components/core/src/clp/ffi/ir_stream/search/test/test_QueryHandlerImpl.cpp
+++ components/core/src/clp/ffi/ir_stream/search/test/test_QueryHandlerImpl.cpp
@@ -448,4 +448,43 @@ TEST_CASE("query_handler_handle_projection", "[ffi][ir_stream][search][QueryHand
 
     REQUIRE((expected_resolved_projections == actual_resolved_projections));
 }
+
+TEST_CASE("query_handler_evaluation_kv_pair_log_event", "[ffi][ir_stream][search][QueryHandler]") {
+    /*
+     * <0:root:Obj>
+     *      |
+     *      |------------> <1:a:Obj>
+     *      |                  |
+     *      |--> <2:b:Int>     |--> <3:b:Obj>
+     *      |                  |        |
+     *      |--> <12:a:Int>    |        |------------> <4:c:Obj>
+     *                         |        |                  |
+     *                         |        |--> <5:d:Str>     |--> <7:a:Str>
+     *                         |        |                  |
+     *                         |        |--> <6:d:Bool>    |--> <8:d:Str>
+     *                         |        |                  |
+     *                         |        |--> <10:e:Obj>    |--> <9:d:Float>
+     *                         |                           |
+     *                         |--> <13:b:Bool>            |--> <11:f:Obj>
+     */
+    auto const schema_tree{std::make_shared<SchemaTree>()};
+    std::vector<SchemaTree::NodeLocator> const locators{
+            {SchemaTree::cRootId, "a", SchemaTree::Node::Type::Obj},
+            {SchemaTree::cRootId, "b", SchemaTree::Node::Type::Int},
+            {1, "b", SchemaTree::Node::Type::Obj},
+            {3, "c", SchemaTree::Node::Type::Obj},
+            {3, "d", SchemaTree::Node::Type::Str},
+            {3, "d", SchemaTree::Node::Type::Bool},
+            {4, "a", SchemaTree::Node::Type::Str},
+            {4, "d", SchemaTree::Node::Type::Str},
+            {4, "d", SchemaTree::Node::Type::Float},
+            {3, "e", SchemaTree::Node::Type::Obj},
+            {4, "f", SchemaTree::Node::Type::Obj},
+            {SchemaTree::cRootId, "a", SchemaTree::Node::Type::Int},
+            {1, "b", SchemaTree::Node::Type::Bool}
+    };
+    for (auto const& locator : locators) {
+        REQUIRE_NOTHROW(schema_tree->insert_node(locator));
+    }
+}
 }  // namespace clp::ffi::ir_stream::search::test
