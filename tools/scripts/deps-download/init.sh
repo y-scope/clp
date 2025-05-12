87a5053c1195ad99ecb22b2f6dd42c3775395358
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-05-12 10:46:58 -0400

    Address the rabbit's review.

--- tools/scripts/deps-download/init.sh
+++ tools/scripts/deps-download/init.sh
@@ -10,8 +10,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 project_root_dir="$script_dir/../../../"
 download_dep_script="$script_dir/download-dep.py"
 
+yscope_dev_utils_commit_sha="2693d8b69050b8d48f96ae60cfce7be8626d3875"
 python3 "${download_dep_script}" \
-    https://github.com/y-scope/yscope-dev-utils/archive/2693d8b.zip \
-    yscope-dev-utils-2693d8b69050b8d48f96ae60cfce7be8626d3875 \
+    "https://github.com/y-scope/yscope-dev-utils/archive/${yscope_dev_utils_commit_sha}.zip" \
+    "yscope-dev-utils-${yscope_dev_utils_commit_sha}" \
     "${project_root_dir}/tools/yscope-dev-utils" \
     --extract
