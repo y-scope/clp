8c7df3d287bf74280471f57df2178b98afe78bde
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-05-12 21:15:14 -0400

    ci(lint): Relax clang-tidy's `misc-include-cleaner` check to a warning, to unblock resolving other clang-tidy violations. (#896)

--- tools/scripts/deps-download/init.sh
+++ tools/scripts/deps-download/init.sh
@@ -10,8 +10,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 project_root_dir="$script_dir/../../../"
 download_dep_script="$script_dir/download-dep.py"
 
+readonly YSCOPE_DEV_UTILS_COMMIT_SHA="2693d8b69050b8d48f96ae60cfce7be8626d3875"
 python3 "${download_dep_script}" \
-    https://github.com/y-scope/yscope-dev-utils/archive/56740d3.zip \
-    yscope-dev-utils-56740d33164f80c00daef37542a27dffcef3a7e1 \
+    "https://github.com/y-scope/yscope-dev-utils/archive/${YSCOPE_DEV_UTILS_COMMIT_SHA}.zip" \
+    "yscope-dev-utils-${YSCOPE_DEV_UTILS_COMMIT_SHA}" \
     "${project_root_dir}/tools/yscope-dev-utils" \
     --extract
