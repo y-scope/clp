3be40cadeb81bf8a24f614c910d900145e99115b
Marco <david.marcovitch@yscope.com>
2025-05-15 14:48:23 +0000

    latest

---
+++ components/log-viewer-webui/server/src/fastify-v2/routes/api/search/typings.ts
@@ -0,0 +1,37 @@
+import {
+    FastifyBaseLogger,
+    FastifyInstance,
+} from "fastify";
+
+import {SEARCH_SIGNAL} from "../../../plugins/app/search/constants.js";
+import {SearchResultsMetadataDocument} from "../../../plugins/app/search/SearchResultsMetadataCollection/typings.js";
+
+
+type UpdateSearchResultsMetaProps = {
+    jobId: number;
+    lastSignal: SEARCH_SIGNAL;
+    SearchResultsMetadataCollection: FastifyInstance["SearchResultsMetadataCollection"];
+    logger: FastifyBaseLogger;
+    fields: Partial<SearchResultsMetadataDocument>;
+};
+
+type UpdateSearchSignalWhenJobsFinishProps = {
+    searchJobId: number;
+    aggregationJobId: number;
+    queryJobsDbManager: FastifyInstance["QueryJobsDbManager"];
+    searchJobCollectionsManager: FastifyInstance["SearchJobCollectionsManager"];
+    SearchResultsMetadataCollection: FastifyInstance["SearchResultsMetadataCollection"];
+    logger: FastifyBaseLogger;
+};
+
+type CreateMongoIndexesProps = {
+    searchJobId: number;
+    searchJobCollectionsManager: FastifyInstance["SearchJobCollectionsManager"];
+    logger: FastifyBaseLogger;
+};
+
+export {
+    CreateMongoIndexesProps,
+    UpdateSearchResultsMetaProps,
+    UpdateSearchSignalWhenJobsFinishProps,
+};
