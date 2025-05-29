665d58960191e5f15b96877a494a8a49f34cf919
Marco <david.marcovitch@yscope.com>
2025-05-29 02:36:19 +0000

    latest

--- components/log-viewer-webui/client/src/router.tsx
+++ components/log-viewer-webui/client/src/router.tsx
@@ -3,7 +3,7 @@ import {createBrowserRouter} from "react-router";
 import MainLayout from "./components/Layout/MainLayout";
 import IngestPage from "./pages/IngestPage";
 import SearchPage from "./pages/SearchPage";
-
+import QueryStatus from "./ui/QueryStatus";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
             {path: "ingest", Component: IngestPage},
             {path: "search", Component: SearchPage},
         ],
+    },
+    {
+    path: "/stream",
+        Component: QueryStatus,
     },
 ]);
 
