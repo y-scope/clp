2859dea8c3149d7c97534ed3e36b27da30d2b41b
Marco <david.marcovitch@yscope.com>
2025-06-09 18:52:51 +0000

    fix

--- components/log-viewer-webui/client/src/components/VirtualTable/typings.tsx
+++ components/log-viewer-webui/client/src/components/VirtualTable/typings.tsx
@@ -4,14 +4,20 @@ import type {TableProps} from "antd";
 /**
  * Number of pixels to scroll vertically when using keyboard arrow navigation.
  */
-export const SCROLL_INCREMENT = 32;
+const SCROLL_INCREMENT = 32;
 
 /**
  * CSS selector for the virtual table body element.
  */
-export const VIRTUAL_TABLE_HOLDER_SELECTOR = ".ant-table-tbody-virtual-holder";
+const VIRTUAL_TABLE_HOLDER_SELECTOR = ".ant-table-tbody-virtual-holder";
 
 /**
  * Antd Table props with virtual omitted since set by VirtualTable.
  */
-export type VirtualTableProps<RecordType> = Omit<TableProps<RecordType>, "virtual">;
+type VirtualTableProps<RecordType> = Omit<TableProps<RecordType>, "virtual">;
+
+export {
+    SCROLL_INCREMENT,
+    VIRTUAL_TABLE_HOLDER_SELECTOR,
+};
+export type {VirtualTableProps};
