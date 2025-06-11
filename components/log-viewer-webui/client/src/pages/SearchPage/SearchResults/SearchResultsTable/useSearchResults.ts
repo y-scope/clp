af84f8915919471e013723c191e8da7be5787fd3
Marco <david.marcovitch@yscope.com>
2025-06-11 16:05:30 +0000

    latest

--- components/log-viewer-webui/client/src/pages/SearchPage/SearchResults/SearchResultsTable/useSearchResults.ts
+++ components/log-viewer-webui/client/src/pages/SearchPage/SearchResults/SearchResultsTable/useSearchResults.ts
@@ -31,7 +31,7 @@ const useSearchResults = () => {
                         "desc",
                     ],
                     [
-                        "id",
+                        "_id",
                         "desc",
                     ],
                 ],
