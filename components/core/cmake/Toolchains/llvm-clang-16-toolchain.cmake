693e2d03d5f69862c519a49ac49c9979b7376ff8
sitaowang1998 <sitaowang1998@outlook.com>
2025-06-28 12:58:34 -0400

    Revert "Not use llvm toolchain in clp"
    This reverts commit c91dc84196208537e084c5eb9de74dce26a6721c.

--- components/core/cmake/Toolchains/llvm-clang-16-toolchain.cmake
+++ components/core/cmake/Toolchains/llvm-clang-16-toolchain.cmake
@@ -17,5 +17,5 @@ endif()
 
 set(CMAKE_C_COMPILER "${LLVM_TOOLCHAIN_PREFIX}/bin/clang")
 set(CMAKE_CXX_COMPILER "${LLVM_TOOLCHAIN_PREFIX}/bin/clang++")
-#set(CMAKE_AR "${LLVM_TOOLCHAIN_PREFIX}/bin/llvm-ar")
-#set(CMAKE_RANLIB "${LLVM_TOOLCHAIN_PREFIX}/bin/llvm-ranlib")
+set(CMAKE_AR "${LLVM_TOOLCHAIN_PREFIX}/bin/llvm-ar")
+set(CMAKE_RANLIB "${LLVM_TOOLCHAIN_PREFIX}/bin/llvm-ranlib")
