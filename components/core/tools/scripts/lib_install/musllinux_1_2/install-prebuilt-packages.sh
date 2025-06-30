06c71478464006a8b4d5e6f0ae9f3de017b48467
Jack Luo <jack.luo@mail.utoronto.ca>
2025-06-30 11:03:47 -0400

    feat(core): Add support for musllinux_1_2

---
+++ components/core/tools/scripts/lib_install/musllinux_1_2/install-prebuilt-packages.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+apk update &&  apk add \
+    openjdk11-jdk \
+    jq \
+    curl-dev \
+    bzip2-dev \
+    bzip2-static \
+    mariadb-connector-c-dev \
+    openssl-dev \
+    zlib-dev \
+    zlib-static
+
+# Install `task`
+# NOTE: We lock `task` to a version < 3.43 to avoid https://github.com/y-scope/clp/issues/
+VERSION=3.42.1
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) ARCH=amd64 ;;
+    aarch64) ARCH=arm64 ;;
+    *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
+
+wget -O /tmp/task.tar.gz \
+    "https://github.com/go-task/task/releases/download/v${VERSION}/task_linux_${ARCH}.tar.gz"
+
+tar -C /usr/local/bin -xzf /tmp/task.tar.gz task
+chmod +x /usr/local/bin/task
+rm /tmp/task.tar.gz
+
+# Downgrade to CMake v3 to work around https://github.com/y-scope/clp/issues/795
+pipx uninstall cmake
+pipx install cmake~=3.31
