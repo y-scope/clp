01db2e6233fb7d4883c2942a0a6583f453b04dd8
Marco <david.marcovitch@yscope.com>
2025-07-04 20:07:26 +0000

    latest

---
+++ components/webui/server/src/fastify-v2/schemas/stream-files.ts
@@ -0,0 +1,31 @@
+import {Type} from "@sinclair/typebox";
+
+import {QUERY_JOB_TYPE} from "../../typings/query.js";
+import {
+    StringSchema,
+} from "./common.js";
+
+
+/**
+ * Schema for request to extract a stream file.
+ */
+const StreamFileExtractionSchema = Type.Object({
+    extractJobType: Type.Enum(QUERY_JOB_TYPE),
+    logEventIdx: Type.Integer(),
+    streamId: StringSchema,
+});
+
+/**
+ * Schema for stream file metadata response.
+ */
+const StreamFileMetadataSchema = Type.Object({
+    path: StringSchema,
+    stream_id: StringSchema,
+    begin_msg_ix: Type.Integer(),
+    end_msg_ix: Type.Integer(),
+});
+
+export {
+    StreamFileExtractionSchema,
+    StreamFileMetadataSchema,
+};
