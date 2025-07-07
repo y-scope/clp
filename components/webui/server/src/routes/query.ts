027c0b8c0ad8fa9cf329abe7a48c2f23dcf7100e
Bingran Hu <bingran.hu@yscope.com>
2025-07-08 03:42:46 +0800

    feat(clp-package): Expose dataset selection in package CLI scripts and web UI.  (#1050)
    Co-authored-by: Marco <david.marcovitch@yscope.com>
    Co-authored-by: Haiqi Xu <14502009+haiqi96@users.noreply.github.com>
    Co-authored-by: Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
    Co-authored-by: hoophalab <200652805+hoophalab@users.noreply.github.com>

--- components/webui/server/src/routes/query.ts
+++ components/webui/server/src/routes/query.ts
@@ -38,13 +38,17 @@ const routes: FastifyPluginAsync = async (app) => {
     fastify.post("/query/extract-stream", {
         schema: {
             body: Type.Object({
+                // Type.Null must come before Type.String;
+                // otherwise, `{dataset: null}` gets converted to `{dataset: ""}`.
+                dataset: Type.Union([Type.Null(),
+                    Type.String()]),
                 extractJobType: Type.Enum(QUERY_JOB_TYPE),
                 logEventIdx: Type.Integer(),
                 streamId: Type.String({minLength: 1}),
             }),
         },
     }, async (req, resp) => {
-        const {extractJobType, logEventIdx, streamId} = req.body;
+        const {dataset, extractJobType, logEventIdx, streamId} = req.body;
         if (false === EXTRACT_JOB_TYPES.has(extractJobType)) {
             resp.code(StatusCodes.BAD_REQUEST);
             throw new Error(`Invalid extractJobType="${extractJobType}".`);
@@ -57,6 +61,7 @@ const routes: FastifyPluginAsync = async (app) => {
 
         if (null === streamMetadata) {
             const extractResult = await fastify.dbManager.submitAndWaitForExtractStreamJob({
+                dataset: dataset,
                 jobType: extractJobType,
                 logEventIdx: logEventIdx,
                 streamId: streamId,
