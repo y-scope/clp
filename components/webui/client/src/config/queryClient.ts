883f8492edb8a0d37404c3769fb548c940940937
Marco <david.marcovitch@yscope.com>
2025-07-08 19:03:00 +0000

    latest

--- components/webui/client/src/config/queryClient.ts
+++ components/webui/client/src/config/queryClient.ts
@@ -1,12 +1,12 @@
 import {QueryClient} from "@tanstack/react-query";
 
 
-const DEFAULT_STALE_TIME_MILLIS = 10_000;
+const DEFAULT_REFETCH_INTERVAL_MILLIS = 10_000;
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: DEFAULT_STALE_TIME_MILLIS,
+            refetchInterval: DEFAULT_REFETCH_INTERVAL_MILLIS,
         },
     },
 });
