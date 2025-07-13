c659eb4f046783915ce16c8a70a002bbc179aa50
Bingran Hu <bingran.hu@yscope.com>
2025-07-13 11:06:52 -0400

    Add clp-s test code

--- integration-tests/tests/test_identity_transformation.py
+++ integration-tests/tests/test_identity_transformation.py
@@ -1,5 +1,6 @@
 import pytest
-from fixtures.fixture_types import DatasetParams
+from fixtures.fixture_types import DatasetParams, PackageTestConfig
+from fixtures.utils import run_and_assert
 
 pytestmark = pytest.mark.binaries
 
@@ -30,7 +31,17 @@ def test_clp_identity_transform(download_and_extract_dataset, request) -> None:
 
 @pytest.mark.clp_s
 @json_datasets
-def test_clp_s_identity_transform(download_and_extract_dataset: DatasetParams, request) -> None:
-    # print(request.getfixturevalue(download_and_extract_dataset))
-    print(download_and_extract_dataset)
-    assert True
+def test_clp_s_identity_transform(
+    request, config: PackageTestConfig, download_and_extract_dataset: DatasetParams
+) -> None:
+    dataset_name = download_and_extract_dataset.dataset_name
+    download_dir = config.uncompressed_logs_dir / dataset_name
+    archives_dir = config.test_output_dir / f"{dataset_name}-archives"
+    extract_dir = config.test_output_dir / f"{dataset_name}-logs"
+
+    compression_cmd = [config.clp_bin_dir / "clp-s", "c", archives_dir, download_dir]
+    print(compression_cmd)
+    run_and_assert(compression_cmd)
+
+    decompression_cmd = [config.clp_bin_dir / "clp-s", "x", archives_dir, extract_dir]
+    run_and_assert(decompression_cmd)
