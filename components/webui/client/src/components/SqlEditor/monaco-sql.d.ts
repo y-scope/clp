b3db4a8e6526524088a06e0f23bd29e97e816df6
Marco <david.marcovitch@yscope.com>
2025-07-18 20:37:03 +0000

    latest

--- components/webui/client/src/components/SqlEditor/monaco-sql.d.ts
+++ components/webui/client/src/components/SqlEditor/monaco-sql.d.ts
@@ -1,5 +1,5 @@
 declare module "monaco-editor/esm/vs/basic-languages/sql/sql.js" {
-    import {languages} from "monaco-editor/esm/vs/editor/editor.api";
+    import {languages} from "monaco-editor";
 
 
     interface SqlLanguageDefinition extends languages.IMonarchLanguage {
