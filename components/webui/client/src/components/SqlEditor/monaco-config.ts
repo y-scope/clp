1b50904039346059e04c345ccd0faffd94d1cdd4
Marco <david.marcovitch@yscope.com>
2025-07-18 21:14:41 +0000

    latest

--- components/webui/client/src/components/SqlEditor/monaco-config.ts
+++ components/webui/client/src/components/SqlEditor/monaco-config.ts
@@ -1,28 +1,11 @@
 import { loader } from '@monaco-editor/react';
-import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 
-// Reference: https://github.com/suren-atoyan/monaco-react?tab=readme-ov-file#use-monaco-editor-as-an-npm-package
 self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker();
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker();
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker();
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker();
-    }
-    return new editorWorker();
-  },
+    getWorker: () => {
+        return new EditorWorker();
+    },
 };
 
 loader.config({ monaco });
