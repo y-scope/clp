b6eb73a5d11e495da16e83fda33cdc468a3a9723
Marco <david.marcovitch@yscope.com>
2025-07-21 14:31:56 +0000

    latest

--- components/webui/client/src/components/SqlEditor/monaco-config.ts
+++ components/webui/client/src/components/SqlEditor/monaco-config.ts
@@ -1,15 +1,21 @@
 /* eslint-disable import/default */
 import {loader} from "@monaco-editor/react";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-
 import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+
 import "monaco-editor/esm/vs/basic-languages/sql/sql.contribution.js";
-import "monaco-editor/esm/vs/editor/editor.all.js"
+import "monaco-editor/esm/vs/editor/editor.all.js";
+
 
 self.MonacoEnvironment = {
-  getWorker() {
-    return new EditorWorker();
-  },
+    /**
+     * Creates a web worker for Monaco Editor.
+     *
+     * @return
+     */
+    getWorker () {
+        return new EditorWorker();
+    },
 };
 
 loader.config({monaco});
