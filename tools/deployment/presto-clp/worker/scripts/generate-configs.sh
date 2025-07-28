70a56d7b9e520c92213af2caed449b91ca4a4991
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-07-28 05:51:33 -0400

    Move getting Presto coordinator version into a function.

--- tools/deployment/presto-clp/worker/scripts/generate-configs.sh
+++ tools/deployment/presto-clp/worker/scripts/generate-configs.sh
@@ -2,7 +2,7 @@
 
 # Sets/updates the given kv-pair in the given properties file.
 #
-# @param $1 The properties file.
+# @param $1 Path to the properties file.
 # @param $2 The key to set.
 # @param $3 The value to set.
 update_config_file() {
@@ -13,9 +13,34 @@ update_config_file() {
     if grep --quiet "^${key}=.*$" "$file_path"; then
         sed --in-place "s|^${key}=.*|${key}=${value}|" "$file_path"
     else
-        echo "${key}=${value}" >> "$file_path"
+        echo "${key}=${value}" >>"$file_path"
     fi
     echo "Set ${key}=${value} in ${file_path}"
+}
+
+# Gets the Presto coordinator's version or exits on failure.
+#
+# @param $1 Path to the config.properties file.
+# @return The Presto version.
+get_coordinator_version() {
+    local config_properties_file=$1
+
+    local discovery_uri
+    discovery_uri=$(awk -F= '/^discovery.uri=/ {print $2}' "$config_properties_file")
+    if response=$(
+        wget --quiet --output-document - --timeout=10 "${discovery_uri}/v1/info" 2>/dev/null
+    ); then
+        version=$(echo "$response" | jq --raw-output '.nodeVersion.version')
+        if [[ "$version" = "null" ]]; then
+            echo "Error: Presto response is empty or doesn't contain version info."
+            exit 1
+        fi
+    else
+        echo "Error: Couldn't get Presto version info."
+        exit 1
+    fi
+
+    echo "$version"
 }
 
 apt-get update && apt-get install --assume-yes --no-install-recommends jq wget
@@ -36,31 +61,10 @@ rm -f ${PRESTO_CONFIG_DIR}/catalog/*
 
 mv ${PRESTO_CONFIG_DIR}/clp.properties ${PRESTO_CONFIG_DIR}/catalog
 
-# Update "presto.version" parameter in config.properties file using values from coordinator
+# Update config.properties
 CONFIG_PROPERTIES_FILE="/opt/presto-server/etc/config.properties"
-
-# 1. Fetch version info from Presto
-DISCOVERY_URI=$(awk -F= '/^discovery.uri=/ {print $2}' "${PRESTO_CONFIG_DIR}/config.properties")
-if response=$(
-    wget --quiet --output-document - --timeout=10 "${DISCOVERY_URI}/v1/info" 2>/dev/null
-); then
-    version=$(echo "$response" | jq --raw-output '.nodeVersion.version')
-    if [ "$version" != "null" ]; then
-        echo "Presto is ready!"
-    else
-        echo "Error: Presto response is empty or doesn't contain version info."
-        exit 1
-    fi
-else
-    echo "Error: Couldn't get Presto version info."
-    exit 1
-fi
-
-# 2. Extract the version using grep and sed (busybox compatible)
-version=$(echo "$response" | grep -o '"version":"[^"]*"' | sed 's/"version":"//;s/"//')
-
+version=$(get_coordinator_version "$CONFIG_PROPERTIES_FILE")
 echo "Detected Presto version: $version"
-
 update_config_file "$CONFIG_PROPERTIES_FILE" "presto.version" "$version"
 
 # Update node.properties
