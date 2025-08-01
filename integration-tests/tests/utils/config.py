be64383f1efff63a7fe14b12f352b7ad62380717
Bingran Hu <bingran.hu@yscope.com>
2025-07-31 22:11:50 -0400

    remove unnecessary class param customizations

--- integration-tests/tests/utils/config.py
+++ integration-tests/tests/utils/config.py
@@ -20,19 +20,15 @@ class DatasetLogs:
     extraction_dir: Path
 
     @classmethod
-    def create(cls, package_config: PackageConfig, dataset_name: str, tarball_url: str, **kwargs):
+    def create(cls, package_config: PackageConfig, dataset_name: str, tarball_url: str):
         dataset_name = dataset_name.strip()
         if 0 == len(dataset_name):
             raise ValueError("`dataset_name` cannot be empty.")
         return cls(
             dataset_name=dataset_name,
             tarball_url=tarball_url,
-            tarball_path=kwargs.get(
-                "tarball_path", package_config.uncompressed_logs_dir / f"{dataset_name}.tar.gz"
-            ),
-            extraction_dir=kwargs.get(
-                "extraction_dir", package_config.uncompressed_logs_dir / dataset_name
-            ),
+            tarball_path=package_config.uncompressed_logs_dir / f"{dataset_name}.tar.gz",
+            extraction_dir=package_config.uncompressed_logs_dir / dataset_name,
         )
 
 
@@ -44,23 +40,14 @@ class CompressionTestPaths:
     decompression_dir: Path
 
     @classmethod
-    def create(cls, package_config: PackageConfig, test_name: str, logs_source_dir: Path, **kwargs):
-        test_name = test_name.strip()
-        if 0 == len(test_name):
-            raise ValueError("`test_name` cannot be empty.")
+    def create(cls, package_config: PackageConfig, test_name: str, logs_source_dir: Path):
         return cls(
             test_name=test_name,
             logs_source_dir=logs_source_dir,
-            compression_dir=kwargs.get(
-                "compression_dir", package_config.test_root_dir / f"{test_name}-archives"
-            ),
-            decompression_dir=kwargs.get(
-                "decompression_dir",
-                package_config.test_root_dir / f"{test_name}-decompressed-logs",
-            ),
+            compression_dir=package_config.test_root_dir / f"{test_name}-archives",
+            decompression_dir=package_config.test_root_dir / f"{test_name}-decompressed-logs",
         )
 
     def clear_test_outputs(self) -> None:
-        """Remove compression and decompression directories for test init and cleanup."""
         shutil.rmtree(self.compression_dir, ignore_errors=True)
         shutil.rmtree(self.decompression_dir, ignore_errors=True)
