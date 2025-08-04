a67f996bd24915f4217bd2451ed723cc3d54bbcf
gibber9809 <devinbook1@gmail.com>
2025-08-04 15:29:04 +0000

    Update cmake scripts to pull in clp core dependencies

--- components/core/cmake/Options/options.cmake
+++ components/core/cmake/Options/options.cmake
@@ -249,6 +249,7 @@ function(set_clp_s_clp_dependencies_dependencies)
         CLP_NEED_BOOST
         CLP_NEED_CURL
         CLP_NEED_FMT
+        CLP_NEED_LOG_SURGEON
         CLP_NEED_MSGPACKCXX
         CLP_NEED_NLOHMANN_JSON
         CLP_NEED_OPENSSL
