8b4f58ad4359e87e593e1429935b3523fd7c1596
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2025-08-07 11:38:18 -0400

    fix(package): Only mount `stream_output_dir` when stream storage type is `FS`. (#1129)
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

--- components/clp-package-utils/clp_package_utils/scripts/start_clp.py
+++ components/clp-package-utils/clp_package_utils/scripts/start_clp.py
@@ -905,7 +905,6 @@ def start_webui(
         "MongoDbStreamFilesCollectionName": clp_config.results_cache.stream_collection_name,
         "ClientDir": str(container_webui_dir / "client"),
         "LogViewerDir": str(container_webui_dir / "yscope-log-viewer"),
-        "StreamFilesDir": str(container_clp_config.stream_output.get_directory()),
         "StreamTargetUncompressedSize": container_clp_config.stream_output.target_uncompressed_size,
     }
 
@@ -914,7 +913,7 @@ def start_webui(
     stream_storage = clp_config.stream_output.storage
     if StorageType.S3 == stream_storage.type:
         s3_config = stream_storage.s3_config
-
+        server_settings_json_updates["StreamFilesDir"] = None
         server_settings_json_updates["StreamFilesS3Region"] = s3_config.region_code
         server_settings_json_updates["StreamFilesS3PathPrefix"] = (
             f"{s3_config.bucket}/{s3_config.key_prefix}"
@@ -925,6 +924,9 @@ def start_webui(
         else:
             server_settings_json_updates["StreamFilesS3Profile"] = None
     elif StorageType.FS == stream_storage.type:
+        server_settings_json_updates["StreamFilesDir"] = str(
+            container_clp_config.stream_output.get_directory()
+        )
         server_settings_json_updates["StreamFilesS3Region"] = None
         server_settings_json_updates["StreamFilesS3PathPrefix"] = None
         server_settings_json_updates["StreamFilesS3Profile"] = None
@@ -957,7 +959,6 @@ def start_webui(
     ]
     necessary_mounts = [
         mounts.clp_home,
-        mounts.stream_output_dir,
     ]
     if StorageType.S3 == stream_storage.type:
         auth = stream_storage.s3_config.aws_authentication
@@ -973,6 +974,9 @@ def start_webui(
                 necessary_mounts.append(mounts.aws_config_dir)
             if aws_env_vars:
                 necessary_env_vars.extend(aws_env_vars)
+    elif StorageType.FS == stream_storage.type:
+        necessary_mounts.append(mounts.stream_output_dir)
+
     append_docker_options(container_cmd, necessary_mounts, necessary_env_vars)
     container_cmd.append(clp_config.execution_container)
 
