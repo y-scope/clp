9807fb2de46710a18ad2212ae0291c5ed6f94774
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-08-13 07:44:39 -0400

    IndexManager: Refactor error handling and add missing parameters to docstring.

--- components/core/src/clp_s/indexer/IndexManager.cpp
+++ components/core/src/clp_s/indexer/IndexManager.cpp
@@ -12,28 +12,22 @@
 
 namespace clp_s::indexer {
 IndexManager::IndexManager(
-        std::optional<clp::GlobalMetadataDBConfig> const& db_config,
+        std::optional<clp::GlobalMetadataDBConfig> const& optional_db_config,
         bool should_create_table
 ) {
-    if (false == db_config.has_value()) {
+    try {
+        auto const& db_config = optional_db_config.value();
+        m_mysql_index_storage = std::make_unique<MySQLIndexStorage>(
+                db_config.get_metadata_db_host(),
+                db_config.get_metadata_db_port(),
+                db_config.get_metadata_db_username().value(),
+                db_config.get_metadata_db_password().value(),
+                db_config.get_metadata_db_name(),
+                db_config.get_metadata_table_prefix()
+        );
+    } catch (std::bad_optional_access const& e) {
         throw OperationFailed(ErrorCodeBadParam, __FILENAME__, __LINE__);
     }
-
-    auto const& db_username = db_config->get_metadata_db_username();
-    auto const& db_password = db_config->get_metadata_db_password();
-    if (false == db_username.has_value() || false == db_password.has_value()) {
-        SPDLOG_ERROR("Unexpected missing MySQL credentials for global metadata DB.");
-        throw OperationFailed(ErrorCodeBadParam, __FILENAME__, __LINE__);
-    }
-
-    m_mysql_index_storage = std::make_unique<MySQLIndexStorage>(
-            db_config->get_metadata_db_host(),
-            db_config->get_metadata_db_port(),
-            db_username.value(),
-            db_password.value(),
-            db_config->get_metadata_db_name(),
-            db_config->get_metadata_table_prefix()
-    );
     m_mysql_index_storage->open();
     m_field_update_callback = [this](std::string& field_name, NodeType field_type) {
         m_mysql_index_storage->add_field(field_name, field_type);
