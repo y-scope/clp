f68ebbec535017c10e26ab3cdbc7db7cb193a5d2
Jack Luo <jack.luo@mail.utoronto.ca>
2025-08-15 05:09:27 +0800

    feat: Package both openssl-1.x and openssl-3.x into manylinux_2_28 compilation image.

--- components/core/tools/scripts/lib_install/manylinux_2_28/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/manylinux_2_28/install-packages-from-source.sh
@@ -9,6 +9,13 @@ lib_install_scripts_dir="${script_dir}/.."
 # NOTE: The remaining installation scripts depend on boost, so we install it beforehand.
 "${lib_install_scripts_dir}/install-boost.sh" 1.87.0
 
+# manylinux_2_28 enables portable builds but lacks support for system OpenSSL 3.x and static
+# libraries. By manually installing both OpenSSL 1.x and 3.x to non-system directories, we gain
+# the ability to switch versions during build configuration prior to compilation.
+# Example: export CMAKE_PREFIX_PATH=/opt/openssl-<version>:$CMAKE_PREFIX_PATH
+"${lib_install_scripts_dir}/openssl.sh" 1.1.1w /opt/openssl-1.x
+"${lib_install_scripts_dir}/openssl.sh" 3.5.2 /opt/openssl-3.x
+
 # NOTE:
 # 1. libarchive may statically link with LZMA, LZ4, and Zstandard, so we install them beforehand.
 # 2. The versions of libarchive, LZMA, LZ4, and Zstandard available in manylinux_2_28's package
