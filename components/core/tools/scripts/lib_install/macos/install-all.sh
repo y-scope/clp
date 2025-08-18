22f70b51ff20df6ac55d5233d70240aaa6db030e
Bingran Hu <bingran.hu@yscope.com>
2025-08-18 11:54:30 -0400

    use pipx to install cmake 3.31

--- components/core/tools/scripts/lib_install/macos/install-all.sh
+++ components/core/tools/scripts/lib_install/macos/install-all.sh
@@ -20,19 +20,8 @@ git -C "${tap_dir}" commit --allow-empty -qm "init tap"
 
 # Install CMake v3.31.6 as ANTLR and yaml-cpp do not yet support CMake v4+.
 # See also: https://github.com/y-scope/clp/issues/795
-cmake_formula_path="${formula_dir}/cmake.rb"
-curl \
-  --fail \
-  --location \
-  --output "$cmake_formula_path" \
-  --show-error \
-  https://raw.githubusercontent.com/Homebrew/homebrew-core/b4e46db74e74a8c1650b38b1da222284ce1ec5ce\
-/Formula/c/cmake.rb
-git -C "${tap_dir}" add "Formula/cmake.rb"
-git -C "${tap_dir}" commit -qm "add cmake formula"
-brew untap "${tap_name}" || true
-brew tap "${tap_name}" "${tap_dir}"
-brew install "${tap_name}/cmake"
+pipx uninstall cmake
+pipx install cmake~=3.31
 
 # Install a version of `task` < 3.43 to avoid https://github.com/y-scope/clp/issues/872
 task_formula_path="${formula_dir}/go-task.rb"
