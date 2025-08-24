b7f67f6bdb98dfd34cc79991cc4236762db23f4b
Bingran Hu <bingran.hu@yscope.com>
2025-08-24 06:25:11 -0400

    Fix relative path

--- components/core/tools/scripts/lib_install/check-build-tool-versions.sh
+++ components/core/tools/scripts/lib_install/check-build-tool-versions.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # TODO: https://github.com/y-scope/clp/issues/795
-"${script_dir}/../check-cmake-version.sh"
+"${script_dir}/check-cmake-version.sh"
 
 # TODO: https://github.com/y-scope/clp/issues/872
-"${script_dir}/../check-go-task-version.sh"
+"${script_dir}/check-go-task-version.sh"
