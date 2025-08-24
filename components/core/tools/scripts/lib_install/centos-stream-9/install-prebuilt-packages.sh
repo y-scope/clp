029b33d7916859c9db65536e481c930a9cd6103d
Bingran Hu <bingran.hu@yscope.com>
2025-08-24 05:57:31 -0400

    add centos 9

--- components/core/tools/scripts/lib_install/centos-stream-9/install-prebuilt-packages.sh
+++ components/core/tools/scripts/lib_install/centos-stream-9/install-prebuilt-packages.sh
@@ -23,18 +23,18 @@ dnf install -y \
     python3-pip \
     unzip
 
-python3 -m pip install --user pipx
-
-# Install CMake v3.31.6 as ANTLR and yaml-cpp do not yet support CMake v4+.
-# See also: https://github.com/y-scope/clp/issues/795
-if command -v cmake ; then
-    dnf remove -y cmake
+if ! command -v pipx; then
+    python3 -m pip install pipx
 fi
-pipx install "cmake~=3.31"
+export PIPX_BIN_DIR=/usr/bin
+
+# Install CMake v3.31.x as ANTLR and yaml-cpp do not yet support CMake v4+.
+# See also: https://github.com/y-scope/clp/issues/795
+if ! command -v cmake ; then
+    pipx install "cmake~=3.31"
+fi
 
 # Install a version of `task` < 3.43 to avoid https://github.com/y-scope/clp/issues/872
-if command -v task ; then
-    echo "placeholder"
-    exit 1
+if ! command -v task ; then
+    pipx install "go-task-bin>=3.40,<3.43"
 fi
-pipx install "go-task-bin>=3.40,<3.43"
