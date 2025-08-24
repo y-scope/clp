c49d8b405d368bcfb2e08a8c80a13da1c9e97e6c
Junhao Liao <junhao@junhao.ca>
2025-08-24 10:07:47 -0400

    remove unneeded prebuilt packages

--- tools/docker-images/clp-package-ubuntu-jammy/setup-scripts/install-prebuilt-packages.sh
+++ tools/docker-images/clp-package-ubuntu-jammy/setup-scripts/install-prebuilt-packages.sh
@@ -6,11 +6,6 @@ set -o pipefail
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   ca-certificates \
-  checkinstall \
-  curl \
   libcurl4 \
   libmariadb-dev \
-  libssl-dev \
-  python3 \
-  rsync \
-  zstd
+  python3
