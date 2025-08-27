bdfc250824ec8509ed21ae86fbf3322a2093dbcb
Marco <david.marcovitch@yscope.com>
2025-08-27 17:34:04 +0000

    latest

--- components/clp-py-utils/clp_py_utils/clp_config.py
+++ components/clp-py-utils/clp_py_utils/clp_config.py
@@ -976,6 +976,8 @@ class CLPConfig(BaseModel):
         package = values.get("package")
         presto = values.get("presto")
         query_engine: Optional[QueryEngine] = None
+        # Root validator with `pre=True`` is called before other validators, so package has not
+        # been validated yet as dict yet. Pydantic will throw error later if package is not a dict.
         if isinstance(package, dict):
             query_engine = package.get("query_engine", None)
         if query_engine == QueryEngine.PRESTO and presto is None:
