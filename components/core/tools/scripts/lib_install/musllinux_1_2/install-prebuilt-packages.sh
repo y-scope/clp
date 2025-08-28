4141831a7a303ca9bac1fd76c1adb16498d61c68
Bingran Hu <bingran.hu@yscope.com>
2025-08-28 12:50:23 -0400

    Allow pipx uninstall not found

--- components/core/tools/scripts/lib_install/musllinux_1_2/install-prebuilt-packages.sh
+++ components/core/tools/scripts/lib_install/musllinux_1_2/install-prebuilt-packages.sh
@@ -17,20 +17,20 @@ apk update && apk add --no-cache \
 # Install `cmake`
 # ystdlib requires CMake v3.23; ANTLR and yaml-cpp do not yet support CMake v4+.
 # See also: https://github.com/y-scope/clp/issues/795
-pipx uninstall cmake
+pipx uninstall cmake || true
 if ! command -v cmake ; then
     pipx install "cmake~=3.23"
 fi
 
 # Install `task`
 # NOTE: We lock `task` to a version < 3.43 to avoid https://github.com/y-scope/clp/issues/872
-pipx uninstall go-task-bin
+pipx uninstall go-task-bin || true
 if ! command -v task ; then
     pipx install "go-task-bin>=3.40,<3.43"
 fi
 
 # Install `uv`
-pipx uninstall uv
+pipx uninstall uv || true
 if ! command -v uv ; then
     pipx install "uv>=0.8"
 fi
