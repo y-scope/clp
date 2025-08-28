5e4939976e4ed535f65a3563da75a88ffde37259
Bingran Hu <bingran.hu@yscope.com>
2025-08-27 23:56:26 -0400

    fix remaining header

--- components/core/tests/test-kql.cpp
+++ components/core/tests/test-kql.cpp
@@ -4,7 +4,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
-#include <fmt/core.h>
+#include <fmt/format.h>
 #include <spdlog/spdlog.h>
 
 #include "../src/clp_s/search/ast/AndExpr.hpp"
