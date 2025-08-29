6b0d3af9a12e58300218a43c8ca0d4b17a10f979
gibber9809 <devinbook1@gmail.com>
2025-08-29 19:46:40 +0000

    Handle pure wildcards and unexpected literal types correctly in

--- components/core/src/clp_s/search/EvaluateTimestampIndex.cpp
+++ components/core/src/clp_s/search/EvaluateTimestampIndex.cpp
@@ -3,6 +3,7 @@
 #include "ast/AndExpr.hpp"
 #include "ast/Expression.hpp"
 #include "ast/FilterExpr.hpp"
+#include "ast/FilterOperation.hpp"
 #include "ast/Integral.hpp"
 #include "ast/Literal.hpp"
 #include "ast/OrExpr.hpp"
@@ -10,6 +11,7 @@
 using clp_s::search::ast::AndExpr;
 using clp_s::search::ast::Expression;
 using clp_s::search::ast::FilterExpr;
+using clp_s::search::ast::FilterOperation;
 using clp_s::search::ast::Integral;
 using clp_s::search::ast::Integral64;
 using clp_s::search::ast::literal_type_bitmask_t;
@@ -82,19 +84,50 @@ EvaluatedValue EvaluateTimestampIndex::run(std::shared_ptr<Expression> const& ex
                 continue;
             }
 
-            EvaluatedValue ret;
-            // this is safe after type narrowing because all DateType literals are either
-            // Integral or a derived class of Integral
-            Integral64 literal = std::static_pointer_cast<Integral>(filter->get_operand())->get();
-            if (std::holds_alternative<int64_t>(literal)) {
+            // Leave handling EXISTS/NEXISTS to schema matching.
+            auto const filter_op{filter->get_operation()};
+            if (FilterOperation::EXISTS == filter_op || FilterOperation::NEXISTS == filter_op) {
+                return EvaluatedValue::Unknown;
+            }
+
+            auto const literal{filter->get_operand()};
+            // Defend against future FilterOperation types that don't take a literal.
+            if (nullptr == literal) {
+                return EvaluatedValue::Unknown;
+            }
+
+            // Handle the case where the literal is a pure wildcard.
+            if (literal->as_any(filter_op)) {
+                switch (filter_op) {
+                    case FilterOperation::EQ:
+                        return filter->is_inverted() ? EvaluatedValue::False : EvaluatedValue::True;
+                    case FilterOperation::NEQ:
+                        return filter->is_inverted() ? EvaluatedValue::True : EvaluatedValue::False;
+                    default:
+                        // Not reachable.
+                        return EvaluatedValue::Unknown;
+                }
+            }
+
+            EvaluatedValue ret{EvaluatedValue::Unknown};
+            // This should be safe after type narrowing and checking that the literal exists and is
+            // not a pure wildcard because all DateType literals are either Integral or a derived
+            // class of Integral. Still, we check whether the literal is an Integral in case this
+            // assumption breaks for future versions of the AST.
+            auto integral_literal{std::dynamic_pointer_cast<Integral>(literal)};
+            if (nullptr == integral_literal) {
+                return EvaluatedValue::Unknown;
+            }
+            Integral64 integral = integral_literal->get();
+            if (std::holds_alternative<int64_t>(integral)) {
                 ret = range_it->second->evaluate_filter(
                         filter->get_operation(),
-                        std::get<int64_t>(literal)
+                        std::get<int64_t>(integral)
                 );
             } else {
                 ret = range_it->second->evaluate_filter(
                         filter->get_operation(),
-                        std::get<double>(literal)
+                        std::get<double>(integral)
                 );
             }
 
