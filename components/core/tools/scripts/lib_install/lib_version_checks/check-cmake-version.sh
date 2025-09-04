8c94fbd15e3626137c7ad0b4670302b18b7da4b3
Bingran Hu <bingran.hu@yscope.com>
2025-09-04 03:36:03 -0400

    Update cmake version check

--- components/core/tools/scripts/lib_install/lib_version_checks/check-cmake-version.sh
+++ components/core/tools/scripts/lib_install/lib_version_checks/check-cmake-version.sh
@@ -7,9 +7,13 @@ set -o pipefail
 # Get the installed cmake version string
 cmake_version=$(cmake -E capabilities | jq --raw-output ".version.string")
 cmake_major_version=$(cmake -E capabilities | jq --raw-output ".version.major")
+cmake_minor_version=$(cmake -E capabilities | jq --raw-output ".version.minor")
 
-# Check if version is 4.0 or higher
-if [[ "${cmake_major_version}" -ge "4" ]]; then
-  echo "Error: CMake version ${cmake_version} is currently unsupported (>= 4.0)."
+# ystdlib requires CMake v3.23; ANTLR and yaml-cpp do not yet support CMake v4+.
+# See also: https://github.com/y-scope/clp/issues/795
+if [[ "${cmake_major_version}" -lt 3 ]] || \
+   [[ "${cmake_major_version}" -eq 3 && "${cmake_minor_version}" -lt 23 ]] || \
+   [[ "${cmake_major_version}" -ge 4 ]]; then
+  echo "Error: CMake version ${cmake_version} is unsupported (require 3.23 ≤ version < 4.0)."
   exit 1
 fi
