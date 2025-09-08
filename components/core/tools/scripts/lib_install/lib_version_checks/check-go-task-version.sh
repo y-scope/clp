e70f2e70abe867518cebe67a1682794c49888b6e
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-09-08 05:55:28 -0400

    Refactor version checking scripts.

--- components/core/tools/scripts/lib_install/lib_version_checks/check-go-task-version.sh
+++ components/core/tools/scripts/lib_install/lib_version_checks/check-go-task-version.sh
@@ -6,15 +6,10 @@ set -o pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Get the installed go-task version string
-task_version=$(task --silent --taskfile "${script_dir}/print-go-task-version.yaml" version)
-IFS=. read -r task_major_version task_minor_version task_patch_version <<< "${task_version}"
+task_version=$(task --silent --taskfile "${script_dir}/print-go-task-version.yaml")
 
-# Check version constraints
-    # We lock `task` to version 3.44.0 to avoid https://github.com/y-scope/clp-ffi-js/issues/110
-if [ "${task_major_version}" -ne "3" ] || \
-   [ "${task_minor_version}" -ne "44" ] || \
-   [ "${task_patch_version}" -ne "0" ]; then
+# We lock to version 3.44.0 to avoid https://github.com/y-scope/clp-ffi-js/issues/110
+if [[ "${task_version}" != "3.44.0" ]]; then
   echo "Error: Task version ${task_version} is currently unsupported (must be 3.44.0)."
   exit 1
 fi
