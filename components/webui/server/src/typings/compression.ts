dd64ac36692b2be86e21b03bc489851e24bc8106
Junhao Liao <junhao@junhao.ca>
2025-09-09 18:11:03 -0400

    fix(webui): Remove unused compression job type definitions and constants.

--- components/webui/server/src/typings/compression.ts
+++ components/webui/server/src/typings/compression.ts
@@ -1,6 +1,3 @@
-import {RowDataPacket} from "mysql2/promise";
-
-
 /**
  * Matching the `CompressionJobStatus` class in `job_orchestration.scheduler.constants`.
  *
@@ -15,14 +12,6 @@ enum COMPRESSION_JOB_STATUS {
 }
 
 /**
- * List of states that indicate the job is either pending or in progress.
- */
-const COMPRESSION_JOB_STATUS_WAITING_STATES = new Set([
-    COMPRESSION_JOB_STATUS.PENDING,
-    COMPRESSION_JOB_STATUS.RUNNING,
-]);
-
-/**
  * The `compression_jobs` table's column names.
  *
  * @enum {string}
@@ -33,15 +22,8 @@ enum COMPRESSION_JOBS_TABLE_COLUMN_NAMES {
     JOB_CONFIG = "clp_config",
 }
 
-interface CompressionJob extends RowDataPacket {
-    [COMPRESSION_JOBS_TABLE_COLUMN_NAMES.ID]: number;
-    [COMPRESSION_JOBS_TABLE_COLUMN_NAMES.STATUS]: COMPRESSION_JOB_STATUS;
-    [COMPRESSION_JOBS_TABLE_COLUMN_NAMES.JOB_CONFIG]: string;
-}
 
-export type {CompressionJob};
 export {
     COMPRESSION_JOB_STATUS,
-    COMPRESSION_JOB_STATUS_WAITING_STATES,
     COMPRESSION_JOBS_TABLE_COLUMN_NAMES,
 };
