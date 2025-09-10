0d517a5fb76b5cc777c442466360ab9f4454782a
hoophalab <200652805+hoophalab@users.noreply.github.com>
2025-09-10 13:18:24 -0400

    use upper case stream

--- components/webui/client/src/sql-parser/index.ts
+++ components/webui/client/src/sql-parser/index.ts
@@ -26,6 +26,27 @@ class SyntaxErrorListener<TSymbol> extends ErrorListener<TSymbol> {
     }
 }
 
+const LOWER_CASE_A_CHAR_CODE = 97;
+const LOWER_CASE_Z_CHAR_CODE = 122;
+const ASCII_CASE_OFFSET = 32;
+
+class UpperCaseCharStream extends CharStream {
+    // Override
+    override LA (offset: number): number {
+        // eslint-disable-next-line new-cap
+        const c = super.LA(offset);
+        if (0 >= c) {
+            return c;
+        }
+        if (LOWER_CASE_A_CHAR_CODE <= c && LOWER_CASE_Z_CHAR_CODE >= c) {
+            return c - ASCII_CASE_OFFSET;
+        }
+
+        return c;
+    }
+}
+
+
 /**
  * Validate a SQL string for syntax errors.
  *
@@ -34,7 +55,7 @@ class SyntaxErrorListener<TSymbol> extends ErrorListener<TSymbol> {
  */
 const validate = (sqlString: string) => {
     const syntaxErrorListener = new SyntaxErrorListener();
-    const lexer = new SqlBaseLexer(new CharStream(sqlString.toUpperCase()));
+    const lexer = new SqlBaseLexer(new UpperCaseCharStream(sqlString));
     lexer.removeErrorListeners();
     lexer.addErrorListener(syntaxErrorListener);
     const parser = new SqlBaseParser(new CommonTokenStream(lexer));
