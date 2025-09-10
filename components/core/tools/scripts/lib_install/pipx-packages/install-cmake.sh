9297f67c8270fe8dc3b886126317a2c3a82421b7
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-09-10 12:26:04 -0400

    Dedupliicate cmake version constraint.

--- components/core/tools/scripts/lib_install/pipx-packages/install-cmake.sh
+++ components/core/tools/scripts/lib_install/pipx-packages/install-cmake.sh
@@ -9,25 +9,33 @@ if ! command -v pipx >/dev/null 2>&1; then
     exit 1
 fi
 
+readonly required_version_major_min=3
+readonly required_version_minor_min=23
+readonly required_version_min="${required_version_major_min}.${required_version_minor_min}"
+readonly required_version_major_max=3
+readonly required_version_major_max_plus_1=$((required_version_major_max + 1))
+
 package_preinstalled=0
 if ! command -v cmake >/dev/null 2>&1; then
     package_preinstalled=1
     # ystdlib requires CMake v3.23; ANTLR and yaml-cpp do not yet support CMake v4+
     # (see https://github.com/y-scope/clp/issues/795).
-    pipx install --force "cmake>=3.23,<4"
+    pipx install --force "cmake>=${required_version_min},<${required_version_major_max_plus_1}"
     pipx ensurepath
 fi
 
-cmake_version=$(cmake -E capabilities | jq --raw-output ".version.string")
-cmake_major_version=$(cmake -E capabilities | jq --raw-output ".version.major")
-cmake_minor_version=$(cmake -E capabilities | jq --raw-output ".version.minor")
+installed_version=$(cmake -E capabilities | jq --raw-output ".version.string")
+installed_version_major=$(cmake -E capabilities | jq --raw-output ".version.major")
+installed_version_minor=$(cmake -E capabilities | jq --raw-output ".version.minor")
 
 # ystdlib requires CMake v3.23; ANTLR and yaml-cpp do not yet support CMake v4+
 # (see https://github.com/y-scope/clp/issues/795).
-if (("${cmake_major_version}" < 3)) \
-    || (("${cmake_major_version}" == 3 && "${cmake_minor_version}" < 23)) \
-    || (("${cmake_major_version}" >= 4)); then
-    echo "Error: CMake version ${cmake_version} is unsupported (require 3.23 ≤ version < 4.0)."
+if (("${installed_version_major}" < "${required_version_major_min}")) \
+    || (("${installed_version_major}" == "${required_version_major_min}" && \
+    "${installed_version_minor}" < "${required_version_minor_min}")) \
+    || (("${installed_version_major}" >= "${required_version_major_max_plus_1}")); then
+    echo "Error: CMake version ${installed_version} is unsupported (require" \
+        "${required_version_min} ≤ version < ${required_version_major_max_plus_1})."
 
     if ((0 == "${package_preinstalled}")); then
         echo "Please uninstall CMake and then re-run the install script."
