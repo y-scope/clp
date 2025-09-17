cb291dcfb74fa5d659d56efb94a8ca4d61f31674
Junhao Liao <junhao@junhao.ca>
2025-09-17 05:56:35 -0400

    remove unused JOB_COMPLETION_STATUS_POLL_INTERVAL_MILLIS

--- components/webui/server/src/plugins/app/CompressionJobDbManager/typings.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Interval (in milliseconds) for polling a job's completion status.
- */
-const JOB_COMPLETION_STATUS_POLL_INTERVAL_MILLIS = 1000;
-
-export {JOB_COMPLETION_STATUS_POLL_INTERVAL_MILLIS};
