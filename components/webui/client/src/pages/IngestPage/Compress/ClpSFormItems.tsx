44033bf89be30b2b2ef88875779472e5d053ba91
Junhao Liao <junhao@junhao.ca>
2025-09-19 15:21:01 -0400

    refactor: Add helper text tooltips for dataset and timestamp key form items

--- components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
+++ components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
@@ -5,6 +5,14 @@ import {
 } from "antd";
 
 
+const DATASET_HELPER_TEXT = "The dataset that the archives belong to. If left empty, dataset " +
+    "\"default\" is used.";
+const TIMESTAMP_KEY_HELPER_TEXT = "The path (e.g. x.y) for the field containing the log event's" +
+    " timestamp.";
+const TIMESTAMP_KEY_HELPER_TEXT_EXTRA = "If not provided, events will not have assigned" +
+    " timestamps and can only be searched from the command line without a timestamp filter.";
+
+
 /**
  * Renders additional compression job submission form items for CLP-S storage engine.
  *
@@ -15,20 +23,18 @@ const ClpSFormItems = () => (
         <Form.Item
             label={"Dataset"}
             name={"dataset"}
+            tooltip={DATASET_HELPER_TEXT}
         >
             <Input
                 defaultValue={CLP_DEFAULT_DATASET_NAME}
-                placeholder={"The dataset that the archives belong to (optional)"}/>
+                placeholder={DATASET_HELPER_TEXT}/>
         </Form.Item>
         <Form.Item
             label={"Timestamp Key"}
             name={"timestampKey"}
+            tooltip={`${TIMESTAMP_KEY_HELPER_TEXT} ${TIMESTAMP_KEY_HELPER_TEXT_EXTRA}`}
         >
-            <Input
-                placeholder={
-                    "The path for the field containing the log event's " +
-                                    "timestamp (optional)"
-                }/>
+            <Input placeholder={TIMESTAMP_KEY_HELPER_TEXT}/>
         </Form.Item>
     </>
 );
