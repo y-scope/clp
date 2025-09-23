e6817d861d04bcf2a106217329c66fbcd2ebe469
Bingran Hu <bingran.hu@yscope.com>
2025-09-23 09:36:08 +0000

    remove sudo when installing pipx packages

--- components/core/tools/scripts/lib_install/pipx-packages/install-all.sh
+++ components/core/tools/scripts/lib_install/pipx-packages/install-all.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+is_sudo_from_non_root=$(( $EUID == 0 && ${SUDO_UID:-0} != 0 ))
+if (( is_sudo_from_non_root )); then
+    echo "Installing pipx packages to the user environment (sudo lifted)."
+    exec sudo --set-home --user="$SUDO_USER" "$0" "$@"
+fi
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 "${script_dir}/install-cmake.sh"
