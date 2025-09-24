23dcd12f52e575d0d673bbe009bfc30e71143f2d
Junhao Liao <junhao@junhao.ca>
2025-09-24 10:33:11 -0400

    Update timestamp key placeholder and helper text.

--- components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
+++ components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
@@ -8,11 +8,10 @@ import {
 const DATASET_HELPER_TEXT = "The dataset that the archives belong to. If left empty, dataset " +
     `"${CLP_DEFAULT_DATASET_NAME}" will be used.`;
 const DATASET_PLACEHOLDER_TEXT = "The dataset for new archives";
-const TIMESTAMP_KEY_HELPER_TEXT = "The path (e.g. x.y) for the field containing the log event's" +
-    " timestamp.";
-const TIMESTAMP_KEY_HELPER_TEXT_EXTRA = "If not provided, events will not have assigned" +
+const TIMESTAMP_KEY_HELPER_TEXT = "If not provided, events will not have assigned" +
     " timestamps and can only be searched from the command line without a timestamp filter.";
-
+const TIMESTAMP_KEY_PLACEHOLDER_TEXT = "The path (e.g. x.y) for the field containing the log event's" +
+    " timestamp.";
 
 /**
  * Renders additional compression job submission form items for CLP-S storage engine.
@@ -31,9 +30,9 @@ const ClpSFormItems = () => (
         <Form.Item
             label={"Timestamp Key"}
             name={"timestampKey"}
-            tooltip={`${TIMESTAMP_KEY_HELPER_TEXT} ${TIMESTAMP_KEY_HELPER_TEXT_EXTRA}`}
+            tooltip={TIMESTAMP_KEY_HELPER_TEXT}
         >
-            <Input placeholder={TIMESTAMP_KEY_HELPER_TEXT}/>
+            <Input placeholder={TIMESTAMP_KEY_PLACEHOLDER_TEXT}/>
         </Form.Item>
     </>
 );
