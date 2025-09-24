e260129f1d3de6f276e8e7657daa3d873c1e102a
Junhao Liao <junhao@junhao.ca>
2025-09-24 10:35:00 -0400

    Update dataset helper text and format timestamp key placeholder.

--- components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
+++ components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
@@ -5,13 +5,12 @@ import {
 } from "antd";
 
 
-const DATASET_HELPER_TEXT = "The dataset that the archives belong to. If left empty, dataset " +
-    `"${CLP_DEFAULT_DATASET_NAME}" will be used.`;
+const DATASET_HELPER_TEXT = `If left empty, dataset "${CLP_DEFAULT_DATASET_NAME}" will be used.`;
 const DATASET_PLACEHOLDER_TEXT = "The dataset for new archives";
 const TIMESTAMP_KEY_HELPER_TEXT = "If not provided, events will not have assigned" +
     " timestamps and can only be searched from the command line without a timestamp filter.";
-const TIMESTAMP_KEY_PLACEHOLDER_TEXT = "The path (e.g. x.y) for the field containing the log event's" +
-    " timestamp.";
+const TIMESTAMP_KEY_PLACEHOLDER_TEXT =
+    "The path (e.g. x.y) for the field containing the log event's timestamp";
 
 /**
  * Renders additional compression job submission form items for CLP-S storage engine.
