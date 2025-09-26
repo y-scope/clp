0ba30e6f8b2767fadaf6664c853b18574c662354
sitaowang1998 <sitaowang1998@outlook.com>
2025-09-25 23:30:17 -0400

    Add docstring

--- components/job-orchestration/job_orchestration/executor/compress/spider_comrpess.py
+++ components/job-orchestration/job_orchestration/executor/compress/spider_comrpess.py
@@ -13,8 +13,19 @@ def compress(
     tag_ids: list[int],
     clp_io_config_json: str,
     paths_to_compress_json: str,
-    clp_metadata_db_connection_config: str,
+    clp_metadata_db_connection_config: dict[str, bool | int | str | None],
 ) -> dict[str, int | float | str | None]:
+    """
+    Compress files using the general compression function.
+    :param _:
+    :param job_id:
+    :param task_id:
+    :param tag_ids:
+    :param clp_io_config_json: A Json string representing `ClpIoConfig`.
+    :param paths_to_compress_json: A Json string representing `PathsToCompress`.
+    :param clp_metadata_db_connection_config: A dict representing `Database`.
+    :return: A dict representing `CompressionTaskResult`.
+    """
     return general_compress(
         job_id,
         task_id,
