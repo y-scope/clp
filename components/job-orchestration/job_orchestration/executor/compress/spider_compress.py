2889a290eb6afc402103c392e68a89d7876484ec
sitaowang1998 <sitaowang1998@outlook.com>
2025-09-28 11:05:24 -0400

    Use json string for dict

--- components/job-orchestration/job_orchestration/executor/compress/spider_compress.py
+++ components/job-orchestration/job_orchestration/executor/compress/spider_compress.py
@@ -1,3 +1,5 @@
+import json
+
 from clp_py_utils.clp_logging import get_logger
 from job_orchestration.executor.compress.compression_task import compression_entry_point
 from spider_py import TaskContext
@@ -10,11 +12,11 @@ def compress(
     _: TaskContext,
     job_id: int,
     task_id: int,
-    tag_ids: list[int] | None,
+    tag_ids: list[int],
     clp_io_config_json: str,
     paths_to_compress_json: str,
-    clp_metadata_db_connection_config: dict[str, bool | int | str | None],
-) -> dict[str, int | float | str | None]:
+    clp_metadata_db_connection_config_json: str,
+) -> str:
     """
     Compresses files using the general compression entry point.
     :param _: Spider's task context. Not used in the function.
@@ -25,16 +27,17 @@ def compress(
         `job_orchestration.scheduler.constants.ClpIoConfig`.
     :param paths_to_compress_json: A JSON string representation of
         `job_orchestration.scheduler.constants.PathToCompress`.
-    :param clp_metadata_db_connection_config: A dict representation of
+    :param clp_metadata_db_connection_config_json: A JSON string representation of
         `clp_py_utils.clp_config.Database`.
-    :return: A dict representation of `job_orchestration.scheduler.constants.CompressionTaskResult`.
+    :return: A JSON string representation of
+        `job_orchestration.scheduler.constants.CompressionTaskResult`.
     """
-    return compression_entry_point(
+    return json.load(compression_entry_point(
         job_id,
         task_id,
         tag_ids,
         clp_io_config_json,
         paths_to_compress_json,
-        clp_metadata_db_connection_config,
+        json.dumps(clp_metadata_db_connection_config_json),
         logger,
-    )
+    ))
