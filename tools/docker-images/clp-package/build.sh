2b73559f9b74e41b67021a30187a61fda67fb2ce
Junhao Liao <junhao@junhao.ca>
2025-09-29 22:13:54 -0400

    reformat

--- tools/docker-images/clp-package/build.sh
+++ tools/docker-images/clp-package/build.sh
@@ -4,18 +4,19 @@ set -eu
 set -o pipefail
 
 remove_prev_image_and_temp_file() {
-    if [[ -n "$prev_image_id" && "$prev_image_id" != "$new_image_id" ]]; then
-        if docker image inspect "$prev_image_id" >/dev/null 2>&1; then
-            echo "Removing previous image $prev_image_id."
-            docker image remove "$prev_image_id"
-        fi
-    fi
     rm -f "$temp_iid_file"
+
+    [[ -z "$prev_image_id" || "$prev_image_id" == "$new_image_id" ]] && return
+
+    if docker image inspect "$prev_image_id" >/dev/null 2>&1; then
+        echo "Removing previous image $prev_image_id."
+        docker image remove "$prev_image_id"
+    fi
 }
 trap remove_prev_image_and_temp_file EXIT
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-repo_root=${script_dir}/../../../
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+repo_root="${script_dir}/../../../"
 iid_file="${repo_root}/build/clp-package-image.id"
 
 prev_image_id=""
@@ -33,7 +34,7 @@ build_cmd=(
     --file "${script_dir}/Dockerfile"
 )
 
-if command -v git >/dev/null && git -C "$script_dir" rev-parse --is-inside-work-tree >/dev/null ;
+if command -v git >/dev/null && git -C "$script_dir" rev-parse --is-inside-work-tree >/dev/null;
 then
     build_cmd+=(
         --label "org.opencontainers.image.revision=$(git -C "$script_dir" rev-parse HEAD)"
@@ -44,11 +45,11 @@ fi
 "${build_cmd[@]}"
 
 if [[ -s "$temp_iid_file" ]]; then
-    new_image_id=$(<"$temp_iid_file")
+    new_image_id="$(<"$temp_iid_file")"
     echo "$new_image_id" > "$iid_file"
 
     user="${USER:-$(id -un 2>/dev/null || whoami 2>/dev/null || echo unknown)}"
-    short_id=${new_image_id#sha256:}
-    short_id=${short_id:0:4}
+    short_id="${new_image_id#sha256:}"
+    short_id="${short_id:0:4}"
     docker tag "$new_image_id" "clp-package:dev-${user}-${short_id}"
 fi
