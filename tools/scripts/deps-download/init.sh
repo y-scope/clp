edba092727a7322ae9db272fb029411176f4707e
Bingran Hu <bingran.hu@yscope.com>
2025-10-06 12:16:13 -0400

    properly fix cmake-settings

--- tools/scripts/deps-download/init.sh
+++ tools/scripts/deps-download/init.sh
@@ -10,9 +10,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 project_root_dir="$script_dir/../../../"
 download_dep_script="$script_dir/download-dep.py"
 
-readonly YSCOPE_DEV_UTILS_COMMIT_SHA="f1202b9c21dac05d77632e4d3e1768b11e0dcf47"
+readonly YSCOPE_DEV_UTILS_COMMIT_SHA="73d6e964d1432dd5f5d7a9c3d945437d689834cc"
 python3 "${download_dep_script}" \
-    "https://github.com/y-scope/yscope-dev-utils/archive/${YSCOPE_DEV_UTILS_COMMIT_SHA}.zip" \
+    "https://github.com/Bill-hbrhbr/yscope-dev-utils/archive/${YSCOPE_DEV_UTILS_COMMIT_SHA}.zip" \
     "yscope-dev-utils-${YSCOPE_DEV_UTILS_COMMIT_SHA}" \
     "${project_root_dir}/tools/yscope-dev-utils" \
     --extract
