2aa9d34e81de204aafe22e542c02d1f4120c7f6e
Bingran Hu <bingran.hu@yscope.com>
2025-10-07 05:00:40 -0400

    Update dev utils

--- tools/scripts/deps-download/init.sh
+++ tools/scripts/deps-download/init.sh
@@ -10,7 +10,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 project_root_dir="$script_dir/../../../"
 download_dep_script="$script_dir/download-dep.py"
 
-readonly YSCOPE_DEV_UTILS_COMMIT_SHA="73d6e964d1432dd5f5d7a9c3d945437d689834cc"
+readonly YSCOPE_DEV_UTILS_COMMIT_SHA="d7648ea57496ae11540e4bca69f7f2a5eeca6444"
 python3 "${download_dep_script}" \
     "https://github.com/Bill-hbrhbr/yscope-dev-utils/archive/${YSCOPE_DEV_UTILS_COMMIT_SHA}.zip" \
     "yscope-dev-utils-${YSCOPE_DEV_UTILS_COMMIT_SHA}" \
