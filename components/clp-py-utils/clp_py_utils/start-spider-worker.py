41b90d74936de848b6bfc0f531f7bd82166586a6
sitaowang1998 <sitaowang1998@outlook.com>
2025-10-09 10:51:47 -0400

    Add script to start multiple spider worker; Add docker service to start spider worker

---
+++ components/clp-py-utils/clp_py_utils/start-spider-worker.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import pathlib
+import subprocess
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parses command line arguments.
+    :return: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--concurrency", type=int, default=1, help="Number of concurrent spider workers."
+    )
+    parser.add_argument("--storage-url", type=str, required=True, help="Spider storage URL.")
+    parser.add_argument("--host", type=str, required=True, help="Worker host address.")
+    return parser.parse_args()
+
+
+def main() -> None:
+    """Main function to start multiple spider workers."""
+    # Parse arguments
+    args = parse_args()
+    concurrency = args.concurrency
+    storage_url = args.storage_url
+    host = args.host
+
+    clp_home = os.getenv("CLP_HOME", "/opt/clp")
+    spider_worker_path = pathlib.Path(clp_home) / "bin" / "spider_worker"
+
+    # Start multiple spider workers
+    processes = []
+    for _ in range(concurrency):
+        process = subprocess.Popen(
+            [spider_worker_path, "--storage_url", storage_url, "--host", host]
+        )
+        processes.append(process)
+
+    for process in processes:
+        process.wait()
