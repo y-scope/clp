0a7b1570982ee61c74783a12ccf4c5d14093026d
sitaowang1998 <sitaowang1998@outlook.com>
2025-10-18 11:20:44 -0400

    Rename variable

--- components/clp-py-utils/clp_py_utils/serialization_utils.py
+++ components/clp-py-utils/clp_py_utils/serialization_utils.py
@@ -3,14 +3,14 @@ import pathlib
 from strenum import StrEnum
 
 
-def serialize_enum(enum_value: StrEnum) -> str:
+def serialize_enum(member: StrEnum) -> str:
     """
     Serializes a `strenum.StrEnum` member to its underlying value.
 
     :param member:
     :return: The underlying string value of the enum member.
     """
-    return enum_value.value
+    return member.value
 
 
 def serialize_path(path: pathlib.Path) -> str:
