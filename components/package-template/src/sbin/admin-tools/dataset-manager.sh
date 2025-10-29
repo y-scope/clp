d910944c5c563f3226d216ef17abcc7fa51b34d2
Junhao Liao <junhao@junhao.ca>
2025-10-29 02:28:26 -0400

    chore(scripts): Add shellcheck directive comments for sourcing common environment scripts.

--- components/package-template/src/sbin/admin-tools/dataset-manager.sh
+++ components/package-template/src/sbin/admin-tools/dataset-manager.sh
@@ -2,6 +2,8 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 common_env_path="$script_dir/../.common-env.sh"
+
+# shellcheck source=.common-env.sh
 source "$common_env_path"
 
 docker compose -f "$CLP_HOME/docker-compose.runtime.yaml" run --rm clp-runtime \
