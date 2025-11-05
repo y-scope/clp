705a9ab3134dfcc1c15cfa8e106b1ed3abbee5fa
Junhao Liao <junhao@junhao.ca>
2025-11-05 17:46:13 -0500

    fix: Standardize error handling in shell scripts with errexit, nounset, and pipefail (fixes #1556).

--- components/core/tools/scripts/lib_install/liblzma.sh
+++ components/core/tools/scripts/lib_install/liblzma.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Exit on any error
-set -e
-
-# Error on undefined variable
-set -u
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Dependencies:
 # - curl
--- components/package-template/src/sbin/.common-env.sh
+++ components/package-template/src/sbin/.common-env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 package_root=$(readlink -f "$script_dir/..")
 
