f3a5bf10b28e145980b345fb35a80db8048b623b
Junhao Liao <junhao@junhao.ca>
2025-11-10 18:37:47 -0500

    format

--- components/clp-package-utils/clp_package_utils/scripts/native/compress.py
+++ components/clp-package-utils/clp_package_utils/scripts/native/compress.py
@@ -64,9 +64,7 @@ def print_compression_job_status(job_row: dict[str, Any]) -> None:
 
 def handle_job_update(db: Any, db_cursor: Any, job_id: int, no_progress_reporting: bool) -> None:
     if no_progress_reporting:
-        polling_query = (
-            f"SELECT status, status_msg FROM {COMPRESSION_JOBS_TABLE_NAME} WHERE id=%s"
-        )
+        polling_query = f"SELECT status, status_msg FROM {COMPRESSION_JOBS_TABLE_NAME} WHERE id=%s"
     else:
         polling_query = (
             f"SELECT start_time, status, status_msg, uncompressed_size, compressed_size, duration "
@@ -131,7 +129,7 @@ def handle_job(
                 quality=4,
             )
             db_cursor.execute(
-                f"INSERT INTO {COMPRESSION_JOBS_TABLE_NAME} (clp_config) VALUES (%s)",  # noqa: S608
+                f"INSERT INTO {COMPRESSION_JOBS_TABLE_NAME} (clp_config) VALUES (%s)",
                 (compressed_clp_io_config,),
             )
             db.commit()
