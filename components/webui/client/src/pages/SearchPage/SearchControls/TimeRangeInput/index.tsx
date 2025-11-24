e2da61d319feeb93996d8e1f9c94bab83dfd8706
marco <david.marcovitch@yscope.com>
2025-11-24 11:28:09 -0500

    latest

--- components/webui/client/src/pages/SearchPage/SearchControls/TimeRangeInput/index.tsx
+++ components/webui/client/src/pages/SearchPage/SearchControls/TimeRangeInput/index.tsx
@@ -3,8 +3,10 @@ import {useCallback} from "react";
 import {
     DatePicker,
     Select,
+    GetProps,
+    GetProp
 } from "antd";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import {
     CLP_QUERY_ENGINES,
@@ -22,6 +24,62 @@ import {
     TIME_RANGE_OPTION_NAMES,
 } from "./utils";
 
+interface CustomDateInputProps {
+    value: string;
+    'date-range': 'start' | 'end';
+}
+
+/**
+ * Gets display text for time range option
+ */
+const getTimeRangeDisplayText = (option: TIME_RANGE_OPTION, isStart: boolean): string => {
+    switch (option) {
+        case TIME_RANGE_OPTION.LAST_15_MINUTES:
+            return isStart ? "15 minutes ago" : "Now";
+        case TIME_RANGE_OPTION.LAST_HOUR:
+            return isStart ? "1 hour ago" : "Now";
+        case TIME_RANGE_OPTION.TODAY:
+            return isStart ? "Start of today" : "End of today";
+        case TIME_RANGE_OPTION.YESTERDAY:
+            return isStart ? "Start of yesterday" : "End of yesterday";
+        case TIME_RANGE_OPTION.LAST_7_DAYS:
+            return isStart ? "7 days ago" : "Now";
+        case TIME_RANGE_OPTION.LAST_30_DAYS:
+            return isStart ? "30 days ago" : "Now";
+        case TIME_RANGE_OPTION.LAST_12_MONTHS:
+            return isStart ? "12 months ago" : "Now";
+        case TIME_RANGE_OPTION.MONTH_TO_DATE:
+            return isStart ? "Start of month" : "Now";
+        case TIME_RANGE_OPTION.YEAR_TO_DATE:
+            return isStart ? "Start of year" : "Now";
+        case TIME_RANGE_OPTION.ALL_TIME:
+            return isStart ? "Earliest" : "Latest";
+        case TIME_RANGE_OPTION.CUSTOM:
+        default:
+            return isStart ? "Start date" : "End date";
+    }
+};
+
+/**
+ * Custom input component for DatePicker that displays custom text
+ */
+const CustomDateInput = (props: CustomDateInputProps) => {
+    const {value, 'date-range': dateRange} = props;
+    const timeRangeOption = useSearchStore((state) => state.timeRangeOption);
+
+    const isStartInput = dateRange === 'start';
+    const displayText = timeRangeOption === TIME_RANGE_OPTION.CUSTOM 
+        ? (value || (isStartInput ? 'Start date' : 'End date'))
+        : getTimeRangeDisplayText(timeRangeOption, isStartInput);
+
+    return (
+        <input
+            {...props}
+            value={displayText}
+            readOnly
+        />
+    );
+};
 
 /**
  * Renders controls for selecting a time range for queries. By default, the component is
@@ -86,20 +144,22 @@ const TimeRangeInput = () => {
                 disabled={searchUiState === SEARCH_UI_STATE.QUERY_ID_PENDING ||
                             searchUiState === SEARCH_UI_STATE.QUERYING}
                 onChange={handleSelectChange}/>
-            {timeRangeOption === TIME_RANGE_OPTION.CUSTOM && (
-                <DatePicker.RangePicker
-                    allowClear={true}
-                    className={styles["rangePicker"] || ""}
-                    renderExtraFooter={renderFooter}
-                    showTime={true}
-                    size={"middle"}
-                    value={timeRange}
-                    disabled={searchUiState === SEARCH_UI_STATE.QUERY_ID_PENDING ||
-                                searchUiState === SEARCH_UI_STATE.QUERYING}
-                    onCalendarChange={(dates) => {
-                        handleRangePickerChange(dates);
-                    }}/>
-            )}
+            <DatePicker.RangePicker
+                allowClear={true}
+                className={styles["rangePicker"] || ""}
+                renderExtraFooter={renderFooter}
+                showTime={true}
+                components={{
+                    input: CustomDateInput,
+                }}
+                size={"middle"}
+                value={timeRange}
+                disabled={timeRangeOption !== TIME_RANGE_OPTION.CUSTOM ||
+                            searchUiState === SEARCH_UI_STATE.QUERY_ID_PENDING ||
+                            searchUiState === SEARCH_UI_STATE.QUERYING}
+                onCalendarChange={(dates) => {
+                    handleRangePickerChange(dates);
+                }}/>
         </div>
     );
 };
