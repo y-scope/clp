b5cf2484f9628d604f7cfd526ce468e2db93a0f3
Quinn <q.mitchell@mail.utoronto.ca>
2025-11-25 20:40:19 +0000

    Revert "Revert "Move content to other PRs.""
    This reverts commit a71bcb034d6d391dca89b1c96e6a5acfd024a0b6.

--- integration-tests/tests/conftest.py
+++ integration-tests/tests/conftest.py
@@ -1,7 +1,5 @@
 """Global pytest setup."""
 
-import pytest
-
 # Make the fixtures defined in `tests/fixtures/` globally available without imports.
 pytest_plugins = [
     "tests.fixtures.integration_test_logs",
@@ -9,16 +7,3 @@ pytest_plugins = [
     "tests.fixtures.package_instance",
     "tests.fixtures.package_config",
 ]
-
-
-def pytest_addoption(parser: pytest.Parser) -> None:
-    """
-    Adds options for pytest.
-
-    :param parser:
-    """
-    parser.addini(
-        "BASE_PORT",
-        "Base port for CLP package integration tests.",
-        default="55000",
-    )
--- integration-tests/tests/fixtures/package_instance.py
+++ integration-tests/tests/fixtures/package_instance.py
@@ -16,10 +16,7 @@ from tests.utils.package_utils import (
 
 
 @pytest.fixture
-def fixt_package_instance(
-    fixt_package_config: PackageConfig,
-    request: pytest.FixtureRequest,
-) -> Iterator[PackageInstance]:
+def fixt_package_instance(fixt_package_config: PackageConfig) -> Iterator[PackageInstance]:
     """
     Starts a CLP package instance for the given configuration and stops it during teardown.
 
@@ -35,12 +32,7 @@ def fixt_package_instance(
         instance = PackageInstance(package_config=fixt_package_config)
         yield instance
     except RuntimeError:
-        base_port_string = request.config.getini("BASE_PORT")
-        pytest.fail(
-            f"Failed to start the {mode_name} package. This could mean that one of the ports"
-            f" derived from BASE_PORT={base_port_string} was unavailable. Try changing BASE_PORT in"
-            " .pytest.ini."
-        )
+        pytest.fail(f"Failed to start the {mode_name} package.")
     finally:
         if instance is not None:
             stop_clp_package(instance)
