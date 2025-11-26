3baa0ac0013205e28b4ae4a4614e54ee5b859e76
hoophalab <200652805+hoophalab@users.noreply.github.com>
2025-11-26 14:45:59 -0500

    comment

--- components/api-server/src/bin/api_server.rs
+++ components/api-server/src/bin/api_server.rs
@@ -1,24 +1,8 @@
 use anyhow::Context;
-use axum::{
-    Json,
-    extract::{Path, State},
-    http::StatusCode,
-    response::{
-        IntoResponse,
-        Sse,
-        sse::{Event, KeepAlive},
-    },
-    routing::get,
-};
 use clap::Parser;
 use clp_rust_utils::{clp_config::package, serde::yaml};
-use futures::{Stream, StreamExt};
-use thiserror::Error;
-use tower_http::cors::{Any, CorsLayer};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::{self, fmt::writer::MakeWriterExt};
-use utoipa::OpenApi;
-use utoipa_axum::{router::OpenApiRouter, routes};
 
 #[derive(Parser)]
 #[command(version, about = "API Server for CLP.")]
@@ -97,141 +81,15 @@ async fn main() -> anyhow::Result<()> {
         .await
         .context(format!("Cannot listen to {addr}"))?;
 
-    let client = clp_client::Client::connect(&config, &credentials)
+    let client = api_server::client::Client::connect(&config, &credentials)
         .await
         .context("Cannot connect to CLP")?;
 
-    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .route("/", get(health))
-        .routes(routes!(health))
-        .routes(routes!(query))
-        .routes(routes!(query_results))
-        .with_state(client)
-        .split_for_parts();
-    let api_json = api.to_json()?;
-    let router = router
-        .route(
-            "/openapi.json",
-            get(|| async { (StatusCode::OK, api_json) }),
-        )
-        .layer(CorsLayer::new().allow_origin(Any));
+    let router = api_server::routes::from_client(client)?;
 
     tracing::info!("Server started at {addr}");
     axum::serve(listener, router)
         .with_graceful_shutdown(shutdown_signal())
         .await?;
     Ok(())
-}
-
-// `utoipa::OpenApi` triggers `clippy::needless_for_each`
-#[allow(clippy::needless_for_each)]
-mod api_doc {
-    #[derive(utoipa::OpenApi)]
-    #[openapi(info(
-        title = "API Server",
-        description = "API Server for CLP",
-        contact(name = "YScope")
-    ))]
-    pub struct ApiDoc;
-}
-use api_doc::ApiDoc;
-
-#[utoipa::path(
-    get,
-    path = "/health",
-    responses((status = OK, body = String))
-)]
-async fn health() -> String {
-    "API server is running".to_owned()
-}
-
-#[utoipa::path(
-    post,
-    path = "/query",
-    description = "Submits a new query job.",
-    request_body(
-        content= clp_client::QueryConfig,
-        example = json!({
-            "query_string": "*",
-            "dataset": "default",
-            "begin_timestamp": 0,
-            "end_timestamp": 17356896,
-            "ignore_case": true,
-            "max_num_results": 0,
-            "write_to_file": false
-        }),
-    responses(
-        (
-            status = OK,
-            body = serde_json::Value,
-            example = json!({"query_results_uri":"/query_results/1"})
-        ),
-        (status = INTERNAL_SERVER_ERROR)
-    )
-)]
-async fn query(
-    State(client): State<clp_client::Client>,
-    Json(query_config): Json<clp_client::QueryConfig>,
-) -> Result<Json<serde_json::Value>, HandlerError> {
-    let search_job_id = client.submit_query(query_config).await?;
-    let uri = format!("/query_results/{search_job_id}");
-    Ok(Json(serde_json::json!({"query_results_uri": uri})))
-}
-
-#[utoipa::path(
-    get,
-    path = "/query_results/{search_job_id}",
-    description = "Streams the results of a previously submitted query as Server-Sent Events \
-        (SSE).",
-    responses(
-        (
-            status = OK,
-            body = String,
-            content_type = "text/event-stream",
-            description = "Server-Sent Events stream of query results. Each event contains a \
-                single line of the query result in JSON format.",
-        ),
-        (status = INTERNAL_SERVER_ERROR)
-    )
-)]
-async fn query_results(
-    State(client): State<clp_client::Client>,
-    Path(search_job_id): Path<u64>,
-) -> Result<Sse<impl Stream<Item = Result<Event, HandlerError>>>, HandlerError> {
-    let results_stream = client.fetch_results(search_job_id).await?;
-    let event_stream = results_stream.map(|res| {
-        let message = res?;
-        let trimmed_message = message.trim();
-        if trimmed_message.lines().count() != 1 {
-            return Err(HandlerError::InternalServer);
-        }
-        Ok(Event::default().data(trimmed_message))
-    });
-    Ok(Sse::new(event_stream).keep_alive(KeepAlive::default()))
-}
-
-/// Generic errors for request handlers.
-#[derive(Error, Debug)]
-enum HandlerError {
-    #[error("Internal server error")]
-    InternalServer,
-}
-
-trait IntoHandlerError {}
-
-impl IntoHandlerError for axum::Error {}
-
-impl IntoHandlerError for clp_client::ClientError {}
-
-impl<T: IntoHandlerError> From<T> for HandlerError {
-    fn from(_: T) -> Self {
-        Self::InternalServer
-    }
-}
-
-/// Converts [`HandlerError`] into an HTTP response.
-impl IntoResponse for HandlerError {
-    fn into_response(self) -> axum::response::Response {
-        StatusCode::INTERNAL_SERVER_ERROR.into_response()
-    }
 }
