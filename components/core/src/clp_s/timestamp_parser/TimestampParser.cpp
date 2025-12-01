84d3ad64c306495aa5d02cd40385a55f79b96582
dgibson <devinbook1@gmail.com>
2025-12-01 19:38:30 +0000

    Improve formatting of timestamp patterns.

--- components/core/src/clp_s/timestamp_parser/TimestampParser.cpp
+++ components/core/src/clp_s/timestamp_parser/TimestampParser.cpp
@@ -100,13 +100,15 @@ constexpr std::array cDefaultDateTimePatterns{
                 R"(\B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec} \d, \Y \I:\M:\s \p)"
         },
         std::string_view{
-                R"(\B{January,February,March,April,May,June,July,August,September,October,November,December} \d, \Y \H:\M)"
+                R"(\B{January,February,March,April,May,June,July,August,September,October,)"
+                R"(November,December} \d, \Y \H:\M)"
         },
         std::string_view{
                 R"([\d\O{-/}\B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec}\O{-/}\Y:\H:\M:\s)"
         },
         std::string_view{
-                R"(\A{Sun,Mon,Tue,Wed,Thu,Fri,Sat} \B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec} \e \H:\M:\s \Y)"
+                R"(\A{Sun,Mon,Tue,Wed,Thu,Fri,Sat} \B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,)"
+                R"(Dec} \e \H:\M:\s \Y)"
         },
         std::string_view{R"(\B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec} \d \H:\M:\s)"},
         std::string_view{R"(\B{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec} \d \H:\M:\s\Z)"},
