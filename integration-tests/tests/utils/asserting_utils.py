3fa94cfac972ffb762f8da2c79959f239bb052fc
Quinn <q.mitchell@mail.utoronto.ca>
2025-12-05 15:35:26 +0000

    Rabbit.

--- integration-tests/tests/utils/asserting_utils.py
+++ integration-tests/tests/utils/asserting_utils.py
@@ -46,7 +46,7 @@ def validate_package_running(package_instance: PackageInstance) -> None:
     project_name = f"clp-package-{instance_id}"
     running_containers = list_running_containers_in_compose_project(project_name)
 
-    # Construct list of unique running component names.
+    # Construct list of unique running component names and sort alphabetically.
     running_components = []
     prefix = f"{project_name}-"
     regex_suffix = r"-\d+"
@@ -55,13 +55,20 @@ def validate_package_running(package_instance: PackageInstance) -> None:
         component_name = strip_regex_suffix(component_name, regex_suffix)
         if component_name not in running_components:
             running_components.append(component_name)
+    running_components = sorted(running_components)
 
-    # Get list of required components.
-    required_components = package_instance.package_config.component_list
-
-    # Alphabetize each list and compare them.
-    required_components.sort()
-    running_components.sort()
+    # Get list of required components and sort alphabetically.
+    required_components = sorted(package_instance.package_config.component_list)
 
     if required_components != running_components:
-        pytest.fail("The list of required components did not match the list of running components.")
+        fail_msg = "Component mismatch."
+
+        missing_components = set(required_components) - set(running_components)
+        if missing_components:
+            fail_msg += f" Missing components: {missing_components}."
+
+        unexpected_components = set(running_components) - set(required_components)
+        if unexpected_components:
+            fail_msg += f" Unexpected components: {unexpected_components}."
+
+        pytest.fail(fail_msg)
