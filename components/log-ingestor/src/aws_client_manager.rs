76c3d32f70cca61161c4bf7b06af77df4fb8709e
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-12-05 14:42:26 -0500

    Move clients.

--- components/log-ingestor/src/aws_client_manager.rs
+++ components/log-ingestor/src/aws_client_manager.rs
@@ -2,6 +2,10 @@ use anyhow::Result;
 use async_trait::async_trait;
 use aws_sdk_s3::Client as S3Client;
 use aws_sdk_sqs::Client as SqsClient;
+use clp_rust_utils::{
+    s3::create_new_client as create_s3_client,
+    sqs::create_new_client as create_sqs_client,
+};
 
 /// A marker trait for AWS client types.
 pub trait AwsClientType: Clone {}
@@ -47,6 +51,18 @@ impl SqsClientWrapper {
     pub const fn from(client: SqsClient) -> Self {
         Self { client }
     }
+
+    pub async fn create(region: &str, access_key_id: &str, secret_access_key: &str) -> Self {
+        let sqs_endpoint = format!("https://sqs.{region}.amazonaws.com");
+        let sqs_client = create_sqs_client(
+            sqs_endpoint.as_str(),
+            region,
+            access_key_id,
+            secret_access_key,
+        )
+        .await;
+        Self::from(sqs_client)
+    }
 }
 
 /// A simple wrapper around an `S3Client` that implements the `AwsClientManagerType` trait.
@@ -65,5 +81,17 @@ impl S3ClientWrapper {
     #[must_use]
     pub const fn from(client: S3Client) -> Self {
         Self { client }
+    }
+
+    pub async fn create(region: &str, access_key_id: &str, secret_access_key: &str) -> Self {
+        let s3_endpoint = format!("https://s3.{region}.amazonaws.com");
+        let s3_client = create_s3_client(
+            s3_endpoint.as_str(),
+            region,
+            access_key_id,
+            secret_access_key,
+        )
+        .await;
+        Self::from(s3_client)
     }
 }
--- components/log-ingestor/src/ingestion_job_manager.rs
+++ components/log-ingestor/src/ingestion_job_manager.rs
@@ -4,8 +4,7 @@ use async_trait::async_trait;
 use clp_rust_utils::{
     clp_config::AwsCredentials,
     job_config::S3IngestionBaseConfig,
-    s3::{ObjectMetadata, create_new_client as create_s3_client},
-    sqs::create_new_client as create_sqs_client,
+    s3::ObjectMetadata,
 };
 use tokio::sync::{Mutex, mpsc};
 use uuid::Uuid;
@@ -52,9 +51,12 @@ impl IngestionJobManager {
     /// * Forwards [`Self::create_s3_ingestion_job`]'s return values on failure.
     pub async fn create_s3_scanner_job(&self, config: S3ScannerConfig) -> Result<Uuid, Error> {
         let ingestion_job_config = config.base.clone();
-        let s3_client_manager = self
-            .create_s3_client_manager(config.base.region.as_str())
-            .await;
+        let s3_client_manager = S3ClientWrapper::create(
+            config.base.region.as_str(),
+            self.aws_credentials.access_key_id.as_str(),
+            self.aws_credentials.secret_access_key.as_str(),
+        )
+        .await;
         self.create_s3_ingestion_job(ingestion_job_config, move |job_id, sender| {
             let scanner = S3Scanner::spawn(job_id, s3_client_manager, config, sender);
             IngestionJob::S3Scanner(scanner)
@@ -75,9 +77,12 @@ impl IngestionJobManager {
     /// * Forwards [`Self::create_s3_ingestion_job`]'s return values on failure.
     pub async fn create_sqs_listener(&self, config: SqsListenerConfig) -> Result<Uuid, Error> {
         let ingestion_job_config = config.base.clone();
-        let sqs_client_manager = self
-            .create_sqs_client_manager(config.base.region.as_str())
-            .await;
+        let sqs_client_manager = SqsClientWrapper::create(
+            config.base.region.as_str(),
+            self.aws_credentials.access_key_id.as_str(),
+            self.aws_credentials.secret_access_key.as_str(),
+        )
+        .await;
         self.create_s3_ingestion_job(ingestion_job_config, move |job_id, sender| {
             let listener = crate::ingestion_job::SqsListener::spawn(
                 job_id,
@@ -202,36 +207,6 @@ impl IngestionJobManager {
         drop(job_table);
 
         Ok(job_id)
-    }
-
-    /// # Returns
-    ///
-    /// A new S3 client (wrapped by [`S3ClientWrapper`]) for the specified region.
-    async fn create_s3_client_manager(&self, region: &str) -> S3ClientWrapper {
-        let s3_endpoint = format!("https://s3.{region}.amazonaws.com");
-        let s3_client = create_s3_client(
-            s3_endpoint.as_str(),
-            region,
-            self.aws_credentials.access_key_id.as_str(),
-            self.aws_credentials.secret_access_key.as_str(),
-        )
-        .await;
-        S3ClientWrapper::from(s3_client)
-    }
-
-    /// # Returns
-    ///
-    /// A new SQS client (wrapped by [`SqsClientWrapper`]) for the specified region.
-    async fn create_sqs_client_manager(&self, region: &str) -> SqsClientWrapper {
-        let sqs_endpoint = format!("https://sqs.{region}.amazonaws.com");
-        let sqs_client = create_sqs_client(
-            sqs_endpoint.as_str(),
-            region,
-            self.aws_credentials.access_key_id.as_str(),
-            self.aws_credentials.secret_access_key.as_str(),
-        )
-        .await;
-        SqsClientWrapper::from(sqs_client)
     }
 
     /// # Returns
