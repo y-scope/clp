8fdd28ded02fdf35887098f780733c95834cdb76
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-12-10 09:57:59 -0500

    Fix typo.

--- components/core/tests/search_test_utils.hpp
+++ components/core/tests/search_test_utils.hpp
@@ -17,7 +17,7 @@
 
 /**
  * Type alias for variable information in tests.
- * Elements: (is_dict_var, is_percise_var, var_dict_ids)
+ * Elements: (is_dict_var, is_precise_var, var_dict_ids)
  */
 using VarInfo = std::tuple<bool, bool, std::unordered_set<clp::variable_dictionary_id_t>>;
 
