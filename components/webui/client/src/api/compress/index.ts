5646c26d5c2dfe47603908c528d8836aef8e801b
Your Name <you@example.com>
2025-12-15 17:45:03 +0000

    latest

--- components/webui/client/src/api/compress/index.ts
+++ components/webui/client/src/api/compress/index.ts
@@ -4,20 +4,6 @@ import {
 } from "@webui/common/schemas/compression";
 import axios from "axios";
 
-interface CompressionJobWithConfig {
-    _id: number;
-    compressed_size: number;
-    dataset: string | null;
-    duration: number | null;
-    paths: string[];
-    retrieval_time: number;
-    start_time: string | null;
-    status: number;
-    status_msg: string;
-    uncompressed_size: number;
-    update_time: string;
-}
-
 
 /**
  * Submits a compression job.
@@ -32,22 +18,4 @@ const submitCompressionJob = async (payload: CompressionJobCreation): Promise<Co
     return data;
 };
 
-/**
- * Fetches compression jobs with decoded dataset and paths.
- *
- * @param lastUpdateTimestampSeconds
- * @return
- */
-const fetchCompressionJobs = async (lastUpdateTimestampSeconds: number) => {
-    const {data} = await axios.get<CompressionJobWithConfig[]>(
-        "/api/compress/jobs",
-        {params: {lastUpdateTimestampSeconds}}
-    );
-
-    return data;
-};
-
-
 export {submitCompressionJob};
-export type {CompressionJobWithConfig};
-export {fetchCompressionJobs};
