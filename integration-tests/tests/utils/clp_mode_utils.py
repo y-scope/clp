e37037e9c7e456855c501786c956632f99e19214
Junhao Liao <junhao@junhao.ca>
2025-12-15 23:53:28 -0500

    refactor(deployment): Standardizes service names to use hyphens instead of underscores for RFC1123 compliance (resolves #1777).

--- integration-tests/tests/utils/clp_mode_utils.py
+++ integration-tests/tests/utils/clp_mode_utils.py
@@ -40,38 +40,27 @@ CLP_MODE_CONFIGS: dict[str, Callable[[], ClpConfig]] = {
 }
 
 
-# TODO: This will eventually be replaced by a formalized mapping between component and service.
-def _to_docker_compose_service_name(name: str) -> str:
-    """
-    Convert a component name to a Docker Compose service name.
-
-    :param name:
-    :return: Service name with underscores replaced by hyphens
-    """
-    return name.replace("_", "-")
-
-
 # TODO: Modify these component lists when the Presto Docker Compose project is integrated with the
 # CLP Docker compose project.
 CLP_BASE_COMPONENTS = [
-    _to_docker_compose_service_name(DB_COMPONENT_NAME),
-    _to_docker_compose_service_name(QUEUE_COMPONENT_NAME),
-    _to_docker_compose_service_name(REDIS_COMPONENT_NAME),
-    _to_docker_compose_service_name(REDUCER_COMPONENT_NAME),
-    _to_docker_compose_service_name(RESULTS_CACHE_COMPONENT_NAME),
-    _to_docker_compose_service_name(COMPRESSION_SCHEDULER_COMPONENT_NAME),
-    _to_docker_compose_service_name(COMPRESSION_WORKER_COMPONENT_NAME),
-    _to_docker_compose_service_name(WEBUI_COMPONENT_NAME),
+    DB_COMPONENT_NAME,
+    QUEUE_COMPONENT_NAME,
+    REDIS_COMPONENT_NAME,
+    REDUCER_COMPONENT_NAME,
+    RESULTS_CACHE_COMPONENT_NAME,
+    COMPRESSION_SCHEDULER_COMPONENT_NAME,
+    COMPRESSION_WORKER_COMPONENT_NAME,
+    WEBUI_COMPONENT_NAME,
 ]
 
 CLP_QUERY_COMPONENTS = [
-    _to_docker_compose_service_name(QUERY_SCHEDULER_COMPONENT_NAME),
-    _to_docker_compose_service_name(QUERY_WORKER_COMPONENT_NAME),
+    QUERY_SCHEDULER_COMPONENT_NAME,
+    QUERY_WORKER_COMPONENT_NAME,
 ]
 
-CLP_API_SERVER_COMPONENT = _to_docker_compose_service_name(API_SERVER_COMPONENT_NAME)
-CLP_GARBAGE_COLLECTOR_COMPONENT = _to_docker_compose_service_name(GARBAGE_COLLECTOR_COMPONENT_NAME)
-CLP_MCP_SERVER_COMPONENT = _to_docker_compose_service_name(MCP_SERVER_COMPONENT_NAME)
+CLP_API_SERVER_COMPONENT = API_SERVER_COMPONENT_NAME
+CLP_GARBAGE_COLLECTOR_COMPONENT = GARBAGE_COLLECTOR_COMPONENT_NAME
+CLP_MCP_SERVER_COMPONENT = MCP_SERVER_COMPONENT_NAME
 
 
 def get_clp_config_from_mode(mode_name: str) -> ClpConfig:
