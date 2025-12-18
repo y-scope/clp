caf9d379809a34086a22d270f7c4ff042b4e726a
Junhao Liao <junhao@junhao.ca>
2025-12-17 19:26:11 -0500

    properly escape multiline mkdir command

--- tools/deployment/package-helm/test.sh
+++ tools/deployment/package-helm/test.sh
@@ -55,11 +55,11 @@ wait_for_pods() {
 
 kind delete cluster --name clp-test
 rm -rf "$CLP_HOME"
-mkdir -p  "$CLP_HOME/var/"{data,log}/{database,queue,redis,results_cache}
-          "$CLP_HOME/var/data/"{archives,streams}
-          "$CLP_HOME/var/log/"{compression_scheduler,compression_worker,user}
-          "$CLP_HOME/var/log/"{query_scheduler,query_worker,reducer}
-          "$CLP_HOME/var/tmp"
+mkdir -p  "$CLP_HOME/var/"{data,log}/{database,queue,redis,results_cache} \
+          "$CLP_HOME/var/data/"{archives,streams} \
+          "$CLP_HOME/var/log/"{compression_scheduler,compression_worker,user} \
+          "$CLP_HOME/var/log/"{query_scheduler,query_worker,reducer} \
+          "$CLP_HOME/var/tmp" \
           "$CLP_HOME/samples"
 
 # Download sample datasets in the background
