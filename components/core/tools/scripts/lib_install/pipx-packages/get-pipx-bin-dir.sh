e0ff9c1ffa644882329bed541e10ae900de7ed3c
Bingran Hu <bingran.hu@yscope.com>
2026-01-19 01:56:21 -0500

    Make pipx bin dir failure msg more clear

--- components/core/tools/scripts/lib_install/pipx-packages/get-pipx-bin-dir.sh
+++ components/core/tools/scripts/lib_install/pipx-packages/get-pipx-bin-dir.sh
@@ -9,6 +9,9 @@ is_sudo_from_non_root=$(( EUID == 0 && ${SUDO_UID:-0} != 0 ))
 if (( is_sudo_from_non_root )); then
     pipx_bin_dir_cmd_args+=(sudo --user="${SUDO_USER}" --set-home --)
 fi
-pipx_bin_dir_cmd_args+=(pipx environment --value=PIPX_BIN_DIR)
+pipx_bin_dir_cmd_args+=(pipx environment --value PIPX_BIN_DIR)
 
-"${pipx_bin_dir_cmd_args[@]}"
+"${pipx_bin_dir_cmd_args[@]}" || {
+    echo "Failed to determine pipx bin directory via pipx environment." >&2
+    exit 1
+}
