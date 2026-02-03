8df42ec934f3370f45febce010d05f72543105ec
kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2026-02-03 14:26:22 -0500

    chore: Bump version for 0.9.0 release and remove edit page button from docs. (#1954)

--- components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
+++ components/webui/client/src/pages/IngestPage/Compress/ClpSFormItems.tsx
@@ -18,7 +18,7 @@ const TIMESTAMP_KEY_HELPER_TEXT = (
         the command line without a timestamp filter. Certain characters require escaping. See the
         {" "}
         <Typography.Link
-            href={"https://docs.yscope.com/clp/main/user-docs/reference-json-search-syntax.html#characters-that-require-escaping"}
+            href={"https://docs.yscope.com/clp/v0.9.0/user-docs/reference-json-search-syntax.html#characters-that-require-escaping"}
             rel={"noopener"}
             target={"_blank"}
         >
@@ -50,7 +50,7 @@ const UNSTRUCTURED_HELPER_TEXT = (
         fields. See the
         {" "}
         <Typography.Link
-            href={"https://docs.yscope.com/clp/main/user-docs/quick-start/clp-json.html#compressing-unstructured-text-logs"}
+            href={"https://docs.yscope.com/clp/v0.9.0/user-docs/quick-start/clp-json.html#compressing-unstructured-text-logs"}
             rel={"noopener"}
             target={"_blank"}
         >
