788dd14f88854d9bf378108108063309f24c5960
LinZhihao-723 <pleiades3190@gmail.com>
2026-02-16 21:17:54 -0500

    Merge

--- components/log-ingestor/src/ingestion_job_manager.rs
+++ components/log-ingestor/src/ingestion_job_manager.rs
@@ -157,6 +157,7 @@ impl IngestionJobManagerState {
     /// * [`Error::InvalidNumConcurrentListenerTasks`] if the given number of concurrent listener
     ///   tasks is invalid.
     /// * Forwards [`Self::create_s3_ingestion_job`]'s return values on failure.
+    /// * Forwards [`ValidatedSqsListenerConfig::validate_and_create`]'s return values on failure.
     pub async fn create_sqs_listener_job(
         &self,
         raw_config: SqsListenerConfig,
