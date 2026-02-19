4ecd3990f834cba9068d9452e43a2920682c4fa2
SharafMohamed <SharafMohamed@users.noreply.github.com>
2026-02-19 11:51:20 -0500

    Rename macro.

--- components/core/src/clp/SchemaSearcher.hpp
+++ components/core/src/clp/SchemaSearcher.hpp
@@ -24,7 +24,7 @@
 #include <clp/VariableDictionaryReaderReq.hpp>
 
 namespace clp {
-#ifdef CLP_ENABLE_TESTS_IN_CPP
+#ifdef CLP_ENABLE_TESTS
 class SchemaSearcherTest;
 #endif
 
@@ -66,7 +66,7 @@ class SchemaSearcherTest;
  * generation, and per-variable processing.
  */
 class SchemaSearcher {
-#ifdef CLP_ENABLE_TESTS_IN_CPP
+#ifdef CLP_ENABLE_TESTS
     friend class SchemaSearcherTest;
 #endif
 
