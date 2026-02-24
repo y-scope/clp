b6018099c5c7fe4be293d5869dbaf0dedc4006b1
Junhao Liao <junhao@junhao.ca>
2026-02-24 23:10:51 +0800

    avoid f-string in ValueError() constructor

--- components/clp-package-utils/clp_package_utils/scripts/native/utils.py
+++ components/clp-package-utils/clp_package_utils/scripts/native/utils.py
@@ -90,7 +90,8 @@ def validate_datasets_exist(db_config: Database, datasets: list[str]) -> None:
         existing_datasets = fetch_existing_datasets(db_cursor, table_prefix)
         missing = [ds for ds in datasets if ds not in existing_datasets]
         if len(missing) > 0:
-            raise ValueError(f"Dataset(s) {missing} don't exist.")
+            err_msg = f"Dataset(s) {missing} don't exist."
+            raise ValueError(err_msg)
 
 
 def wait_for_query_job(sql_adapter: SqlAdapter, job_id: int) -> QueryJobStatus:
