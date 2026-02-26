d74c856f32cdebe673b3ce8af774d2e9f1d55408
Junhao Liao <junhao.liao@yscope.com>
2026-02-26 15:57:26 +0800

    refactor set_up_logging - apply suggestion
    Co-authored-by: hoophalab <200652805+hoophalab@users.noreply.github.com>

--- components/clp-rust-utils/src/logging.rs
+++ components/clp-rust-utils/src/logging.rs
@@ -10,28 +10,7 @@ use tracing_subscriber::{self, fmt::writer::MakeWriterExt};
 /// rolling file (`{log_filename}`) in that directory. The returned [`WorkerGuard`]
 /// must be held for the lifetime of the program to ensure file logs are flushed.
 pub fn set_up_logging(log_filename: &str) -> Option<WorkerGuard> {
-    if let Ok(logs_directory) = std::env::var("CLP_LOGS_DIR") {
-        let logs_directory = std::path::Path::new(logs_directory.as_str());
-        let file_appender =
-            RollingFileAppender::new(Rotation::HOURLY, logs_directory, log_filename);
-        let (non_blocking_writer, guard) = tracing_appender::non_blocking(file_appender);
-        tracing_subscriber::fmt()
-            .event_format(
-                tracing_subscriber::fmt::format()
-                    .with_level(true)
-                    .with_target(false)
-                    .with_file(true)
-                    .with_line_number(true)
-                    .json(),
-            )
-            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-            .with_ansi(false)
-            .with_writer(std::io::stdout.and(non_blocking_writer))
-            .init();
-        return Some(guard);
-    }
-
-    tracing_subscriber::fmt()
+    let subscriber = tracing_subscriber::fmt()
         .event_format(
             tracing_subscriber::fmt::format()
                 .with_level(true)
@@ -41,8 +20,20 @@ pub fn set_up_logging(log_filename: &str) -> Option<WorkerGuard> {
                 .json(),
         )
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_ansi(false)
-        .with_writer(std::io::stdout)
-        .init();
-    None
+        .with_ansi(false);
+
+    if let Ok(logs_directory) = std::env::var("CLP_LOGS_DIR") {
+        let logs_directory = std::path::Path::new(logs_directory.as_str());
+        let file_appender =
+            RollingFileAppender::new(Rotation::HOURLY, logs_directory, log_filename);
+        let (non_blocking_writer, guard) = tracing_appender::non_blocking(file_appender);
+
+        subscriber
+            .with_writer(std::io::stdout.and(non_blocking_writer))
+            .init();
+        Some(guard)
+    } else {
+        subscriber.with_writer(std::io::stdout).init();
+        None
+    }
 }
