a91e5f71f0715d7d6b3ea7c177e1b39b3e6a24a6
Quinn Taylor Mitchell <q.mitchell@mail.utoronto.ca>
2026-03-02 12:35:06 -0500

    feat(integration-tests): Test package compression. (#1801)
    Co-authored-by: Jack Luo <jack.luo@yscope.com>
    Co-authored-by: Junhao Liao <junhao.liao@yscope.com>

--- integration-tests/tests/package_tests/clp_text/test_clp_text.py
+++ integration-tests/tests/package_tests/clp_text/test_clp_text.py
@@ -12,9 +12,11 @@ from clp_py_utils.clp_config import (
 
 from tests.utils.asserting_utils import (
     validate_package_instance,
+    verify_package_compression,
 )
 from tests.utils.clp_mode_utils import CLP_BASE_COMPONENTS
-from tests.utils.config import PackageInstance, PackageModeConfig
+from tests.utils.config import PackageCompressionJob, PackageInstance, PackageModeConfig
+from tests.utils.package_utils import run_package_compression_script
 
 logger = logging.getLogger(__name__)
 
@@ -49,3 +51,37 @@ def test_clp_text_startup(fixt_package_instance: PackageInstance) -> None:
 
     log_msg = "test_clp_text_startup was successful."
     logger.info(log_msg)
+
+
+@pytest.mark.compression
+def test_clp_text_compression_text_multifile(fixt_package_instance: PackageInstance) -> None:
+    """
+    Validate that the `clp-text` package successfully compresses the `text-multifile` dataset.
+
+    :param fixt_package_instance:
+    """
+    validate_package_instance(fixt_package_instance)
+
+    # Clear archives before compressing.
+    package_test_config = fixt_package_instance.package_test_config
+    package_path_config = package_test_config.path_config
+    package_path_config.clear_package_archives()
+
+    # Compress a dataset.
+    compression_job = PackageCompressionJob(
+        path_to_original_dataset=(
+            package_path_config.clp_text_test_data_path / "text-multifile" / "logs"
+        ),
+        options=None,
+        positional_args=None,
+    )
+    run_package_compression_script(compression_job, package_test_config)
+
+    # Check the correctness of compression.
+    verify_package_compression(compression_job.path_to_original_dataset, package_test_config)
+
+    log_msg = "test_clp_text_compression_text_multifile was successful."
+    logger.info(log_msg)
+
+    # Clear archives.
+    package_path_config.clear_package_archives()
