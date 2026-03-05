f5b80b05317ff205ec4d71040ea9e701377148c0
SharafMohamed <SharafMohamed@users.noreply.github.com>
2026-03-05 16:35:29 -0500

    Add ProfilerReported documentation.

--- components/core/src/clp/ProfilerReporter.hpp
+++ components/core/src/clp/ProfilerReporter.hpp
@@ -4,6 +4,23 @@
 #include "Profiler.hpp"
 
 namespace clp {
+/**
+ * RAII helper for automatically reporting all runtime measurements at scope exit.
+ *
+ * This class is designed to simplify the reporting of runtime measurements captured using the
+ * `Profiler` class. By creating an instance of `ProfilerReporter`, all runtime measurements will
+ * automatically be printed when the object goes out of scope.
+ *
+ * Usage:
+ * - Define a `ProfilerReporter` at any logical unit that encompasses all operations you want to
+ *   profile. A common place is in the `main()` function of an executables.
+ * - Once the object is destructured (scope exit), the runtime measurmenets are reported.
+ *
+ * Notes:
+ *  - Only runtime measurements (those tracked by string names) are reported. This class is
+ *    primarily designed to work in tandem with the `ScopedProfiler` class.
+ *  - Copy and move operations are removed to prevent accidentaly multiple reporting.
+ */
 class ProfilerReporter {
 public:
     ProfilerReporter() { Profiler::init(); }
