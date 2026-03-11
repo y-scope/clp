5765cfa6e4bbb57d8bc3996172789e5dc8896a83
SharafMohamed <SharafMohamed@users.noreply.github.com>
2026-03-11 05:32:53 -0400

    Allow for disabling scopes in reporting.

--- components/core/src/clp/ProfilerReporter.hpp
+++ components/core/src/clp/ProfilerReporter.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 #include <spdlog/spdlog.h>
 
@@ -14,15 +15,22 @@ namespace clp {
  * RAII helper for automatically reporting all runtime measurements at scope exit.
  *
  * This class is designed to simplify the reporting of runtime measurements captured using the
- * `Profiler` class. By creating an instance of `ProfilerReporter`, all runtime measurements will
- * automatically be printed when the object goes out of scope.
+ * `Profiler` class.
+ *
+ * Features:
+ * - Prints runtime measurements (total time, call count, average time) when the object is
+ *   destructed. This is the default behavior if no sink is provided in the constructor.
+ * - Can write measurements to a user provided sink when the object is destructed.
+ *   - The sink will be cleared before reporting.
+ *   - Printing will not occur if writing to a sink.
+ * - Supports disabling certain scopes so neither printed nor written to sink.
  *
  * Usage:
  * - Define a `ProfilerReporter` at any logical unit that encompasses all operations you want to
- *   profile. A common place is in the `main()` function of an executables.
+ *   profile. A common place is in the `main()` function of an executable.
  * - If `ScopedProfiler` or `Profiler` is used in the same scope as `ProfilerReporter`,
- *   `ProfilerReporter`must be declared first, such that its destructor is called last.
- * - Once the object is destructed (scope exit), the runtime measurmenets are reported.
+ *   `ProfilerReporter` must be declared first, such that its destructor is called last.
+ * - Once the object is destructed (scope exit), the runtime measurments are reported.
  *
  * Notes:
  *  - Only runtime measurements (those tracked by string names) are reported. This class is
@@ -31,18 +39,34 @@ namespace clp {
  */
 class ProfilerReporter {
 public:
-    ProfilerReporter() = default;
+    explicit ProfilerReporter(std::unordered_set<std::string> disabled_scopes = {}) {
+        set_disabled_scopes(std::move(disabled_scopes));
+    }
 
-    explicit ProfilerReporter(std::unordered_map<std::string, Stopwatch>& sink) : m_sink(&sink) {}
+    explicit ProfilerReporter(
+            std::unordered_map<std::string, Stopwatch>& sink,
+            std::unordered_set<std::string> disabled_scopes = {}
+    )
+            : m_sink(&sink) {
+        set_disabled_scopes(std::move(disabled_scopes));
+    }
 
     ~ProfilerReporter() {
         if (nullptr != m_sink) {
-            *m_sink = Profiler::get_runtime_measurements();
+            m_sink->clear();
+            for (auto const& [name, stopwatch] : Profiler::get_runtime_measurements()) {
+                if (false == m_disabled_scopes.contains(name)) {
+                    m_sink->insert({name, stopwatch});
+                }
+            }
             return;
         }
 
         SPDLOG_INFO("---MEASUREMENTS START---");
         for (auto const& [name, stopwatch] : Profiler::get_runtime_measurements()) {
+            if (m_disabled_scopes.contains(name)) {
+                continue;
+            }
             auto total{stopwatch.get_time_taken_in_seconds()};
             auto calls{stopwatch.get_call_count()};
             auto avg{calls > 0 ? total / calls : 0.0};
@@ -58,7 +82,12 @@ public:
     ProfilerReporter& operator=(ProfilerReporter&&) = delete;
 
 private:
+    auto set_disabled_scopes(std::unordered_set<std::string> disabled_scopes) -> void {
+        m_disabled_scopes = std::move(disabled_scopes);
+    }
+
     std::unordered_map<std::string, Stopwatch>* m_sink{nullptr};
+    std::unordered_set<std::string> m_disabled_scopes;
 };
 }  // namespace clp
 
