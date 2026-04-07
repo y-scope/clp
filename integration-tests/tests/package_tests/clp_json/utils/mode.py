5ff30ddf50cf66e016305f189f83aee043a97626
Quinn <q.mitchell@mail.utoronto.ca>
2026-04-07 16:24:10 +0000

    Describe operating mode in modes.py and mode.py.

---
+++ integration-tests/tests/package_tests/clp_json/utils/mode.py
@@ -0,0 +1,40 @@
+"""Docstring."""
+
+import logging
+
+from clp_py_utils.clp_config import (
+    ClpConfig,
+    Package,
+    QueryEngine,
+    StorageEngine,
+)
+
+from tests.package_tests.classes import (
+    ClpPackageModeConfig,
+)
+from tests.package_tests.utils.modes import (
+    CLP_API_SERVER_COMPONENT,
+    CLP_BASE_COMPONENTS,
+    CLP_GARBAGE_COLLECTOR_COMPONENT,
+    CLP_QUERY_COMPONENTS,
+    CLP_REDUCER_COMPONENT,
+)
+
+logger = logging.getLogger(__name__)
+
+CLP_JSON_MODE = ClpPackageModeConfig(
+    mode_name="clp-json",
+    clp_config=ClpConfig(
+        package=Package(
+            storage_engine=StorageEngine.CLP_S,
+            query_engine=QueryEngine.CLP_S,
+        ),
+    ),
+    component_list=(
+        *CLP_BASE_COMPONENTS,
+        CLP_REDUCER_COMPONENT,
+        *CLP_QUERY_COMPONENTS,
+        CLP_GARBAGE_COLLECTOR_COMPONENT,
+        CLP_API_SERVER_COMPONENT,
+    ),
+)
---
+++ integration-tests/tests/package_tests/utils/modes.py
@@ -0,0 +1,57 @@
+"""Provides utilities related to the user-level configurations of CLP's operating modes."""
+
+import logging
+
+from clp_py_utils.clp_config import (
+    API_SERVER_COMPONENT_NAME,
+    COMPRESSION_SCHEDULER_COMPONENT_NAME,
+    COMPRESSION_WORKER_COMPONENT_NAME,
+    DB_COMPONENT_NAME,
+    GARBAGE_COLLECTOR_COMPONENT_NAME,
+    LOG_INGESTOR_COMPONENT_NAME,
+    PRESTO_COORDINATOR_COMPONENT_NAME,
+    QUERY_SCHEDULER_COMPONENT_NAME,
+    QUERY_WORKER_COMPONENT_NAME,
+    QUEUE_COMPONENT_NAME,
+    REDIS_COMPONENT_NAME,
+    REDUCER_COMPONENT_NAME,
+    RESULTS_CACHE_COMPONENT_NAME,
+    WEBUI_COMPONENT_NAME,
+)
+
+logger = logging.getLogger(__name__)
+
+
+# TODO: This will eventually be replaced by a formalized mapping between component and service.
+def _to_docker_compose_service_name(name: str) -> str:
+    """
+    Convert a component name to a Docker Compose service name.
+
+    :param name:
+    :return: Service name with underscores replaced by hyphens
+    """
+    return name.replace("_", "-")
+
+
+# Names of components that may comprise a given package mode.
+CLP_BASE_COMPONENTS: tuple[str, ...] = (
+    _to_docker_compose_service_name(DB_COMPONENT_NAME),
+    _to_docker_compose_service_name(QUEUE_COMPONENT_NAME),
+    _to_docker_compose_service_name(REDIS_COMPONENT_NAME),
+    _to_docker_compose_service_name(RESULTS_CACHE_COMPONENT_NAME),
+    _to_docker_compose_service_name(COMPRESSION_SCHEDULER_COMPONENT_NAME),
+    _to_docker_compose_service_name(COMPRESSION_WORKER_COMPONENT_NAME),
+    _to_docker_compose_service_name(WEBUI_COMPONENT_NAME),
+)
+CLP_REDUCER_COMPONENT = _to_docker_compose_service_name(REDUCER_COMPONENT_NAME)
+CLP_QUERY_COMPONENTS: tuple[str, ...] = (
+    _to_docker_compose_service_name(QUERY_SCHEDULER_COMPONENT_NAME),
+    _to_docker_compose_service_name(QUERY_WORKER_COMPONENT_NAME),
+)
+CLP_GARBAGE_COLLECTOR_COMPONENT = _to_docker_compose_service_name(GARBAGE_COLLECTOR_COMPONENT_NAME)
+CLP_API_SERVER_COMPONENT = _to_docker_compose_service_name(API_SERVER_COMPONENT_NAME)
+CLP_LOG_INGESTOR_COMPONENT = _to_docker_compose_service_name(LOG_INGESTOR_COMPONENT_NAME)
+CLP_PRESTO_COMPONENTS: tuple[str, ...] = (
+    _to_docker_compose_service_name(PRESTO_COORDINATOR_COMPONENT_NAME),
+    "presto-worker",
+)
