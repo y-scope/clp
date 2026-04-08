8330ec67a3a1a148d7c2a2b3c91552cb0782ad86
Quinn <q.mitchell@mail.utoronto.ca>
2026-04-08 18:06:41 +0000

    Make dataset metadata schema more type-safe with pydantic object.

--- integration-tests/tests/fixtures/datasets.py
+++ integration-tests/tests/fixtures/datasets.py
@@ -6,7 +6,6 @@ from tests.utils.classes import (
     IntegrationTestDataset,
     IntegrationTestPathConfig,
 )
-from tests.utils.utils import load_json_to_dict
 
 
 @pytest.fixture(scope="session")
@@ -14,12 +13,10 @@ def json_multifile(
     integration_test_path_config: IntegrationTestPathConfig,
 ) -> IntegrationTestDataset:
     """Returns an object corresponding to the `json_multifile` test dataset."""
-    path_to_dataset = integration_test_path_config.test_data_path / "json_multifile"
-    metadata_dict = load_json_to_dict(path_to_dataset / "metadata.json")
+    dataset_name = "json_multifile"
     return IntegrationTestDataset(
-        dataset_name="json_multifile",
-        path_to_dataset_logs=path_to_dataset / metadata_dict["file_structure"]["logs_subdir"],
-        metadata_dict=metadata_dict,
+        dataset_name=dataset_name,
+        path_to_dataset_root=integration_test_path_config.test_data_path / dataset_name,
     )
 
 
@@ -28,10 +25,8 @@ def text_multifile(
     integration_test_path_config: IntegrationTestPathConfig,
 ) -> IntegrationTestDataset:
     """Returns an object corresponding to the `text_multifile` test dataset."""
-    path_to_dataset = integration_test_path_config.test_data_path / "text_multifile"
-    metadata_dict = load_json_to_dict(path_to_dataset / "metadata.json")
+    dataset_name = "text_multifile"
     return IntegrationTestDataset(
-        dataset_name="text_multifile",
-        path_to_dataset_logs=path_to_dataset / metadata_dict["file_structure"]["logs_subdir"],
-        metadata_dict=metadata_dict,
+        dataset_name=dataset_name,
+        path_to_dataset_root=integration_test_path_config.test_data_path / dataset_name,
     )
