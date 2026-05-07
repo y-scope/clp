80239b90d2261fcd1df6c81d17dfeece8a872711
Nathan <30803215+Nathan903@users.noreply.github.com>
2026-05-07 11:41:54 -0400

    lint

--- integration-tests/tests/package_tests/classes.py
+++ integration-tests/tests/package_tests/classes.py
@@ -187,7 +187,9 @@ class ClpPackage:
             err_msg = f"Cannot read instance-id file '{clp_instance_id_file_path}': {err}"
             pytest.fail(err_msg)
 
-        uuid_pattern = r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+        uuid_pattern = (
+            r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+        )
         if not re.fullmatch(r"[0-9a-fA-F]{4}|[0-9a-fA-F]{32}|" + uuid_pattern, contents):
             err_msg = (
                 f"Invalid instance ID in {clp_instance_id_file_path}: expected a 4-character"
