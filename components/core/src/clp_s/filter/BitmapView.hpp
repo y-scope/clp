feb92f3d3cca382a0f82e5a09a0783ed723eebc6
gibber9809 <devinbook1@gmail.com>
2026-06-17 16:32:32 +0000

    Minor renames.

--- components/core/src/clp_s/filter/BitmapView.hpp
+++ components/core/src/clp_s/filter/BitmapView.hpp
@@ -14,13 +14,13 @@ namespace clp_s::filter {
  * Concept that defines a method that visits each set bit in a bitmap and determines whether it
  * should remain set or become unset.
  * @param bit_handler
- * @param bit_ix The index of the set bit in the bitmap.
+ * @param bit_idx The index of the set bit in the bitmap.
  * @return A boolean result indicating whether the bit should remain set, or an error code
  * indicating the failure.
  */
 template <typename SetBitVisitor>
-concept SetBitVisitorConcept = requires(SetBitVisitor bit_handler, size_t bit_ix) {
-    { bit_handler(bit_ix) } -> std::same_as<ystdlib::error_handling::Result<bool>>;
+concept SetBitVisitorConcept = requires(SetBitVisitor bit_handler, size_t bit_idx) {
+    { bit_handler(bit_idx) } -> std::same_as<ystdlib::error_handling::Result<bool>>;
 };
 
 /**
@@ -50,10 +50,10 @@ public:
             return std::errc::result_out_of_range;
         }
 
-        auto const component_ix{index / cNumBitsPerComponent};
+        auto const component_idx{index / cNumBitsPerComponent};
         auto const bit_offset{index % cNumBitsPerComponent};
         auto const bit_mask{static_cast<bitmap_component_t>(1) << bit_offset};
-        return 0 != (m_bitmap_array[component_ix] & bit_mask);
+        return 0 != (m_bitmap_array[component_idx] & bit_mask);
     }
 
     [[nodiscard]] auto set_bit(size_t index, bool value) -> ystdlib::error_handling::Result<void> {
@@ -61,13 +61,13 @@ public:
             return std::errc::result_out_of_range;
         }
 
-        auto const component_ix{index / cNumBitsPerComponent};
+        auto const component_idx{index / cNumBitsPerComponent};
         auto const bit_offset{index % cNumBitsPerComponent};
         auto const bit_mask{static_cast<bitmap_component_t>(1) << bit_offset};
         if (value) {
-            m_bitmap_array[component_ix] |= bit_mask;
+            m_bitmap_array[component_idx] |= bit_mask;
         } else {
-            m_bitmap_array[component_ix] &= ~bit_mask;
+            m_bitmap_array[component_idx] &= ~bit_mask;
         }
         return ystdlib::error_handling::success();
     }
@@ -75,29 +75,29 @@ public:
     template <SetBitVisitorConcept SetBitVisitor>
     [[nodiscard]] auto filter_set_bits(SetBitVisitor set_bit_visitor)
             -> ystdlib::error_handling::Result<void> {
-        for (size_t component_ix{}; component_ix < m_bitmap_array_length; ++component_ix) {
-            if (0 == m_bitmap_array[component_ix]) {
+        for (size_t component_idx{}; component_idx < m_bitmap_array_length; ++component_idx) {
+            if (0 == m_bitmap_array[component_idx]) {
                 continue;
             }
 
-            auto const current_component{m_bitmap_array[component_ix]};
+            auto const current_component{m_bitmap_array[component_idx]};
             bitmap_component_t new_component{};
-            auto const last_component_ix{m_bitmap_array_length - 1};
+            auto const last_component_idx{m_bitmap_array_length - 1};
             auto const max_bit{
-                    component_ix == last_component_ix
-                            ? m_num_bits - cNumBitsPerComponent * last_component_ix
+                    component_idx == last_component_idx
+                            ? m_num_bits - cNumBitsPerComponent * last_component_idx
                             : cNumBitsPerComponent
             };
-            for (size_t bit_ix{}; bit_ix < max_bit; ++bit_ix) {
-                auto const current_bit{static_cast<bitmap_component_t>(1) << bit_ix};
+            for (size_t bit_idx{}; bit_idx < max_bit; ++bit_idx) {
+                auto const current_bit{static_cast<bitmap_component_t>(1) << bit_idx};
                 if (0 != (current_component & current_bit)) {
-                    auto const current_ix{bit_ix + cNumBitsPerComponent * component_ix};
-                    if (YSTDLIB_ERROR_HANDLING_TRYX(set_bit_visitor(current_ix))) {
+                    auto const current_idx{bit_idx + cNumBitsPerComponent * component_idx};
+                    if (YSTDLIB_ERROR_HANDLING_TRYX(set_bit_visitor(current_idx))) {
                         new_component |= current_bit;
                     }
                 }
             }
-            m_bitmap_array[component_ix] = new_component;
+            m_bitmap_array[component_idx] = new_component;
         }
         return ystdlib::error_handling::success();
     }
