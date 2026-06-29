a42185c3ac1584c80ffdf50786c3823e63bfe232
ChenXing Yang <60459812+20001020ycx@users.noreply.github.com>
2026-06-29 13:14:47 -0400

    feat(webui): Make Presto search result limit configurable and warn on truncation (resolves #2330). (#2350)
    Co-authored-by: Junhao Liao <junhao.liao@yscope.com>
    Co-authored-by: hoophalab <200652805+hoophalab@users.noreply.github.com>
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

--- components/webui/packages/client/src/config/index.ts
+++ components/webui/packages/client/src/config/index.ts
@@ -5,6 +5,7 @@ import {settings} from "../settings";
 
 const SETTINGS_LOGS_INPUT_TYPE = settings.LogsInputType;
 const SETTINGS_MAX_DATASETS_PER_QUERY = settings.MaxDatasetsPerQuery;
+const SETTINGS_PRESTO_MAX_NUM_SEARCH_RESULTS = settings.PrestoMaxNumSearchResults;
 const SETTINGS_STORAGE_ENGINE = settings.ClpStorageEngine;
 const SETTINGS_QUERY_ENGINE = settings.ClpQueryEngine;
 
@@ -18,6 +19,7 @@ const STREAM_TYPE = CLP_STORAGE_ENGINES.CLP === SETTINGS_STORAGE_ENGINE ?
 export {
     SETTINGS_LOGS_INPUT_TYPE,
     SETTINGS_MAX_DATASETS_PER_QUERY,
+    SETTINGS_PRESTO_MAX_NUM_SEARCH_RESULTS,
     SETTINGS_QUERY_ENGINE,
     SETTINGS_STORAGE_ENGINE,
     STREAM_TYPE,
