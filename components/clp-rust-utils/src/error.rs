8021a139f85a8ec250317e7a315889926c4e056c
Junhao Liao <junhao@junhao.ca>
2026-07-19 04:57:43 -0400

    fix(deps): place yscope-log-viewer tar in build/deps

--- components/clp-rust-utils/src/error.rs
+++ components/clp-rust-utils/src/error.rs
@@ -8,8 +8,8 @@ pub enum Error {
     #[error("`std::io::Error`: {0}")]
     Io(#[from] std::io::Error),
 
-    #[error("`yaml_serde::Error`: {0}")]
-    SerdeYaml(#[from] yaml_serde::Error),
+    #[error("`serde_yaml::Error`: {0}")]
+    SerdeYaml(#[from] serde_yaml::Error),
 
     #[error("`sqlx::Error`: {0}")]
     Sqlx(#[from] sqlx::Error),
