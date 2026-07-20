666aa4907b745b670a04e6e68d23a1942c9f8ead
davidlion <6627627+davidlion@users.noreply.github.com>
2026-07-20 03:24:28 -0400

    New float regex.

--- components/core/src/clpp/Defs.hpp
+++ components/core/src/clpp/Defs.hpp
@@ -17,7 +17,7 @@ inline constexpr std::string_view cFloatEncodingName{"float"};
 inline constexpr std::string_view cIntEncodingName{"int"};
 // A log_surgeon::Match's possible encodings are listed in insertion order to the parsing spec.
 inline constexpr std::array cEncodingPatterns{
-        EncodingPattern{.name = cFloatEncodingName, .pattern = R"([-+]?\d*\.?\d+([eE][-+]?\d+)?)"},
+        EncodingPattern{.name = cFloatEncodingName, .pattern = R"(-?\d+\.\d+([eE]-?\d+)?)"},
         EncodingPattern{.name = cIntEncodingName, .pattern = R"(-?\d+)"}
 };
 
