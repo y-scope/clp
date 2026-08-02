a4efe19f6c19d3200af3f3858bb415092a014a83
Sitao Wang <sitaowang1998@outlook.com>
2026-08-02 16:31:54 -0400

    Remove config in rust

--- components/clp-tdl-package/src/task/compression/compress.rs
+++ components/clp-tdl-package/src/task/compression/compress.rs
@@ -1087,7 +1087,6 @@ mod tests {
             port: 3306,
             names: ClpDbNames {
                 clp: "clp-db".to_string(),
-                spider: "spider-db".to_string(),
             },
             table_prefix: "clp_".to_string(),
         };
