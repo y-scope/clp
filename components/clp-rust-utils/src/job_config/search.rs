2096e9b42e74b25d0d03881a492c387c0da6a181
Bingran Hu <bingran.hu@yscope.com>
2026-08-31 18:29:49 -0400

    Add query job ID type

--- components/clp-rust-utils/src/job_config/search.rs
+++ components/clp-rust-utils/src/job_config/search.rs
@@ -3,6 +3,8 @@ use num_enum::TryFromPrimitive;
 use serde::Deserialize;
 use serde::Serialize;
 
+pub type QueryJobId = i32;
+
 pub const QUERY_JOBS_TABLE_NAME: &str = "query_jobs";
 
 /// Mirror of `job_orchestration.scheduler.job_config.AggregationConfig`. Must be kept in sync.
--- components/clp-tdl-package/src/task/query/mod.rs
+++ components/clp-tdl-package/src/task/query/mod.rs
@@ -1,5 +1,6 @@
 //! The query-task signatures registered with Spider.
 
+use clp_rust_utils::job_config::QueryJobId;
 use clp_rust_utils::task_io::query::ClpSQueryOption;
 use clp_rust_utils::task_io::query::QueryTaskOutput;
 use spider_tdl::TaskContext;
@@ -9,7 +10,7 @@ use spider_tdl::task;
 #[task(name = "query::clp_s_query_to_results_cache")]
 pub(crate) fn clp_s_query_to_results_cache_task(
     _ctx: TaskContext,
-    _query_job_id: i32,
+    _query_job_id: QueryJobId,
     _clp_s_query_option: ClpSQueryOption,
     _dataset: String,
     _archive_id: String,
