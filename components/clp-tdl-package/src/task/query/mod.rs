323acfc9acf0776f15342703a77553d0f677542a
Bingran Hu <bingran.hu@yscope.com>
2026-08-31 19:37:17 -0400

    Rename task

--- components/clp-tdl-package/src/task/query/mod.rs
+++ components/clp-tdl-package/src/task/query/mod.rs
@@ -6,13 +6,13 @@ use non_empty_string::NonEmptyString;
 use spider_tdl::TaskContext;
 use spider_tdl::task;
 
-#[task(name = "query::clp_s_query_to_results_cache")]
-pub(crate) fn clp_s_query_to_results_cache_task(
+#[task(name = "query::clp_s_search")]
+pub(crate) fn clp_s_search_task(
     _ctx: TaskContext,
     _query_job_id: QueryJobId,
     _clp_s_query_option: ClpSQueryOption,
     _dataset: Option<NonEmptyString>,
     _archive_id: NonEmptyString,
 ) -> Result<(), spider_tdl::TdlError> {
-    todo!("Implement the CLP-S results-cache query task")
+    todo!("Implement the CLP-S search task")
 }
